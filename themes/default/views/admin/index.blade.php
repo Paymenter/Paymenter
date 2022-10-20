d04863af9b77eb12b7b252e2605450fff152ee9a
Easylemon24 <loggorus@gmail.com>
2022-10-21 01:33:12 +0200

    Updated Design
    Completed the admin dashboard

--- themes/default/views/admin/index.blade.php
+++ themes/default/views/admin/index.blade.php
@@ -4,32 +4,32 @@
     </x-slot>
     <div class="dark:bg-darkmode py-10">
         <div class="mx-auto sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg p-4 grid md:grid-cols-3">
+            <div class="dark:bg-darkmode2 overflow-hidden bg-white shadow-sm sm:rounded-lg p-4 grid md:grid-cols-3">
                 <!-- show ticketclosed, tickets, orders -->
-                <div class="p-10 bg-white border-2 rounded-xl border-grey-600 col-span-2">
+                <div class="dark:bg-darkmode dark:border-darkmode p-10 bg-white border-2 rounded-xl border-grey-600 col-span-2">
                     <div class="grid grid-cols-3 p-2 pb-10">
-                        <div class="bg-normal rounded-md mr-3 p-2">
-                            <h1 class="text-xl text-gray-500">Revenue today</h1>
-                            <p class="text-black font-bold text-2xl">€20</p>
+                        <div class="dark:bg-darkmode2 bg-normal rounded-md mr-3 p-2">
+                            <h1 class="dark:text-darkmodetext text-xl text-gray-500">Revenue today</h1>
+                            <p class="dark:text-darkmodetext text-black font-bold text-2xl">€20</p>
                         </div>
-                        <div class="bg-normal rounded-md mr-3 p-2">
-                            <h1 class="text-xl text-gray-500">Tickets today</h1>
-                            <p class="text-black font-bold text-2xl">5</p>
+                        <div class="dark:bg-darkmode2 bg-normal rounded-md mr-3 p-2">
+                            <h1 class="dark:text-darkmodetext text-xl text-gray-500">Tickets today</h1>
+                            <p class="dark:text-darkmodetext text-black font-bold text-2xl">5</p>
                         </div>
-                        <div class="bg-normal rounded-md p-2">
-                            <h1 class="text-xl text-gray-500">Revenue Total</h1>
-                            <p class="text-black font-bold text-2xl">$500</p>
+                        <div class="dark:bg-darkmode2 bg-normal rounded-md p-2">
+                            <h1 class="dark:text-darkmodetext text-xl text-gray-500">Revenue Total</h1>
+                            <p class="dark:text-darkmodetext text-black font-bold text-2xl">$500</p>
                         </div>
                     </div>
                     <canvas id="myChart" style="width:100%;max-height:400px;"></canvas>
                 </div>
-                <div class="p-10 bg-white border-2 rounded-xl border-grey-600 ml-4">
-                    <h1 class="text-xl text-gray-500">Recent tickets</h1>
+                <div class="dark:bg-darkmode dark:border-darkmode p-10 bg-white border-2 rounded-xl border-grey-600 ml-4">
+                    <h1 class="dark:text-darkmodetext text-xl text-gray-500">Recent tickets</h1>
                     <div class="grid grid-cols-1 gap-4">
                     @foreach(App\Models\Tickets::all()->take(3) as $ticket)
                         <div class="bg-normal rounded-md p-2">
-                            <h1 class="text-xl text-gray-500">Ticket #{{$ticket->id}}</h1>
-                            <p class="text-black font-bold text-2xl">{{ $ticket->title }}</p>
+                            <h1 class="dark:text-darkmodetext text-xl text-gray-500">Ticket #{{$ticket->id}}</h1>
+                            <p class="dark:text-darkmodetext text-black font-bold text-2xl">{{ $ticket->title }}</p>
                         </div>
                     @endforeach
 
