b83718e3b1457ff8421117510d614c2003a92561
Easylemon24 <loggorus@gmail.com>
2022-10-21 00:46:16 +0200

    Changed the look of Products
    Just made that the Products have a bit more space of you have multiple. Also added a bit of space after the toggle Mode of Website(dark,light) of Website

--- themes/default/views/layouts/adminnavigation.blade.php
+++ themes/default/views/layouts/adminnavigation.blade.php
@@ -1,5 +1,5 @@
-<nav class="bg-white border-b border-gray-100">
-    <div class="px-4 max-w-7xl sm:px-6 lg:px-8">
+<nav class="dark:bg-darkmode bg-white border-b border-gray-100">
+    <div class="dark:bg-darkmode px-4 max-w-7xl sm:px-6 lg:px-8">
         <div class="flex  h-16 text-gray-500 hover:text-gray-700 text-sm font-medium leading-5 items-center">
             <div class="flex">
                 <div class="flex items-center flex-shrink-0">
