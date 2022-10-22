2c6cd88d40ea4163ee52afd24ecb6db20d265193
Easylemon24 <loggorus@gmail.com>
2022-10-22 15:17:07 +0200

    Updated Design
    Added Darkmode to the Show ticket in admin tab

--- themes/default/views/admin/tickets/index.blade.php
+++ themes/default/views/admin/tickets/index.blade.php
@@ -4,81 +4,81 @@
     </x-slot>
     <div class="py-12">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+            <div class="dark:bg-darkmode2 overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <!-- show the user tickets and products -->
                 <!-- create button -->
                 <div class="flex items-center justify-end mt-4">
                     <a href="{{ route('admin.tickets.create') }}"
-                        class="bg-logo hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        class="mr-4 bg-logo hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
                         {{ __('normal.create') }}
                     </a>
                 </div>
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="dark:bg-darkmode2 p-6 bg-white ">
                     <div class="flex flex-col">
                         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                                <div class="shadow overflow-hidden sm:rounded-lg">
                                     <table class="min-w-full divide-y divide-gray-200" id="tickets">
-                                        <thead class="bg-gray-50">
+                                        <thead class="dark:bg-darkmode">
                                             <tr>
                                                 <th scope="col"
-                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    class="dark:text-darkmodetext px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     {{ __('tickets.subject') }}
                                                 </th>
                                                 <th scope="col"
-                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    class="dark:text-darkmodetext px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     {{ __('tickets.priority') }}
                                                 </th>
                                                 <th scope="col"
-                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    class="dark:text-darkmodetext px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     {{ __('normal.client') }}
                                                 </th>
                                                 <th scope="col"
-                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    class="dark:text-darkmodetext px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     {{ __('tickets.status') }}
                                                 </th>
                                                 <th scope="col"
-                                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                                    class="dark:text-darkmodetext px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                                                     {{ __('normal.created_at') }}
                                                 </th>
                                                 <th scope="col" class="relative px-6 py-3">
-                                                    <span class="sr-only">Reply</span>
+                                                    <span class="dark:text-darkmodetext sr-only">Reply</span>
                                                 </th>
                                             </tr>
                                         </thead>
-                                        <tbody class="bg-white divide-y divide-gray-200">
+                                        <tbody class="dark:bbg-darkmode bg-white divide-y divide-gray-200">
                                             @foreach ($tickets as $service)
                                                 <tr>
                                                     <td class="px-6 py-4 whitespace-nowrap">
-                                                        <div class="text-sm text-gray-900">
+                                                        <div class="dark:text-darkmodetext text-sm text-gray-900">
                                                             {{ $service->title }}
                                                         </div>
                                                     </td>
                                                     <td class="px-6 py-4 whitespace-nowrap">
                                                         @if ($service->priority == 'low')
                                                             <span
-                                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
+                                                                class="dark:text-darkmodetext px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
                                                                 {{ $service->priority }}
                                                             </span>
                                                         @elseif($service->priority == 'medium')
                                                             <span
-                                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
+                                                                class="dark:text-darkmodetext px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-yellow-100 text-yellow-800">
                                                                 {{ $service->priority }}
                                                             </span>
                                                         @elseif($service->priority == 'high')
                                                             <span
-                                                                class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
+                                                                class="dark:text-darkmodetext px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-red-100 text-red-800">
                                                                 {{ $service->priority }}
                                                             </span>
                                                         @endif
                                                     </td>
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                    <td class="dark:text-darkmodetext px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                         {{ $service->client()->get()[0]->name }}
                                                     </td>
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                    <td class="dark:text-darkmodetext px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                         {{ $service->status }}
                                                     </td>
-                                                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                                                    <td class="dark:text-darkmodetext px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                                                         {{ $service->created_at }}
                                                     </td>
                                                     <td
