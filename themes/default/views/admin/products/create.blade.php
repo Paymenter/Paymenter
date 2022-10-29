af54ea2c491b8f1c31100699eb4ab63a2447a650
Easylemon24 <loggorus@gmail.com>
2022-10-29 22:31:32 +0200

    Updated Design
    Added Darkmode to products/create

--- themes/default/views/admin/products/create.blade.php
+++ themes/default/views/admin/products/create.blade.php
@@ -5,42 +5,42 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
-                    <div class="mt-8 text-2xl">
+            <div class="dark:bg-darkmode2 bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <div class="dark:bg-darkmode2 p-6 sm:px-20 bg-white">
+                    <div class="dark:text-darkmodetext mt-8 text-2xl">
                         Create Product
                     </div>
                     <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-                    <div class="mt-6 text-gray-500">
+                    <div class="dark:text-darkmodetext mt-6 text-gray-500">
                         <form method="POST" action="{{ route('admin.products.store') }}" enctype="multipart/form-data">
                             @csrf
                             <div>
                                 <label for="name">{{ __('Name') }}</label>
 
-                                <input id="name" class="block mt-1 w-full" type="text" name="name"
+                                <input id="name" class="dark:bg-darkmode rounded-lg block mt-1 w-full" type="text" name="name"
                                     value="{{ old('name') }}" required autofocus />
                             </div>
                             <div class="mt-4">
                                 <label for="description">{{ __('Description') }}</label>
 
-                                <textarea id="description" class="block mt-1 w-full" name="description" required>{{ old('description') }}</textarea>
+                                <textarea id="description" class="dark:bg-darkmode rounded-lg block mt-1 w-full" name="description" required>{{ old('description') }}</textarea>
                             </div>
                             <div class="mt-4">
                                 <label for="price">{{ __('Price') }}</label>
 
-                                <input id="price" class="block mt-1 w-full" type="number" name="price"  min="1" step="any"
+                                <input id="price" class="dark:bg-darkmode rounded-lg block mt-1 w-full" type="number" name="price"  min="1" step="any"
                                     value="{{ old('price') }}" required />
                             </div>
                             <div class="mt-4">
                                 <label for="image">{{ __('Image') }}</label>
 
-                                <input id="image" class="block mt-1 w-full" type="file" name="image"
+                                <input id="image" class="dark:bg-darkmode rounded-lg block mt-1 w-full" type="file" name="image"
                                     required />
                             </div>
                             <div class="mt-4">
                                 <label for="category">{{ __('Category') }}</label>
-                                <select id="category" class="block mt-1 w-full" name="category_id" required>
+                                <select id="category" class="dark:bg-darkmode rounded-lg block mt-1 w-full" name="category_id" required>
                                     @if ($categories->count())
                                         @foreach ($categories as $category)
                                             <option value="{{ $category->id }}">{{ $category->name }}</option>
