323762ad200ba832214965a20c5341d82e10c438
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-11-05 10:08:29 +0100

    Updated install script(tested on ubuntu 20.04)

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ pterodactyl_token=""
 
 # installation toggles
 configure_letsencrypt=false
+email=""
 
 # Visual and input
 info() {
@@ -30,7 +31,7 @@ info() {
 print_error() {
     COLOR_RED='\033[0;31m'
     COLOR_NC='\033[0m'
-
+    
     echo ""
     echo -e "* ${COLOR_RED}ERROR${COLOR_NC}: $1"
     echo ""
@@ -39,14 +40,14 @@ print_error() {
 required_input() {
     local __resultvar=$1
     local result=''
-
+    
     while [ -z "$result" ]; do
         echo -n "* ${2}"
         read -r result
-
+        
         [ -z "$result" ] && print_error "${3}"
     done
-
+    
     eval "$__resultvar="'$result'""
 }
 
@@ -54,10 +55,10 @@ password_input() {
     local __resultvar=$1
     local result=''
     local default="$4"
-
+    
     while [ -z "$result" ]; do
         echo -n "* ${2}"
-
+        
         # modified from https://stackoverflow.com/a/22940001
         while IFS= read -r -s -n1 char; do
             [[ -z $char ]] && {
@@ -82,7 +83,7 @@ password_input() {
         [ -z "$result" ] && [ -n "$default" ] && result="$default"
         [ -z "$result" ] && print_error "${3}"
     done
-
+    
     eval "$__resultvar="'$result'""
 }
 
@@ -93,24 +94,24 @@ detect_distro() {
         . /etc/os-release
         OS=$(echo "$ID" | awk '{print tolower($0)}')
         OS_VER=$VERSION_ID
-    elif type lsb_release >/dev/null 2>&1; then
+        elif type lsb_release >/dev/null 2>&1; then
         # linuxbase.org
         OS=$(lsb_release -si | awk '{print tolower($0)}')
         OS_VER=$(lsb_release -sr)
-    elif [ -f /etc/lsb-release ]; then
+        elif [ -f /etc/lsb-release ]; then
         # For some versions of Debian/Ubuntu without lsb_release command
         . /etc/lsb-release
         OS=$(echo "$DISTRIB_ID" | awk '{print tolower($0)}')
         OS_VER=$DISTRIB_RELEASE
-    elif [ -f /etc/debian_version ]; then
+        elif [ -f /etc/debian_version ]; then
         # Older Debian/Ubuntu/etc.
         OS="debian"
         OS_VER=$(cat /etc/debian_version)
-    elif [ -f /etc/SuSe-release ]; then
+        elif [ -f /etc/SuSe-release ]; then
         # Older SuSE/etc.
         OS="SuSE"
         OS_VER="?"
-    elif [ -f /etc/redhat-release ]; then
+        elif [ -f /etc/redhat-release ]; then
         # Older Red Hat, CentOS, etc.
         OS="Red Hat/CentOS"
         OS_VER="?"
@@ -119,21 +120,21 @@ detect_distro() {
         OS=$(uname -s)
         OS_VER=$(uname -r)
     fi
-
+    
     OS=$(echo "$OS" | awk '{print tolower($0)}')
     OS_VER_MAJOR=$(echo "$OS_VER" | cut -d. -f1)
 }
 
 check_os_comp() {
     case "$OS" in
-    ubuntu)
-        [ "$OS_VER_MAJOR" == "20" ] && SUPPORTED=true
+        ubuntu)
+            [ "$OS_VER_MAJOR" == "20" ] && SUPPORTED=true
         ;;
-    *)
-        SUPPORTED=false
+        *)
+            SUPPORTED=false
         ;;
     esac
-
+    
     # exit if not supported
     if [ "$SUPPORTED" == true ]; then
         info "$OS $OS_VER is supported."
@@ -152,42 +153,40 @@ check_dashboard_present() {
     fi
 }
 
-check_dependencies() {
-    for dependency in composer crontab; do
-        if ! [ -x "$(command -v $dependency)" ]; then
-            print_error "Missing $dependency, are you sure you are running Pterodactyl on this machine?"
-            exit 1
-        fi
-    done
-}
 
 ask_variables() {
     # MySQL
     read -r -p "* Database name ($mysql_db): " mysql_db
     mysql_db=${mysql_db:-dashboard}
-
+    
     read -r -p "* Database user ($mysql_user): " mysql_user
     mysql_user=${mysql_user:-dashboarduser}
-
+    
     # password input
     rand_pw=$(
         tr -dc 'A-Za-z0-9' </dev/urandom | head -c 64
         echo
     )
     password_input mysql_password "Password (press enter to use randomly generated password): " "MySQL password cannot be empty" "$rand_pw"
-
+    
     # Application details
     read -r -p "* Name of dashboard ($app_name): " app_name
     app_name=${app_name:-Dashboard}
-
+    
     required_input fqdn "FQDN of your new installation: " "FQDN cannot be empty"
     app_url="http://$fqdn"
-
+    
     # Let's Encrypt
     echo -e -n "* Do you want to automatically configure HTTPS using Let's Encrypt? (y/N): "
     read -r confirm_ssl
-
+    
     [[ "$confirm_ssl" =~ [Yy] ]] && configure_letsencrypt=true && app_url="https://$fqdn"
+    
+    # Ask for email if Let's Encrypt is enabled
+    if [ "$configure_letsencrypt" == true ]; then
+        required_input email "Email address for Let's Encrypt: " "Email cannot be empty"
+    fi
+    email=${email:-admin@$fqdn}
 
     true # last statement of function is function exit value, if above statement evaluates to false then script exits without this
 }
@@ -203,57 +202,65 @@ confirm() {
 install_dependencies() {
     apt update
     apt upgrade -y
-    apt install -y git curl php8.0-intl
+    apt -y install software-properties-common curl apt-transport-https ca-certificates gnupg
+    LC_ALL=C.UTF-8 add-apt-repository -y ppa:ondrej/php
+    curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
+    apt update
+    apt-add-repository universe
+    apt -y install php8.0 php8.0-{common,cli,gd,mysql,mbstring,bcmath,xml,fpm,curl,zip} mariadb-server nginx tar unzip git redis-server
+    curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 }
 
 install_controllpanel() {
     mkdir -p /var/www/paymenter
     cd /var/www/paymenter
+    
+    curl -Lo paymenter.tar.gz https://github.com/paymenter/paymenter/releases/latest/download/paymenter.tar.gz
+    tar -xzvf paymenter.tar.gz
 
-    git clone https://github.com/Paymenter/Paymenter.git ./
     chmod -R 755 storage/* bootstrap/cache/
-
+    
     cp .env.example .env
     COMPOSER_ALLOW_SUPERUSER=1 composer install --no-dev --optimize-autoloader
-
+    
     php artisan key:generate --force
     php artisan storage:link
 }
 
 create_database() {
     echo "* Performing MySQL queries.."
-
+    
     echo "* Creating MySQL user.."
     mysql -u root -e "CREATE USER '${mysql_user}'@'127.0.0.1' IDENTIFIED BY '${mysql_password}';"
-
+    
     echo "* Creating database.."
     mysql -u root -e "CREATE DATABASE ${mysql_db};"
-
+    
     echo "* Granting privileges.."
     mysql -u root -e "GRANT ALL PRIVILEGES ON ${mysql_db}.* TO '${mysql_user}'@'127.0.0.1' WITH GRANT OPTION;"
-
+    
     echo "* Flushing privileges.."
     mysql -u root -e "FLUSH PRIVILEGES;"
-
+    
     echo "* MySQL database created & configured!"
 }
 
 configure() {
     sed -i "s@APP_NAME=Paymenter@APP_NAME=$app_name@g" .env
     sed -i "s@APP_URL=http://localhost@APP_URL=$app_url@g" .env
-
+    
     sed -i "s@DB_DATABASE=paymenter@DB_DATABASE=$mysql_db@g" .env
     sed -i "s@DB_USERNAME=paymenter@DB_USERNAME=$mysql_user@g" .env
     sed -i "s@DB_PASSWORD=@DB_PASSWORD=$mysql_password@g" .env
 }
 
 migrate() {
-    php artisan migrate 
+    php artisan migrate
 }
 
 create_initial_user() {
     # Create user account
-    php artisan p:make:user || true
+    php artisan p:user:create || true
 }
 
 set_permissions() {
@@ -316,10 +323,9 @@ EOF
 }
 
 obtain_le_cert() {
-    add-apt-repository ppa:certbot/certbot
-    apt update
-    apt install python-certbot-nginx
-    certbot --nginx -d $fqdn
+    apt-get update
+    apt-get install certbot python3-certbot-nginx -y
+    certbot --nginx -d $fqdn --non-interactive --agree-tos --redirect --email $email
 }
 
 restart_nginx() {
@@ -328,17 +334,16 @@ restart_nginx() {
 
 main() {
     info "Paymenter installation script"
-
+    
     # pre-checks
     detect_distro
     check_os_comp
     check_dashboard_present
-    check_dependencies
-
+    
     # ask questions about configuration details
     ask_variables
     confirm
-
+    
     # installation
     install_dependencies
     install_controllpanel
