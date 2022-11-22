910f75c98fcfe3befddb81f1d95c75e9dfd142a3
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-11-22 13:19:41 +0100

    Removed unneccesary packages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
@@ -11,6 +10,5 @@ export default defineConfig({
             ],
             refresh: true,
         }),
-        react(),
     ],
 });
