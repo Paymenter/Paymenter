bc55a8fcb3b0419aa740e53243f80ab69585e6cb
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2022-11-26 15:48:30 +0100

    solved bug with migration

--- database/migrations/2022_11_23_170600_admin_perm.php
+++ database/migrations/2022_11_23_170600_admin_perm.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->array('permissions')->default([]);
+            $table->json('permissions')->default([]);
         });
     }
 
