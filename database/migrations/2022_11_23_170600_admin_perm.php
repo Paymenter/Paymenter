f640345c721fe084f9e8b699631f9057be35d04b
Corwin <info@corwindev.nl>
2022-12-19 11:36:51 +0100

    Json can't have a default value

--- database/migrations/2022_11_23_170600_admin_perm.php
+++ database/migrations/2022_11_23_170600_admin_perm.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->json('permissions')->default('[]');
+            $table->json('permissions');
         });
     }
 
