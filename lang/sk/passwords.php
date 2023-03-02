390b542a4f74220e30d398c5ea7a4d78fd6574ec
SprayerGaming <66383583+SprayerGaming@users.noreply.github.com>
2023-03-02 10:22:41 +0100

    sk folder fixed

---
+++ lang/sk/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'A jelszavad megváltozott',
+    'sent' => 'Elküldtük a jelszó visszaállitási hivatkozást emailben!',
+    'throttled' => 'Kérlek várj egy kicsit, majd próbáld ujra',
+    'token' => 'Ez a jelszó helyreállitási token helytelen',
+    'user' => "A megadott e-mail cim nem található.",
+];
