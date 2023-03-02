6cf7a65ce6ac822f325f5f25caf76410564fb4db
fpaiva36834 <72525693+fpaiva36834@users.noreply.github.com>
2023-03-02 09:23:55 +0000

    pt-translation (#77)

---
+++ lang/pt/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Autenticação de Linhas de Idioma
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de idioma são utilizadas durante a autenticação para vários
+    | mensagens que precisamos mostrar ao usuário. Você é livre para modificar
+    | estas linhas de idioma de acordo com os requisitos da sua aplicação.
+    |
+    */
+
+    'failed' => 'As credenciais inseridas estão incorretas.',
+    'password' => 'A senha está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Tente novamente em :seconds segundos.',
+
+];
---
+++ lang/pt/pagination.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de idioma de paginação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de paginação são usadas para os links de paginação.
+    | Você é livre para alterá-las como desejar.
+    | Se você tiver uma ideia mais emocionante, nos informe.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next'     => 'Seguinte &raquo;',
+
+];
