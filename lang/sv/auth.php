667acf795b0a5f1b73319b8348fbcc2b1944cc49
Sixten Peterson <sixten.peterson@winterzone.se>
2023-03-10 19:39:59 +0100

    Rough translation of the folder files for Swedish

---
+++ lang/sv/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    // Not a very direct translate but the message is roughly the same. -> Could probably be improved though.
+    'failed' => 'Inloggning misslyckades. Dessa kontouppgifter stämmer inte överens.',
+    'password' => 'Fel lösenord.',
+    'throttle' => 'Du har försökt logga in för många gånger i rad. Vänligen försök igen om :seconds sekunder.',
+];
---
+++ lang/sv/passwords.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Ditt lösenord har återställts!',
+    'sent' => 'Vi har skickat ett mail med en länk för att återställa ditt lösenord!',
+    'throttled' => 'Vönligen vänta innan du försöker igen.',
+    // For lack of a better word I decided on going with "nyckel", meaning "key" as I can't come up with a better word for "token" in Swedish.
+    'token' => 'Denna lösenordsåterställningsnyckel är ogiltig.',
+    'user' => "Vi kan inte hitta en användare med den e-postadressen.",
+];
