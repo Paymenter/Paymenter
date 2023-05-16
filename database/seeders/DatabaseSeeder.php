97a16e6ed23c9b7e1e638dab91feaaf9a5fcc70c
ItsJusty <k.terpstra2000@icloud.com>
2023-05-16 15:12:10 +0200

    chore: Updated settings migration + seeder.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,8 +12,9 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         $this->call(RolePermissionSeeder::class);
+        $this->call(SettingSeeder::class);
     }
 }
