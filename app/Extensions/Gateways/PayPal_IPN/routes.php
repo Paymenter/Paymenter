0727cc2ec61f01b80ba522fcfe4da4160493cfd6
Corwin <info@corwindev.nl>
2023-05-22 19:32:40 +0200

    revert: pushed paypal_ipn

--- app/Extensions/Gateways/PayPal_IPN/routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-include_once __DIR__ . '/index.php';
-
-Route::post('/paypal_ipn/webhook', function () {
-    PayPal_IPN_webhook(request());
-})->name('paypal_ipn.webhook');
