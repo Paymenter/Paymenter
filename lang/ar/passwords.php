f1d43165ccca444c6339c3093d736ca67e0e979c
Turkidev <114973795+Turkidev@users.noreply.github.com>
2023-08-16 22:32:05 +0300

    Create passwords.php

---
+++ lang/ar/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+'reset' => 'تم إعادة تعيين كلمة المرور الخاصة بك!',
+'sent' => 'لقد أرسلنا رابط إعادة تعيين كلمة المرور إلى بريدك الإلكتروني!',
+'throttled' => 'الرجاء الانتظار قبل إعادة المحاولة.',
+'token' => 'رمز إعادة تعيين كلمة المرور غير صالح.',
+'user' => "لا يمكننا العثور على مستخدم بهذا البريد الإلكتروني.",
+];
