807b16124206ed825fd7193df8a46b3f84772e4c
Mateusz Sikorski <mateusz@mscode.pl>
2023-08-22 00:31:23 +0200

    FIXED: 1. Username error

--- tests/Feature/Auth/RegistrationTest.php
+++ tests/Feature/Auth/RegistrationTest.php
@@ -23,6 +23,7 @@ class RegistrationTest extends TestCase
         $response = $this->post('/register', [
             'name' => 'Test User',
             'email' => 'test@example.com',
+            'username' => 'test',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
