9ba0826d6ee11cd4291dd12c6287c2b39832b309
Mateusz Sikorski <mateusz@mscode.pl>
2023-08-21 11:47:33 +0200

    UI and FIXES (#131)
    * FIX & UI: adminnavigation.blade.php (Mobile Navigation)
    
    * FIXES:
    1. Admin Navigation style
    2. Clients Home > Date display style
    3. CronJob > Space between the name and the date in the invoice
    
    UI:
    1. Clients Home > Pending Invoices
    2. Admin Orders > Style
    
    * FIXES
    
    * FIX
    
    * quick fix
    
    * Update adminnavigation.blade.php
    
    * UI:
    1. Admin Invoices Show
    2. Admin invoices list
    
    * UI:
    1. Admin Invoice info display style changed
    
    * UI:
    1. Admin Invoice info display style changed
    
    * UI:
    1. Add avatar to user in admin/clients/index.php
    
    * quick fix
    
    * quick fix
    
    * quick fix
    
    ---------
    
    Co-authored-by: Corwin <info@corwindev.nl>

--- themes/default/views/layouts/adminnavigation.blade.php
+++ themes/default/views/layouts/adminnavigation.blade.php
@@ -128,60 +128,6 @@
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                     @csrf
                 </form>
-                <div class="flex justify-end col-span-1 pl-1">
-                    <button id="theme-toggle" type="button" class="mr-4 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                        <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                        </svg>
-                        <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-                        </svg>
-                    </button>
-                </div>
-                <script>
-                    var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-                    var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-                    // Change the icons inside the button based on previous settings
-                    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia(
-                        '(prefers-color-scheme: dark)').matches)) {
-                        themeToggleLightIcon.classList.remove('hidden');
-                    } else {
-                        themeToggleDarkIcon.classList.remove('hidden');
-                    }
-
-                    var themeToggleBtn = document.getElementById('theme-toggle');
-
-                    themeToggleBtn.addEventListener('click', function() {
-
-                        // toggle icons inside button
-                        themeToggleDarkIcon.classList.toggle('hidden');
-                        themeToggleLightIcon.classList.toggle('hidden');
-
-                        // if set via local storage previously
-                        if (localStorage.getItem('theme')) {
-                            if (localStorage.getItem('theme') === 'light') {
-                                document.documentElement.classList.add('dark');
-                                localStorage.setItem('theme', 'dark');
-                            } else {
-                                document.documentElement.classList.remove('dark');
-                                localStorage.setItem('theme', 'light');
-                            }
-
-                            // if NOT set via local storage previously
-                        } else {
-                            if (document.documentElement.classList.contains('dark')) {
-                                document.documentElement.classList.remove('dark');
-                                localStorage.setItem('theme', 'light');
-                            } else {
-                                document.documentElement.classList.add('dark');
-                                localStorage.setItem('theme', 'dark');
-                            }
-                        }
-
-                    });
-
-                </script>
             </div>
         </div>
         <div class="hidden lg:hidden" id="mobile-menu">
@@ -253,60 +199,6 @@
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
                             @csrf
                         </form>
-                        <div class="flex justify-end col-span-1 pl-1">
-                            <button id="theme-toggle" type="button" class="mr-4 text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5">
-                                <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
-                                </svg>
-                                <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                    <path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path>
-                                </svg>
-                            </button>
-                        </div>
-                        <script>
-                            var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
-                            var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
-
-                            // Change the icons inside the button based on previous settings
-                            if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia(
-                                '(prefers-color-scheme: dark)').matches)) {
-                                themeToggleLightIcon.classList.remove('hidden');
-                            } else {
-                                themeToggleDarkIcon.classList.remove('hidden');
-                            }
-
-                            var themeToggleBtn = document.getElementById('theme-toggle');
-
-                            themeToggleBtn.addEventListener('click', function() {
-
-                                // toggle icons inside button
-                                themeToggleDarkIcon.classList.toggle('hidden');
-                                themeToggleLightIcon.classList.toggle('hidden');
-
-                                // if set via local storage previously
-                                if (localStorage.getItem('theme')) {
-                                    if (localStorage.getItem('theme') === 'light') {
-                                        document.documentElement.classList.add('dark');
-                                        localStorage.setItem('theme', 'dark');
-                                    } else {
-                                        document.documentElement.classList.remove('dark');
-                                        localStorage.setItem('theme', 'light');
-                                    }
-
-                                    // if NOT set via local storage previously
-                                } else {
-                                    if (document.documentElement.classList.contains('dark')) {
-                                        document.documentElement.classList.remove('dark');
-                                        localStorage.setItem('theme', 'light');
-                                    } else {
-                                        document.documentElement.classList.add('dark');
-                                        localStorage.setItem('theme', 'dark');
-                                    }
-                                }
-
-                            });
-
-                        </script>
                     </div>
                 @endauth
             </div>
