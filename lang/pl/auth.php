983cc5daf6ad88d28e2084d674c79571547a8513
Mateusz Sikorski <mateusz@mscode.pl>
2023-08-22 13:59:02 +0200

    LANG: 1. laravel-language-extractor 2. Added Polish (This is not the final version, there will probably be corrections)

---
+++ lang/pl/auth.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Te dane uwierzytelniające nie są zgodne z naszymi danymi.',
+    'password' => 'Podane hasło jest nieprawidłowe.',
+    'throttle' => 'Zbyt wiele prób logowania. Spróbuj ponownie za :seconds sekund.',
+];
