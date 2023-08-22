f8523ffb4e189ad731bc9d936c4ca5ef3652c2bb
Mateusz Sikorski <mateusz@mscode.pl>
2023-08-22 14:22:58 +0200

    quick fix

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'username' => fake()->unique()->userName(),
+            'lastname' => fake()->lastname(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
             'role_id' => 2,
