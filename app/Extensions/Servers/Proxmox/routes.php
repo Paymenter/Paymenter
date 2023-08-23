405016731b5ee75e389ffc854d03152591261918
Corwin <info@corwindev.nl>
2023-08-23 09:24:06 +0200

    fix: fixed proxmox start/stop

--- app/Extensions/Servers/Proxmox/routes.php
+++ app/Extensions/Servers/Proxmox/routes.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/proxmox/status/{id}', [App\Extensions\Servers\Proxmox\Proxmox::class, 'status'])->name('extensions.proxmox.status');
+Route::post('/proxmox/status/{product}', [App\Extensions\Servers\Proxmox\Proxmox::class, 'status'])->name('extensions.proxmox.status');
 
-Route::post('/proxmox/configure/{id}', [App\Extensions\Servers\Proxmox\Proxmox::class, 'configure'])->name('extensions.proxmox.configure');
+Route::post('/proxmox/configure/{product}', [App\Extensions\Servers\Proxmox\Proxmox::class, 'configure'])->name('extensions.proxmox.configure');
