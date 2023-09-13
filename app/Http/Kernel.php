5b223e56d03b7828268a62707755789f5eecd9d1
Corwin <info@corwindev.nl>
2023-09-13 18:12:35 +0200

    feat: limit products per user

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,7 +57,6 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
-        'auth.admin.api' => \App\Http\Middleware\AdminAPIRequest::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
