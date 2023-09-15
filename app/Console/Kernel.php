7379fb1108f97faa501ba7aac04d379546936a4d
Mateusz Sikorski <mateusz@mscode.pl>
2023-09-15 12:28:37 +0200

    removed unused import

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Jobs\CheckUpdates;
 use App\Models\Setting;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
