088c01d832ac2b7800ae8836fe7edb5837e5725b
Mateusz Sikorski <mateusz@mscode.pl>
2023-10-07 14:09:15 +0200

    Pr 14 (UI Fixes) (#159)
    * Zindex for elements in admin menu in mobile versionD
    
    * Extension browser UI Fix

--- themes/default/views/admin/extensions/browse.blade.php
+++ themes/default/views/admin/extensions/browse.blade.php
@@ -1,7 +1,7 @@
 <x-admin-layout title="{{ __('Browse extensions') }}">
-    <div class="flex justify-between">
+    <div class="flex flex-col md:flex-row justify-center md:justify-between">
         <h1 class="text-2xl font-bold">{{ __('Browse extensions') }}</h1>
-        <input type="text" class="form-input w-1/5 h-2/3" placeholder="{{ __('Search') }}" id="extensionSearch">
+        <input type="text" class="form-input md:w-1/5 h-2/3" placeholder="{{ __('Search') }}" id="extensionSearch">
     </div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-3 mt-3">
         @foreach ($extensions as $extension)
@@ -14,38 +14,41 @@
                             <img src="{{ config('app.marketplace') . '../../storage/' . $extension->icon }}" alt="{{ $extension->name }}" class="w-[128px] h-auto rounded-md drop-shadow-lg">
                         </div>
 
-                        <div class="flex flex-col relative h-full w-full">
+                        <div class="flex flex-col h-full w-full">
                             <h1 class="text-xl font-bold">{{ $extension->name }}</h1>
                             <span>{{ $extension->slogan }}</span>
-                            <div class="absolute bottom-0 w-full">
-                                <div class="flex flex-row justify-between items-center relative w-full">
-                                    <div>
+                            <div class="relative mt-12 h-full w-full">
+                                <div class="absolute bottom-0 w-full">
+                                    <div class="flex flex-row justify-between items-center relative w-full">
                                         <div>
-                                            <span class="font-semibold">{{ __('Price') }}: </span>
-                                            {{ $extension->price == 0?"Free":$extension->price . "$" }}
+                                            <div>
+                                                <span class="font-semibold">{{ __('Price') }}: </span>
+                                                {{ $extension->price == 0?"Free":$extension->price . "$" }}
+                                            </div>
+                                            <div>
+                                                <span class="font-semibold">{{ __('Type') }}: </span>
+                                                {{ ucFirst($extension->type) }}
+                                            </div>
                                         </div>
-                                        <div>
-                                            <span class="font-semibold">{{ __('Type') }}: </span>
-                                            {{ ucFirst($extension->type) }}
-                                        </div>
+                                        @if ($extension->price == 0)
+                                            <div class="flex h-full">
+                                                <form action="{{ route('admin.extensions.install', $extension->id) }}" method="POST">
+                                                    @csrf
+                                                    <button type="submit" class="button button-primary">
+                                                        <i class="ri-download-2-line text-xs"></i>
+                                                        <span class="hidden 2xl:flex">{{ __('Download') }}</span>
+                                                    </button>
+                                                </form>
+                                            </div>
+                                        @else
+                                            <div class="flex h-full">
+                                                <a href="{{ config('app.marketplace') . '/extension/' . $extension->id  . '/' . $extension->name }}" class="button button-primary">
+                                                    <i class="ri-wallet-3-fill text-xs"></i>
+                                                    <span class="hidden 2xl:flex">{{ __('Buy') }}</span>
+                                                </a>
+                                            </div>
+                                        @endif
                                     </div>
-                                    @if ($extension->price == 0)
-                                        <div class="flex h-full">
-                                            <form action="{{ route('admin.extensions.install', $extension->id) }}" method="POST">
-                                                @csrf
-                                                <button type="submit" class="button button-primary">
-                                                    <i class="ri-download-2-line text-xs"></i>
-                                                    {{ __('Download') }}
-                                                </button>
-                                            </form>
-                                        </div>
-                                    @else
-                                        <div class="flex h-full">
-                                            <a href="{{ config('app.marketplace') . '/extension/' . $extension->id  . '/' . $extension->name }}" class="button button-primary">
-                                                <i class="ri-wallet-3-fill text-xs"></i> {{ __('Buy') }}
-                                            </a>
-                                        </div>
-                                    @endif
                                 </div>
                             </div>
                         </div>
