765f3d7281bf0cffa29eaaf1b433772f7490e214
Mateusz <mateusz@mscode.pl>
2023-10-30 16:01:16 +0100

    Scrollable tickets (Client side)

--- themes/default/views/clients/tickets/show.blade.php
+++ themes/default/views/clients/tickets/show.blade.php
@@ -1,20 +1,12 @@
 <x-app-layout clients title="{{ __('Ticket') }}">
-    <!-- show last messages and form to reply -->
-    <div class="py-12">
-        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg dark:bg-secondary-100 dark:border-secondary-300">
-                <div class="p-6 bg-white sm:px-20 dark:bg-secondary-100">
-                    <x-success class="mt-4" />
-                    <h1 class="text-2xl font-bold text-gray-500 dark:text-darkmodetext">{{__('Ticket')}} #{{ $ticket->id }}
-                    </h1>
-                    <div class="grid grid-cols-1 gap-4">
-                        <div class="mt-3 text-gray-500 dark:text-darkmodetext dark:bg-secondary-100">
-                            <div class="flex flex-row gap-x-4 items-baseline">
-                                <x-input disabled type="text" id="title" :label="__('Subject')" name="title" value="{{ $ticket->title }}"
-                                         required class="mt-2 w-full" icon="ri-pencil-line" />
-
-                                <x-input disabled type="text" name="status" :label="__('Status')" icon="ri-bar-chart-line" class="mt-2 w-full" value="{{ ucfirst($ticket->status) }}"/>
-                            </div>
+        <!-- show last messages and form to reply -->
+        <div class="py-3 sm:py-12">
+            <div class="mx-auto max-w-7xl sm:px-6 lg:px-5">
+                <div class="overflow-hidden bg-white shadow-xl sm:rounded-lg dark:bg-secondary-100 dark:border-secondary-300">
+                    <div class="p-2 bg-white sm:px-10 dark:bg-secondary-100">
+                        <x-success class="mt-4" />
+                        <div class="justify-between flex flex-row items-center">
+                            <h1 class="text-2xl font-bold text-gray-500 dark:text-darkmodetext">{{__('Ticket')}} #{{ $ticket->id }}</h1>
                             @if($ticket->status !== 'closed')
                                 <form action="{{ route('clients.tickets.close', $ticket->id) }} " method="POST" class="mt-3">
                                     @csrf
@@ -22,30 +14,39 @@
                                 </form>
                             @endif
                         </div>
-                    </div>
-                </div>
+                        <div class="grid grid-cols-1 gap-4">
+                            <div class="text-gray-500 dark:text-darkmodetext dark:bg-secondary-100">
+                                <div class="flex flex-row gap-x-4 items-baseline">
+                                    <x-input disabled type="text" id="title" :label="__('Subject')" name="title" value="{{ $ticket->title }}"
+                                             required class="mt-2 w-full" icon="ri-pencil-line" />
 
-                @php $messages = $ticket->messages()->with('user')->get(); @endphp
-                @empty($messages)
-                    <div class="ml-10 flex items-baseline ">
-                        <p class="dark:text-darkmodetext text-gray-600 px-3 rounded-md text-xl m-4">
-                            {{ __('No messages yet') }}
-                        </p>
+                                    <x-input disabled type="text" name="status" :label="__('Status')" icon="ri-bar-chart-line" class="mt-2 w-full" value="{{ ucfirst($ticket->status) }}"/>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                @else
-                    <div class="p-6 bg-white sm:px-20 border-b border-gray-200 dark:bg-secondary-100 dark:border-secondary-300">
-                        <div class="mt-6 dark:bg-secondary-100">
-                            <div class="grid grid-cols-3 gap-4">
 
-                                @foreach ($messages as $message)
-                                    @if ($message->user_id == Auth::user()->id)
-                                        <div class="col-span-1"></div>
-                                        <div class="w-full col-span-2" id="message">
-                                            <div class="grid grid-cols-12 place-items-end">
-                                                <div class="col-span-11">
-                                                    <div class="text-sm flex justify-end font-normal text-opacity-50 mr-4">({{$message->messageDate()}}) {{__('You')}}</div>
-                                                    <div class="w-full flex justify-end">
-                                                        <div class="max-w-fit text-gray-100 break-all dark:text-white-400 rounded-2xl bg-primary-400 p-2 px-4 mr-2">
+    @php $messages = $ticket->messages()->with('user')->get(); @endphp
+                    @empty($messages)
+                        <div class="ml-10 flex items-baseline ">
+                            <p class="dark:text-darkmodetext text-gray-600 px-3 rounded-md text-xl m-4">
+                                {{ __('No messages yet') }}
+                            </p>
+                        </div>
+                    @else
+                        <div class="md:p-6 bg-white sm:px-20 border-b border-gray-200 dark:bg-secondary-100 dark:border-secondary-300">
+                            <div class="mt-6">
+                                <div class="grid grid-cols-3 md:gap-4 max-h-[45vh] overflow-y-auto" id="content">
+
+                                    @foreach ($messages as $message)
+                                        @if ($message->user_id == Auth::user()->id)
+                                            <div class="md:col-span-1"></div>
+                                            <div class="w-full col-span-3 md:col-span-2" id="message">
+                                                <div class="grid grid-cols-12 place-items-end">
+                                                    <div class="col-span-10 md:col-span-11">
+                                                        <div class="text-sm flex justify-end font-normal text-opacity-50 mr-4">({{$message->messageDate()}}) {{__('You')}}</div>
+                                                        <div class="w-full flex justify-end">
+                                                            <div class="max-w-fit text-gray-100 break-all dark:text-white-400 rounded-2xl bg-primary-400 p-2 px-4 mr-2">
                                                 <span class="text-end" style="color: white !important;">
                                                     {!! Str::Markdown(str_replace("\n", "  \n", $message->message), ['html_input' => 'escape']) !!}
 
@@ -80,27 +81,27 @@
                                                         </div>
                                                     @endif
                                                 </span>
+                                                            </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="justify-start w-full h-full flex col-span-1">
-                                                    <img src="https://www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=200&d=mp"
-                                                         class="h-10 w-10 mt-6 shadow-md rounded-full bg-secondary-200 inline-block"
-                                                         onerror='this.error=null;this.src="https://d33wubrfki0l68.cloudfront.net/c0e8a3c6172bd5bebfe787d49974adcff1ec4d3a/ca6a2/img/people/joseph-jolton.png";' alt="">
+                                                    <div class="justify-start w-full h-full flex col-span-2 md:col-span-1">
+                                                        <img src="https://www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=200&d=mp"
+                                                             class="h-10 w-10 mt-6 shadow-md rounded-full bg-secondary-200 inline-block"
+                                                             onerror='this.error=null;this.src="https://d33wubrfki0l68.cloudfront.net/c0e8a3c6172bd5bebfe787d49974adcff1ec4d3a/ca6a2/img/people/joseph-jolton.png";' alt="">
+                                                    </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    @elseif ($message->user_id !== Auth::user()->id)
-                                        <div class="w-full col-span-2" id="message">
-                                            <div class="grid grid-cols-12 max-w-full">
-                                                <div class="justify-end w-full flex col-span-1">
-                                                    <img src="https://www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=200&d=mp"
-                                                         class="h-10 w-10 mt-6 shadow-md rounded-full bg-secondary-200 inline-block"
-                                                         onerror='this.error=null;this.src="https://d33wubrfki0l68.cloudfront.net/c0e8a3c6172bd5bebfe787d49974adcff1ec4d3a/ca6a2/img/people/joseph-jolton.png";' alt="">
-                                                </div>
-                                                <div class="col-span-11">
-                                                    <span class="text-sm ml-5 font-normal text-opacity-50">{{$message->user->name}} ({{$message->messageDate()}})</span>
-                                                    <div class="my-auto text-gray-500 break-all dark:text-darkmodetext ml-2 w-fit rounded-2xl bg-gray-200 dark:bg-darkmode p-2 px-4">
+                                        @elseif ($message->user_id !== Auth::user()->id)
+                                            <div class="w-full col-span-3 md:col-span-2" id="message">
+                                                <div class="grid grid-cols-12 max-w-full">
+                                                    <div class="justify-end w-full flex col-span-2 md:col-span-1">
+                                                        <img src="https://www.gravatar.com/avatar/{{ md5($message->user->email) }}?s=200&d=mp"
+                                                             class="h-10 w-10 mt-6 shadow-md rounded-full bg-secondary-200 inline-block"
+                                                             onerror='this.error=null;this.src="https://d33wubrfki0l68.cloudfront.net/c0e8a3c6172bd5bebfe787d49974adcff1ec4d3a/ca6a2/img/people/joseph-jolton.png";' alt="">
+                                                    </div>
+                                                    <div class="col-span-10">
+                                                        <span class="text-sm ml-5 font-normal text-opacity-50">{{$message->user->name}} ({{$message->messageDate()}})</span>
+                                                        <div class="my-auto text-gray-500 break-all dark:text-darkmodetext ml-2 w-fit rounded-2xl bg-gray-200 dark:bg-darkmode p-2 px-4">
                                             <span class="text-end" style="color: white !important;">
                                                     {!! Str::Markdown(str_replace("\n", "  \n", $message->message), ['html_input' => 'escape']) !!}
 
@@ -131,63 +132,66 @@
                                                         </div>
                                                 @endif
                                                 </span>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
 
-                                        <div class="col-span-1"></div>
-                                    @endif
-                                @endforeach
+                                            <div class="md:col-span-1"></div>
+                                        @endif
+                                    @endforeach
 
-                            </div>
-                        </div>
-                    </div>
-                @endempty
-            <div class="overflow-hidden bg-white shadow-xl sm:rounded-b-lg dark:bg-secondary-100">
-                @if($ticket->status == "closed")
-                    <div class="p-6 text-center text-red-500 bg-white sm:px-20 dark:bg-secondary-100 mx-auto my-auto">
-                        {{__('The ticket has been closed, you cannot respond.')}}
-                    </div>
-                @else
-                    <form method="POST" action="{{ route('clients.tickets.reply', $ticket->id) }}" class="mt-10" id="reply" enctype="multipart/form-data">
-                        @csrf
-                        <div class="bg-white mb-5 border-gray-200 sm:px-20 dark:bg-secondary-100 dark:border-black mt-5">
-                            <h1 class="text-xl text-gray-500 dark:text-darkmodetext font-bold">{{ __('Reply') }}</h1>
-                            <div class="grid grid-cols-1 gap-4">
-                                <div class="mt-3 text-gray-500 dark:text-darkmodetext dark:bg-secondary-100">
-
-                                    <div id="attachments-list" class="flex flex-row gap-x-4 mb-3"></div>
-
-                                    <div class="flex flex-row">
-                                        <label for="attachments" class="button-secondary rounded-full cursor-pointer flex w-10 h-10 mr-2 transition-all ease-in-out">
-                                            <i class="ri-add-line my-auto mx-auto"></i>
-                                        </label>
-                                        <textarea
-                                            id="message"
-                                            class="block my-auto w-full rounded-2xl shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-indigo-300 dark:border-0 sm:text-sm dark:bg-secondary-200"
-                                            rows="1"
-                                            name="message"
-                                            placeholder="Aa"
-                                            required
-                                        ></textarea>
-                                        <button type="submit" id="submit-button" class="button-primary rounded-full w-10 ml-2 h-10 float-right transition-all ease-in-out">
-                                            <i class="ri-send-plane-fill"></i>
-                                        </button>
-                                    </div>
-                                    <x-input type="file" id="attachments" :label="__('Attachments')" name="attachments[]" multiple class="hidden"  />
-                                    <x-recaptcha form="reply" />
-                                    <br>
                                 </div>
                             </div>
                         </div>
-                    </form>
-                @endif
+                    @endempty
+    <br>
+    <div class="overflow-hidden">
+        <form method="POST" action="{{ route('admin.tickets.reply', $ticket->id) }}"id="reply" enctype="multipart/form-data">
+            @csrf
+            <div class="bg-white mb-3 border-gray-200 sm:px-20 dark:bg-secondary-100 dark:border-black mt-2">
+                <h1 class="text-xl text-gray-500 dark:text-darkmodetext font-bold">{{ __('Reply') }}</h1>
+                <div class="grid grid-cols-1 gap-4">
+                    <div class="mt-3 text-gray-500 dark:text-darkmodetext dark:bg-secondary-100">
+
+                        <div id="attachments-list" class="flex flex-row gap-x-4 mb-3"></div>
+
+                        <div class="flex flex-row">
+                            <label for="attachments" class="button-secondary rounded-full cursor-pointer flex w-10 h-10 mr-2 transition-all ease-in-out">
+                                <i class="ri-add-line my-auto mx-auto"></i>
+                            </label>
+                            <textarea
+                                id="message"
+                                class="block my-auto w-full rounded-2xl shadow-sm focus:ring-indigo-500 focus:border-indigo-500 border-indigo-300 dark:border-0 sm:text-sm dark:bg-secondary-200"
+                                rows="1"
+                                name="message"
+                                placeholder="Aa"
+                                required
+                            ></textarea>
+                            <button type="submit" id="submit-button" class="button-primary rounded-full w-10 ml-2 h-10 float-right transition-all ease-in-out">
+                                <i class="ri-send-plane-fill"></i>
+                            </button>
+                        </div>
+                        <x-input type="file" id="attachments" :label="__('Attachments')" name="attachments[]" multiple class="hidden"  />
+                        <x-recaptcha form="reply" />
+                        <br>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
+        </form>
     </div>
     <script>
+
+        function slideDown() {
+            const content = document.getElementById('content');
+            const contentHeight = content.scrollHeight;
+            content.scroll(0, contentHeight);
+
+        }
+
+        // Uruchom funkcję po załadowaniu strony
+        window.addEventListener('load', slideDown);
+
         const fileInput = document.getElementById('attachments');
         const fileList = document.getElementById('attachments-list');
         const submitButton = document.getElementById('submit-button');
@@ -256,7 +260,6 @@
                 fileInput.files = fileListArray.files;
             }
 
-            console.log('Wartość inputu "file" ustawiona na wcześniej wybrane pliki.');
         });
 
         const tx = document.getElementsByTagName("textarea");
