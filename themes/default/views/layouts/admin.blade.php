4df4d49417739bbe1fc651ce14512b394da5cfb1
Vikas Dongre <47200577+zvikasdongre@users.noreply.github.com>
2023-12-01 19:36:52 +0530

    refactor: improve theme support (#269)
    * refactor: improve theme support
    Moves vite & tailwind configs and theme's css & js into theme's directory.
    
    * feat: add p:make-theme command
    And Fixed some bugs.
    
    * fix: git ignore default theme's built files.
    
    * style: made vite assets line more compact
    
    * fix: npm build and dev scripts
    
    * refactor: merge dev and build tool files
    
    * fix: move vite script out of tools dir
    
    * fix: change scripts in MakeTheme command
     and fix script's path

--- themes/default/views/layouts/admin.blade.php
+++ themes/default/views/layouts/admin.blade.php
@@ -9,7 +9,9 @@
 
     <title>{{ 'Admin - ' . $title }}</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
-    @vite(['resources/js/app.js', 'resources/css/app.css'])
+
+    @vite(['themes/' . config('settings::theme-active') . '/js/app.js', 'themes/' . config('settings::theme-active') . '/css/app.css'], config('settings::theme-active'))
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@3.9.1/dist/chart.min.js"></script>
     @if (config('settings::app_logo'))
