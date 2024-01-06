be751a169f6f0acd88f21f2d1a935fe8b9fff1d6
ProgyNL <53907616+ProgyNL@users.noreply.github.com>
2024-01-06 23:59:54 +0100

    Turkish language
    tr.json, auth.php, pagination.php password.php and validaiton.php

---
+++ lang/tr/passwords.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Password Reset Language Lines
+|--------------------------------------------------------------------------
+|
+| The following language lines are the default lines which match reasons
+| that are given by the password broker for a password update attempt
+| has failed, such as for an invalid token or invalid new password.
+|
+*/
+
+return [
+    'reset'     => 'Parolanız başarıyla sıfırlandı!',
+    'sent'      => 'Parola sıfırlama bileti e-posta ile gönderildi!',
+    'throttled' => 'Lütfen tekrar denemeden önce bekleyin.',
+    'token'     => 'Parola sıfırlama bileti geçersiz.',
+    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunmuyor.',
+];
