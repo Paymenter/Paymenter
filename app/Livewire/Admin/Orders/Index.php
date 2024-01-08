f440da290acc3aefd86ac9eb9b8e96f959a94523
NoPantsMcDance <nopantsmcdance@gmail.com>
2024-01-08 03:42:01 -0500

    Change default sort order to latest ID (#330)
    * Update Index.php
    
    Change default sort order to latest ID
    
    * Update Index.php
    
    Change default sort order to latest ID
    
    * Update Index.php
    
    Change default sort order to latest ID
    
    * Update Index.php
    
    Corwin wants it like this
    
    * Update Index.php
    
    remove space

--- app/Livewire/Admin/Orders/Index.php
+++ app/Livewire/Admin/Orders/Index.php
@@ -26,7 +26,8 @@ class Index extends DataTableComponent
 
     public function configure(): void
     {
-        $this->setPrimaryKey('id')->setTableRowUrl(fn ($row) => route('admin.orders.show', $row));
+        $this->setPrimaryKey('id')->setTableRowUrl(fn ($row) => route('admin.orders.show', $row))
+        ->setDefaultSort('id', 'desc');
     }
 
     public function deleteOrders(): void
