413c5759b3eb4aeb63f2a6af83991a6621814456
Corwin <info@corwindev.nl>
2024-01-23 13:15:13 +0100

    feat: thrown a _blank to footer notice

--- themes/default/views/components/footer.blade.php
+++ themes/default/views/components/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="pt-5 pb-3 mt-auto">
     <div class="text-sm text-center content text-secondary-600">
-        <a href="https://paymenter.org">
+        <a href="https://paymenter.org" target="_blank">
             Paymenter &copy; 2022 - {{ date('Y') }}
         </a>
     </div>
