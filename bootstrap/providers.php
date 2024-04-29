287fcd59534ae088c535b22293fc94fb7ba02351
Vikas Dongre <zvikasdongre@gmail.com>
2024-04-29 13:58:48 +0530

    feat: add filament admin panel

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,5 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\SettingsProvider::class,
 ];
--- routes/web.php
+++ routes/web.php
@@ -32,15 +32,15 @@ Route::group(['middleware' => ['web', 'auth']], function () {
     })->name('profile');
 });
 
-Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
-    Route::get('/', Admin\Index::class)->name('index')->middleware('has:admin.dashboard.view');
-    Route::get('settings', Admin\Configuration\Settings::class)->name('settings')->middleware('has:admin.settings.view');
-    Route::get('health', Admin\Configuration\Health::class)->name('health')->middleware('has:admin.health.view');
+// Route::group(['middleware' => ['web', 'auth'], 'prefix' => 'admin', 'as' => 'admin.'], function () {
+//     Route::get('/', Admin\Index::class)->name('index')->middleware('has:admin.dashboard.view');
+//     Route::get('settings', Admin\Configuration\Settings::class)->name('settings')->middleware('has:admin.settings.view');
+//     Route::get('health', Admin\Configuration\Health::class)->name('health')->middleware('has:admin.health.view');
 
-    Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
-        Route::get('/', Admin\Users\Index::class)->name('index')->middleware('has:admin.users.view');
-        // Route::get('create', Admin\Users\Create::class)->name('create')->middleware('has:admin.users.create');
-        Route::get('{user}', Admin\Users\Show::class)->name('show')->middleware('has:admin.users.view');
-        Route::get('{user}/edit', Admin\Users\Edit::class)->name('edit')->middleware('has:admin.users.edit');
-    });
-});
+//     Route::group(['prefix' => 'users', 'as' => 'users.'], function () {
+//         Route::get('/', Admin\Users\Index::class)->name('index')->middleware('has:admin.users.view');
+//         // Route::get('create', Admin\Users\Create::class)->name('create')->middleware('has:admin.users.create');
+//         Route::get('{user}', Admin\Users\Show::class)->name('show')->middleware('has:admin.users.view');
+//         Route::get('{user}/edit', Admin\Users\Edit::class)->name('edit')->middleware('has:admin.users.edit');
+//     });
+// });
