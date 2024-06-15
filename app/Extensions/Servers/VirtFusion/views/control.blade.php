fa7e4777ab18e0afcde24bf145975a7d75929b10
Aubrey Yang <i@aub.ooo>
2024-06-15 17:47:52 +0900

    Resolve issue with multiple IP addresses not displaying and IPv6 showing as 'N/A'. (#454)
    * Fix 0.8.0 PayPal not working
    
    * Fix Virtfusion Extension Multi IP not display bugs.
    
    Resolve issue with multiple IP addresses not displaying and IPv6 showing as 'N/A'.
    
    * Update control.blade.php
    
    optimistic more elegant way to handle PHP in Laravel

--- app/Extensions/Servers/VirtFusion/views/control.blade.php
+++ app/Extensions/Servers/VirtFusion/views/control.blade.php
@@ -5,33 +5,62 @@
             <div class="font-bold">Hostname:</div>
             <div class="font-bold">Ram:</div>
             <div class="font-bold">CPU:</div>
-        </div>
-        <div class="flex-col ml-4">
-            <div>@empty($details->name) N/A @else {{ $details->name }} @endempty</div>
-            <div>{{ $details->hostname ?? 'N/A'}}</div>
-            <div>{{ $details->settings['resources']['memory'] ?? 'N/A'}} MB</div>
-            <div>{{ $details->settings['resources']['cpuCores'] ?? 'N/A' }} @if($details->settings['resources']['cpuCores'] > 1)cores @else core @endif</div>
-        </div>
-    </div>
-    <div class="flex">
-        <div class="flex-col">
-            <div class="font-bold">IPv4:</div>
-            <div class="font-bold">IPv6:</div>
             <div class="font-bold">Storage:</div>
             <div class="font-bold">Traffic:</div>
         </div>
         <div class="flex-col ml-4">
-            <div>{{ $details->network['interfaces'][0]['ipv4'][0]['address'] ?? 'N/A' }}</div>
-            <div>{{ $details->network['interfaces'][0]['ipv6'][0]['address'] ?? 'N/A' }}</div>  
+            <div>@empty($details->name)
+                    N/A
+                @else
+                    {{ $details->name }}
+                @endempty</div>
+            <div>{{ $details->hostname ?? 'N/A'}}</div>
+            <div>{{ $details->settings['resources']['memory'] ?? 'N/A'}} MB</div>
+            <div>{{ $details->settings['resources']['cpuCores'] ?? 'N/A' }} @if($details->settings['resources']['cpuCores'] > 1)
+                    cores
+                @else
+                    core
+                @endif</div>
             <div>{{ $details->settings['resources']['storage'] ?? 'N/A' }} GB</div>
             <div>{{ $details->settings['resources']['traffic'] ?? 'N/A'}} GB</div>
         </div>
     </div>
+    <div class="flex">
 
-    
+        <div class="flex-col ml-4">
+        @php
+            $ipv4_addresses = array_map(function ($ipv4) {
+                return $ipv4['address'] ?? 'N/A';
+            }, $details->network['interfaces'][0]['ipv4']);
+            $ipv6_addresses = [];
+            foreach ($details->network['interfaces'][0]['ipv6'] as $ipv6) {
+                if (!empty($ipv6['addresses'])) {
+                    $ipv6_addresses = array_merge($ipv6_addresses, $ipv6['addresses']);
+                }
+            }
+            $ipv4_string = implode('<br/>', $ipv4_addresses);
+            $ipv6_string = implode('<br/>', $ipv6_addresses);
+        @endphp
+
+        <div>
+            <p>
+            <div class="font-bold">IPv4:</div>
+                {!! $ipv4_string !!}</p>
+        </div>
+        <div>
+            <p>
+            <div class="font-bold">IPv6:</div>
+                {!! $ipv6_string !!}</p>
+        </div>
+
+        </div>
+    </div>
+
 </div>
 
-<p class="mt-8">Manage your server via our dedicated control panel. You will be automatically authenticated and the control panel will open in a new tab.</p>
-<a class="button button-primary mt-2" href="{{ route('extensions.virtfusion.login', $orderProduct->id) }}" target="_blank">
+<p class="mt-8">Manage your server via our dedicated control panel. You will be automatically authenticated and the
+    control panel will open in a new tab.</p>
+<a class="button button-primary mt-2" href="{{ route('extensions.virtfusion.login', $orderProduct->id) }}"
+   target="_blank">
     Login to control panel
 </a>
