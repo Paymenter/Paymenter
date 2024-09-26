ded2e7004c34020bbfc36eb4a7127ec1bd67d719
CorwinDev <CorwinDev@users.noreply.github.com>
2024-09-26 19:43:00 +0000

    style: linted files with pint

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -50,7 +50,7 @@ class Service extends Model
     public function currency(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->order->currency
+            get: fn () => $this->order->currency
         );
     }
 
@@ -62,7 +62,7 @@ class Service extends Model
     public function formattedPrice(): Attribute
     {
         return Attribute::make(
-            get: fn() => new Price(['price' => $this->price * $this->quantity, 'currency' => $this->order->currency])
+            get: fn () => new Price(['price' => $this->price * $this->quantity, 'currency' => $this->order->currency])
         );
     }
 
@@ -73,14 +73,14 @@ class Service extends Model
     {
         if ($this->plan->type == 'free' || $this->plan->type == 'one-time') {
             return Attribute::make(
-                get: fn() => $this->product->name
+                get: fn () => $this->product->name
             );
         }
         $date = $this->expires_at ?? now();
         $endDate = $date->{'add' . ucfirst($this->plan->billing_unit) . 's'}($this->plan->billing_period);
 
         return Attribute::make(
-            get: fn() => $this->product->name . ' (' . $date->format('M d, Y') . ' - ' . $endDate->format('M d, Y') . ')'
+            get: fn () => $this->product->name . ' (' . $date->format('M d, Y') . ' - ' . $endDate->format('M d, Y') . ')'
         );
     }
 
@@ -140,14 +140,14 @@ class Service extends Model
     public function cancellable(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->status !== 'cancelled' && $this->plan->type != 'free' && $this->plan->type != 'onetime'
+            get: fn () => $this->status !== 'cancelled' && $this->plan->type != 'free' && $this->plan->type != 'onetime'
         );
     }
 
     public function upgradable(): Attribute
     {
         return Attribute::make(
-            get: fn() => $this->product->upgrades()->count() > 0 && $this->status == 'active'
+            get: fn () => $this->product->upgrades()->count() > 0 && $this->status == 'active'
         );
     }
 
@@ -158,8 +158,10 @@ class Service extends Model
             // Only get the upgrades that have the exact same billing cycle as the service
             if ($plan->count() > 0) {
                 $product->plan = $plan->first();
+
                 return $product;
             }
+
             return null;
         });
     }
