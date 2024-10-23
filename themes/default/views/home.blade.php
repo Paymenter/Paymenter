f460b0020dc0628fe9151f2ad29df028ee6483cd
Corwin <info@corwindev.nl>
2024-10-23 20:45:20 +0200

    feat: add homepage hook

--- themes/default/views/home.blade.php
+++ themes/default/views/home.blade.php
@@ -1,7 +1,21 @@
-<x-app-layout>
-    <x-center>
-        <h1 class="mt-10">Hello World</h1>
-
-    </x-center>
-
-</x-app-layout>
+<div>
+    <div class="flex flex-col gap-6">
+        <div class="mx-auto container bg-primary-800 p-4 rounded-md">
+            <article class="prose prose-invert">
+                {!! Str::markdown(theme('home_page_text', 'Welcome to Paymenter'), [
+                'html_input' => 'strip',
+                'allow_unsafe_links' => false,
+                ]) !!}
+            </article>
+        </div>
+        <div class="mx-auto container bg-primary-800 p-4 rounded-md">
+            <h1 class="text-2xl font-semibold text-white">Products</h1>
+            @foreach ($categories as $category)
+                
+                
+                
+            @endforeach
+        </div>
+    </div>
+    {!! hook('pages.home') !!}
+</div>
