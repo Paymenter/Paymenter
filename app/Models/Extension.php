94cb0ac5f2891a0b0a55834dd07de8abdc3a0d0c
CorwinDev <CorwinDev@users.noreply.github.com>
2024-10-29 20:42:45 +0000

    style: linted files with pint

--- app/Models/Extension.php
+++ app/Models/Extension.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Artisan;
 
 class Extension extends Model
 {
