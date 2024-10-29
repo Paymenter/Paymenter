fac81a15c79a6fff79d1e74c3bfcb27ed063d066
Corwin <info@corwindev.nl>
2024-10-29 16:12:57 +0100

    feat: add sponsors button to admin footer

--- themes/default/views/components/admin-footer.blade.php
+++ themes/default/views/components/admin-footer.blade.php
@@ -1,4 +1,8 @@
-<p class="text-xs text-gray-400 mt-auto">
-    Powered by <a href="https://paymenter.org" target="_blank" class="text-gray-400">Paymenter &copy;
-        {{ date('Y') }}</a>
-</p>
+<div class="flex flex-col gap-2">
+    <p class="text-xs text-gray-400 mt-auto">
+        Powered by <a href="https://paymenter.org" target="_blank" class="text-gray-400">Paymenter &copy;
+            {{ date('Y') }}</a>
+    </p>
+    <iframe src="https://github.com/sponsors/Paymenter/button" title="Sponsor Paymenter" height="32" width="114"
+        style="border: 0; border-radius: 6px;"></iframe>
+</div>
