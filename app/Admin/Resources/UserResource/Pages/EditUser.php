3d5303b25f2dc3219f15e8b6bed18e3a03b5b3e9
Sarthak Jain <88922662+Sarthak-07@users.noreply.github.com>
2024-10-31 00:18:15 +0530

    feat: you cannot delete yourself

--- app/Admin/Resources/UserResource/Pages/EditUser.php
+++ app/Admin/Resources/UserResource/Pages/EditUser.php
@@ -13,7 +13,8 @@ class EditUser extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            Actions\DeleteAction::make()
+                ->hidden(fn ($record) => auth()->user ()->id === $record->id),
         ];
     }
 }
