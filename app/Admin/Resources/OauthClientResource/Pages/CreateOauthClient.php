cd28c212ae25520cdb7dd9bc7a9c582ed84f1dff
Corwin <info@corwindev.nl>
2024-10-30 21:02:41 +0100

    fix(oauth): generate ssl key if not exists

--- app/Admin/Resources/OauthClientResource/Pages/CreateOauthClient.php
+++ app/Admin/Resources/OauthClientResource/Pages/CreateOauthClient.php
@@ -5,8 +5,11 @@ namespace App\Admin\Resources\OauthClientResource\Pages;
 use App\Admin\Resources\OauthClientResource;
 use Filament\Resources\Pages\CreateRecord;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Str;
 use Laravel\Passport\ClientRepository;
+use phpseclib3\Crypt\RSA;
+use Laravel\Passport\Passport;
 
 class CreateOauthClient extends CreateRecord
 {
@@ -14,16 +17,26 @@ class CreateOauthClient extends CreateRecord
 
     protected function handleRecordCreation(array $data): Model
     {
-        $data = array_merge($data, [
-            'secret' => Str::random(40),
-            'personal_access_client' => false,
-            'password_client' => false,
-            'revoked' => false,
-        ]);
-        $record = static::getModel()::create($data);
+        [$publicKey, $privateKey] = [
+            Passport::keyPath('oauth-public.key'),
+            Passport::keyPath('oauth-private.key'),
+        ];
+        // Do the encryption keys exist?
+        // Read storage/oauth-private.key and storage/oauth-public.key
+        if (!file_exists($publicKey) && !file_exists($privateKey)) {
+            $key = RSA::createKey(4096);
+
+            file_put_contents($publicKey, (string) $key->getPublicKey());
+            file_put_contents($privateKey, (string) $key);
+
+            if (! windows_os()) {
+                chmod($publicKey, 0660);
+                chmod($privateKey, 0600);
+            }
+        }
 
         $clientRepository = new ClientRepository;
-        $clientRepository->create(null, $record->name, $record->redirect);
+        $record = $clientRepository->create(null, $data['name'], $data['redirect']);
 
         return $record;
     }
