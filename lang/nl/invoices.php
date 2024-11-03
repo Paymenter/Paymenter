be8da8c5150fc4ce3d218aeb00b43e9cc7146d30
Corwin <info@corwindev.nl>
2024-11-03 17:53:06 +0100

    New translations invoices.php (Dutch)

---
+++ lang/nl/invoices.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'invoices' => 'Invoices',
+    'product' => 'Product',
+    'price' => 'Price',
+    'status' => 'Status',
+    'actions' => 'Actions',
+    'view' => 'View',
+    'id' => 'ID',
+    'total' => 'Total',
+    'invoice' => 'Invoice #:id',
+    'unit_price' => 'Unit Price',
+
+    'invoice_date' => 'Invoice Date',
+    'invoice_no' => 'Invoice No',
+    'item' => 'Item',
+    'quantity' => 'Quantity',
+    'transaction_id' => 'Transaction ID',
+    'payment_date' => 'Payment Date',
+    'payment_method' => 'Payment Method',
+    'amount' => 'Amount',
+];
