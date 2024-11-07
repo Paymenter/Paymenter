6717f74c1d42fac78cc61956008611da7bcec5cb
Corwin <info@corwindev.nl>
2024-11-07 21:47:18 +0100

    New translations product.php (Swedish)

---
+++ lang/sv/product.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'add_to_cart' => 'Lägg till i varukorg',
+    'view' => 'Visa',
+    'checkout' => 'Checka ut',
+    'remove' => 'Ta bort',
+    'empty_cart' => 'Din varukorg är tom',
+    'total_today' => 'Totalt idag',
+    'subtotal' => 'Delsumma',
+    'total' => 'Totalt',
+
+    'then_after_x' => 'Sedan efter :time',
+    'order_summary' => 'Ordersammanfattning',
+    'edit' => 'Ändra',
+
+    'apply' => 'Tillämpa',
+    'price' => 'Pris',
+    'setup_fee' => 'Installationsavgift',
+
+    'in_stock' => 'I lager',
+    'out_of_stock' => 'Produkten :product är slut i lager',
+    'user_limit' => 'Du har nått gränsen för :product',
+];
