0691a260e22c6986e2fdcbbdca5af5e4564f56ee
Corwin <info@corwindev.nl>
2024-11-07 19:40:17 +0100

    New translations account.php (Ukrainian)

---
+++ lang/uk/account.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'personal_details' => 'Особисті дані',
+    'security' => 'Безпека',
+
+    'change_password' => 'Змінити пароль',
+
+    'two_factor_authentication' => 'Двофакторна автентифікація',
+    'two_factor_authentication_description' => 'Додайте додатковий рівень безпеки до вашого облікового запису, ввімкнувши двофакторну автентифікацію.',
+    'two_factor_authentication_enabled' => 'Двофакторна автентифікація ввімкнена для вашого облікового запису.',
+    'two_factor_authentication_enable' => 'Увімкнути двофакторну автентифікацію',
+    'two_factor_authentication_disable' => 'Вимкнути двофакторну автентифікацію',
+    'two_factor_authentication_enable_description' => 'Щоб увімкнути двофакторну автентифікацію, проскануйте QR-код нижче за допомогою додатка для автентифікації, наприклад, Google Authenticator або Authy.',
+    'two_factor_authentication_qr_code' => 'Скануйте QR-код нижче у вашому додатку для автентифікації:',
+    'two_factor_authentication_secret' => 'Або введіть наступний код вручну:',
+
+    'sessions' => 'Сесії',
+    'sessions_description' => 'Керуйте та виходьте зі своїх активних сесій на інших браузерах і пристроях.',
+    'logout_sessions' => 'Вийти з цієї сесії',
+
+    'input' => [
+        'current_password' => 'Поточний пароль',
+        'current_password_placeholder' => 'Ваш поточний пароль',
+        'new_password' => 'Новий пароль',
+        'new_password_placeholder' => 'Ваш новий пароль',
+        'confirm_password' => 'Підтвердити пароль',
+        'confirm_password_placeholder' => 'Підтвердьте ваш новий пароль',
+
+        'two_factor_code' => 'Введіть код з вашого додатка для автентифікації',
+        'two_factor_code_placeholder' => 'Ваш код двофакторної автентифікації',
+    ],
+
+    'notifications' => [
+        'password_changed' => 'Пароль змінено.',
+        'password_incorrect' => 'Поточний пароль невірний.',
+        'two_factor_enabled' => 'Двофакторна автентифікація ввімкнена.',
+        'two_factor_disabled' => 'Двофакторна автентифікація вимкнена.',
+        'two_factor_code_incorrect' => 'Код невірний.',
+        'session_logged_out' => 'Сесію завершено.',
+    ],
+];
