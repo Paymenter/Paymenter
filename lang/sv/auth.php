a56f47f35e8285699ed34b1510a8884acc1389a3
Corwin <info@corwindev.nl>
2024-11-07 21:47:11 +0100

    New translations auth.php (Swedish)

---
+++ lang/sv/auth.php
@@ -0,0 +1,53 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ett konto med dessa uppgifter kunde ej hittas.',
+    'password' => 'Det angivna lösenordet är felaktigt.',
+    'throttle' => 'För många inloggningsförsök. Vänligen försök igen om :seconds sekunder.',
+
+    'sign_in' => 'Logga in',
+    'sign_in_title' => 'Logga in till ditt konto',
+    'or_sign_in_with' => 'Eller logga in med',
+    'forgot_password' => 'Glömt ditt lösenord?',
+    'dont_have_account' => 'Har du inget konto?',
+    'already_have_account' => 'Har du redan ett konto?',
+
+    'sign_up' => 'Registrera dig',
+    'sign_up_title' => 'Skapa ditt konto',
+
+    'logout' => 'Logga ut',
+
+    'input' => [
+        'email' => 'E-post',
+        'email_label' => 'E-postadress',
+        'password' => 'Lösenord',
+    ],
+
+    'oauth' => [
+        'unverified_discord_account' => 'Ditt Discord-konto är inte verifierat.',
+        'account_not_registered' => 'Du är inte registrerad på denna webbplats.',
+    ],
+
+    'verify_2fa' => 'Verifiera 2FA',
+    'verify' => 'Verifiera',
+
+    'verification' => [
+        'notice' => 'Verifiera din e-postadress',
+        'sent' => 'En ny verifieringslänk har skickats till din e-postadress.',
+        'check_your_email' => 'Innan du fortsätter, kontrollera din e-post efter en verifieringslänk.',
+        'not_received' => 'Om du inte fick e-postmeddelandet kan du begära ett till verifieringsmail.',
+        'request_another' => 'Skicka verifieringsmail igen',
+    ],
+];
