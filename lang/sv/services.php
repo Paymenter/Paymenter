84266eed480bf8faac03f760faf5d86aa82f9c1f
Corwin <info@corwindev.nl>
2024-11-07 21:47:19 +0100

    New translations services.php (Swedish)

---
+++ lang/sv/services.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'services' => 'Tjänster',
+    'product' => 'Produkt',
+    'price' => 'Pris',
+    'status' => 'Status',
+    'name' => 'Namn',
+    'actions' => 'Åtgärder',
+    'view' => 'Visa',
+
+    'product_details' => 'Produktdetaljer',
+    'billing_cycle' => 'Faktureringsperiod',
+    'cancel' => 'Avbryt',
+    'cancellation' => 'Annullering av :service',
+    'cancel_are_you_sure' => 'Är du säker på att du vill avbryta den här tjänsten?',
+    'cancel_reason' => 'Anledning till annullering',
+    'cancel_type' => 'Annulleringstyp',
+    'cancel_immediate' => 'Avbryt omedelbart',
+    'cancel_end_of_period' => 'Avbryt i slutet av faktureringsperioden',
+    'cancel_immediate_warning' => 'När du trycker på knappen nedan kommer tjänsten att avbrytas omedelbart och du kommer inte att kunna använda den längre.',
+    'cancellation_requested' => 'Annullering begärd',
+
+    'current_plan' => 'Nuvarande Plan',
+    'new_plan' => 'Ny Plan',
+    'change_plan' => 'Ändra Plan',
+    'current_price' => 'Nuvarande pris',
+    'new_price' => 'Nytt pris',
+    'upgrade' => 'Uppgradera',
+    'upgrade_summary' => 'Sammanfattning av uppgradering',
+    'total_today' => 'Totalt idag',
+];
