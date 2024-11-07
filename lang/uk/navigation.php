109c3d5b02407c2b6ff70bc683b7c3b08382995e
Corwin <info@corwindev.nl>
2024-11-07 19:40:28 +0100

    New translations navigation.php (Ukrainian)

---
+++ lang/uk/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'shop' => 'Shop',
+    'cart' => 'Корзина',
+    'login' => 'Увійти',
+    'register' => 'Зареєструватись',
+    'account' => 'Account',
+    'dashboard' => 'Dashboard',
+    'tickets' => 'Tickets',
+    'admin' => 'Admin',
+];
