6bb558b79210681606ebe7d73c5475fb1a32b7e1
Corwin <info@corwindev.nl>
2024-11-08 14:44:09 +0100

    New translations navigation.php (Italian)

---
+++ lang/it/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'shop' => 'Shop',
+    'cart' => 'Cart',
+    'login' => 'Login',
+    'register' => 'Register',
+    'account' => 'Account',
+    'dashboard' => 'Dashboard',
+    'tickets' => 'Tickets',
+    'admin' => 'Admin',
+];
