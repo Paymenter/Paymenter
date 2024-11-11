541639ffd36ee6aad4f646dcdacdd185dd39eee8
Corwin <info@corwindev.nl>
2024-11-11 14:50:48 +0100

    New translations product.php (French)

---
+++ lang/fr/product.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'add_to_cart' => 'Ajouter au panier',
+    'view' => 'Afficher',
+    'checkout' => 'Paiement',
+    'remove' => 'Retirer',
+    'empty_cart' => 'Votre panier est vide',
+    'total_today' => 'Montant total aujourd\'hui',
+    'subtotal' => 'Sous-total',
+    'total' => 'Total',
+
+    'then_after_x' => 'Ensuite, après :time',
+    'order_summary' => 'Résumé de la commande',
+    'edit' => 'Éditer',
+
+    'apply' => 'Appliquer',
+    'price' => 'Prix',
+    'setup_fee' => 'Installation gratuite',
+
+    'in_stock' => 'En stock',
+    'out_of_stock' => 'Produit :product est en rupture de stock',
+    'user_limit' => 'Vous avez atteint la limite de :product',
+];
