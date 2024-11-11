3d6c59c9290a00797c25bd4d7945121863e08465
Corwin <info@corwindev.nl>
2024-11-11 14:50:49 +0100

    New translations services.php (French)

---
+++ lang/fr/services.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'services' => 'Services',
+    'product' => 'Produit',
+    'price' => 'Prix',
+    'status' => 'État',
+    'name' => 'Nom',
+    'actions' => 'Actions',
+    'view' => 'Afficher',
+
+    'product_details' => 'Détails du Produit',
+    'billing_cycle' => 'Mode de facturation',
+    'cancel' => 'Annuler',
+    'cancellation' => 'Annulation de :service',
+    'cancel_are_you_sure' => 'Êtes-vous sûr de vouloir annuler ce service ?',
+    'cancel_reason' => 'Raison d\'annulation',
+    'cancel_type' => 'Type d’annulation',
+    'cancel_immediate' => 'Annuler immédiatement',
+    'cancel_end_of_period' => 'Annuler à la fin de la période de facturation',
+    'cancel_immediate_warning' => 'Lorsque vous appuyez sur le bouton ci-dessous, le service sera annulé immédiatement et vous ne pourrez plus l\'utiliser.',
+    'cancellation_requested' => 'Annulation demandée',
+
+    'current_plan' => 'Current Plan',
+    'new_plan' => 'New Plan',
+    'change_plan' => 'Change Plan',
+    'current_price' => 'Current Price',
+    'new_price' => 'New Price',
+    'upgrade' => 'Upgrade',
+    'upgrade_summary' => 'Upgrade Summary',
+    'total_today' => 'Total Today',
+];
