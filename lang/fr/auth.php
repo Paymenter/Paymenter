27bc8bacb03d999fced5f5b66da496d710635740
Corwin <info@corwindev.nl>
2024-11-11 14:50:41 +0100

    New translations auth.php (French)

---
+++ lang/fr/auth.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Ces identifiants n\'existent pas dans notre base.',
+    'password' => 'Le mot de passe saisi est incorrect.',
+    'throttle' => 'Trop de tentatives de connexion. S’il vous plaît essayer à nouveau dans :seconds secondes.',
+
+    'sign_in' => 'Connexion',
+    'sign_in_title' => 'Connectez-vous à votre compte',
+    'or_sign_in_with' => 'Ou connectez-vous avec',
+    'forgot_password' => 'Mot de passe oublié ?',
+    'dont_have_account' => 'Vous n\'avez pas de compte ?',
+    'already_have_account' => 'Vous avez déjà un compte ?',
+
+    'sign_up' => 'Inscription',
+    'sign_up_title' => 'Créez votre compte',
+
+    'logout' => 'Déconnexion',
+
+    'input' => [
+        'email' => 'E-mail',
+        'email_label' => 'Adresse mail',
+        'password' => 'Mot de passe',
+    ],
+
+    'oauth' => [
+        'unverified_discord_account' => 'Votre compte Discord n\'est pas vérifié.',
+        'account_not_registered' => 'Vous n\'êtes pas inscrit sur ce site.',
+    ],
+
+    'reset_password' => 'Réinitialiser le mot de passe',
+
+    'verify_2fa' => 'Vérifier l’authentification 2FA',
+    'verify' => 'Vérifier',
+
+    'verification' => [
+        'notice' => 'Vérifiez votre adresse e-mail',
+        'sent' => 'Un nouveau lien de vérification a été envoyé sur votre adresse e-mail.',
+        'check_your_email' => 'Before proceeding, please check your email for a verification link.',
+        'not_received' => 'If you did not receive the email you can request another verification email.',
+        'request_another' => 'Resend verification email',
+    ],
+];
