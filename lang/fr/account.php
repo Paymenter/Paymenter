f1df09b7837f915071e4bfaabac4e26571e9995e
Corwin <info@corwindev.nl>
2024-11-11 14:50:40 +0100

    New translations account.php (French)

---
+++ lang/fr/account.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'personal_details' => 'Détails personnels',
+    'security' => 'Sécurité',
+
+    'change_password' => 'Modifier le mot de passe',
+
+    'two_factor_authentication' => 'Double authentification',
+    'two_factor_authentication_description' => 'Ajoutez un niveau de sécurité supplémentaire à votre compte en activant l\'authentification la double authentification.',
+    'two_factor_authentication_enabled' => 'La double authentification est maintenant activée pour votre compte.',
+    'two_factor_authentication_enable' => 'Activer la double authentification',
+    'two_factor_authentication_disable' => 'Désactiver la double authentification',
+    'two_factor_authentication_enable_description' => 'Pour activer la double authentification, vous devez scanner le code QR ci-dessous avec une application d\'authentification comme Google Authenticator ou Authy.',
+    'two_factor_authentication_qr_code' => 'Scannez le QR code ci-dessous avec votre app d\'authentification :',
+    'two_factor_authentication_secret' => 'Ou saisissez le code suivant manuellement :',
+
+    'sessions' => 'Sessions',
+    'sessions_description' => 'Gérer et déconnecter vos sessions actives sur les autres navigateurs et appareils.',
+    'logout_sessions' => 'Déconnecter cette session',
+
+    'input' => [
+        'current_password' => 'Mot de passe actuel',
+        'current_password_placeholder' => 'Votre mot de passe actuel',
+        'new_password' => 'Nouveau mot de passe',
+        'new_password_placeholder' => 'Votre nouveau mot de passe',
+        'confirm_password' => 'Confirmation du mot de passe',
+        'confirm_password_placeholder' => 'Confirmez votre nouveau mot de passe',
+
+        'two_factor_code' => 'Entrez le code de votre application d\'authentification',
+        'two_factor_code_placeholder' => 'Votre code de double authentification',
+    ],
+
+    'notifications' => [
+        'password_changed' => 'Le mot de passe a été modifié.',
+        'password_incorrect' => 'Le mot de passe actuel est incorrect.',
+        'two_factor_enabled' => 'La double authentification a été activée.',
+        'two_factor_disabled' => 'La double authentification a été désactivée.',
+        'two_factor_code_incorrect' => 'Le code est incorrect.',
+        'session_logged_out' => 'La session a été déconnectée.',
+    ],
+];
