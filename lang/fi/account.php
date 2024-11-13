47e581b442866f3603e045e3a1a06d556fcde6a2
Corwin <info@corwindev.nl>
2024-11-13 18:31:28 +0100

    New translations account.php (Finnish)

---
+++ lang/fi/account.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+    'personal_details' => 'Henkilökohtaiset tiedot',
+    'security' => 'Turvallisuus',
+
+    'change_password' => 'Vaihda salasana',
+
+    'two_factor_authentication' => 'Kaksivaiheinen tunnistautuminen',
+    'two_factor_authentication_description' => 'Lisää ylimääräinen suojauskerros tilillesi ottamalla käyttöön kaksivaiheisen tunnistautumisen.',
+    'two_factor_authentication_enabled' => 'Kaksivaiheinen todennus on käytössä tililläsi.',
+    'two_factor_authentication_enable' => 'Ota käyttöön kaksivaiheinen tunnistautuminen',
+    'two_factor_authentication_disable' => 'Poista käytöstä kaksivaiheinen tunnistautuminen',
+    'two_factor_authentication_enable_description' => 'Ottaaksesi käyttöön kaksivaiheisen todennuksen, sinun täytyy skannata alla oleva QR-koodi autentikointisovelluksella, kuten Google Authenticator tai Authy.',
+    'two_factor_authentication_qr_code' => 'Skannaa alla oleva QR-koodi tunnistautumissovelluksellasi:',
+    'two_factor_authentication_secret' => 'Tai kirjoita seuraava koodi manuaalisesti:',
+
+    'sessions' => 'Istunnot',
+    'sessions_description' => 'Manage and log out your active sessions on other browsers and devices.',
+    'logout_sessions' => 'Log this session out',
+
+    'input' => [
+        'current_password' => 'Current password',
+        'current_password_placeholder' => 'Your current password',
+        'new_password' => 'New password',
+        'new_password_placeholder' => 'Your new password',
+        'confirm_password' => 'Confirm password',
+        'confirm_password_placeholder' => 'Confirm your new password',
+
+        'two_factor_code' => 'Enter the code from your authenticator app',
+        'two_factor_code_placeholder' => 'Your two-factor authentication code',
+    ],
+
+    'notifications' => [
+        'password_changed' => 'Password has been changed.',
+        'password_incorrect' => 'The current password is incorrect.',
+        'two_factor_enabled' => 'Two-factor authentication has been enabled.',
+        'two_factor_disabled' => 'Two-factor authentication has been disabled.',
+        'two_factor_code_incorrect' => 'The code is incorrect.',
+        'session_logged_out' => 'Session has been logged out.',
+    ],
+];
