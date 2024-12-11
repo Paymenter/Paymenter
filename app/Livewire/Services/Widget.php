263c4b9dc9f1e4be1597dcc2961f27eaa34db7b2
Darkle <tomasromero@outlook.com>
2024-12-11 03:13:43 +0100

    feat: add services widget

---
+++ app/Livewire/Services/Widget.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Livewire\Services;
+
+use App\Livewire\Component;
+use Illuminate\Support\Facades\Auth;
+use Livewire\WithPagination;
+
+class Widget extends Component
+{
+    use WithPagination;
+
+    public $status = null;
+
+    public function render()
+    {
+        $query = Auth::user()->services();
+
+        if ($this->status) {
+            $query->where('status', $this->status);
+        } else {
+            $query->where('status', '!=', 'cancelled');
+        }
+
+        return view('services.widget', [
+            'services' => $query->paginate(config('settings.pagination')),
+        ])->layoutData([
+            'title' => 'Services',
+        ]);
+    }
+}
