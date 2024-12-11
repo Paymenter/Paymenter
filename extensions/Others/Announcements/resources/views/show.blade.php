7e5f128922ecd3939604d6ae64dd3511d849a270
Darkle <tomasromero@outlook.com>
2024-12-11 03:24:09 +0100

    fix: bg color

--- extensions/Others/Announcements/resources/views/show.blade.php
+++ extensions/Others/Announcements/resources/views/show.blade.php
@@ -1,5 +1,5 @@
 <div class="mx-auto container mt-4">
-    <div class="bg-primary-800 p-4 rounded-md">
+    <div class="bg-background-secondary p-4 rounded-md">
         <div class="flex flex-row justify-between mb-6">
             <h2 class="text-xl font-bold">{{ $announcement->title }}</h2>
             <p class="text-sm text-gray-400">{{ $announcement->published_at->diffForHumans() }}</p>
