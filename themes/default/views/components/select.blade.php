ec05868c11fe9adc297d9b52e5677c99fa0caccd
Darkle <tomasromero@outlook.com>
2024-12-11 15:33:06 +0100

    feat: custom select instead of browser/system

--- themes/default/views/components/select.blade.php
+++ themes/default/views/components/select.blade.php
@@ -1,18 +1,112 @@
-<div class="relative w-full">
-    <label for="{{ $id ?? 'select' }}" class="sr-only">{{ $label ?? '' }}</label>
-    
-    <select
-        id="{{ $id ?? 'select' }}"
-        name="{{ $name ?? '' }}"
-        class="relative block w-full min-h-[38px] py-2 pl-3 pr-10 text-left bg-secondary border border-neutral rounded-md shadow-sm cursor-pointer text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-800 appearance-none"
-        {{ $attributes }}
+<div
+    x-data="{
+        selectOpen: false,
+        selectedValue: @entangle($attributes->wire('model')),
+        selectableItems: {{ json_encode($options) }},
+        selectableItemActive: null,
+        selectId: $id('select'),
+        selectDropdownPosition: 'bottom',
+
+        init() {
+            this.$watch('selectOpen', () => {
+                if (this.selectOpen) {
+                    this.selectableItemActive = this.selectableItems.find(item => item.value === this.selectedValue) 
+                        || this.selectableItems[0];
+                    this.$nextTick(() => this.selectScrollToActiveItem());
+                }
+                this.selectPositionUpdate();
+            });
+
+            window.addEventListener('resize', () => this.selectPositionUpdate());
+        },
+
+        selectableItemIsActive(item) {
+            return this.selectableItemActive && this.selectableItemActive.value === item.value;
+        },
+
+        selectableItemActiveNext() {
+            let index = this.selectableItems.indexOf(this.selectableItemActive);
+            if (index < this.selectableItems.length - 1) {
+                this.selectableItemActive = this.selectableItems[index + 1];
+                this.selectScrollToActiveItem();
+            }
+        },
+
+        selectableItemActivePrevious() {
+            let index = this.selectableItems.indexOf(this.selectableItemActive);
+            if (index > 0) {
+                this.selectableItemActive = this.selectableItems[index - 1];
+                this.selectScrollToActiveItem();
+            }
+        },
+
+        selectScrollToActiveItem() {
+            if (this.selectableItemActive) {
+                const activeElement = document.getElementById(this.selectableItemActive.value + '-' + this.selectId);
+                if (activeElement) {
+                    activeElement.scrollIntoView({ block: 'nearest' });
+                }
+            }
+        },
+
+        selectPositionUpdate() {
+            const selectDropdownBottomPos = this.$refs.selectButton.getBoundingClientRect().top + 
+                this.$refs.selectButton.offsetHeight + 
+                this.$refs.selectableItemsList.offsetHeight;
+            
+            this.selectDropdownPosition = window.innerHeight < selectDropdownBottomPos ? 'top' : 'bottom';
+        }
+    }"
+    @keydown.escape="selectOpen = false"
+    @keydown.down.prevent="if(selectOpen) { selectableItemActiveNext() } else { selectOpen = true }"
+    @keydown.up.prevent="if(selectOpen) { selectableItemActivePrevious() } else { selectOpen = true }"
+    @keydown.enter.prevent="selectedValue = selectableItemActive.value; selectOpen = false"
+    class="relative w-full"
+>
+    <button 
+        x-ref="selectButton"
+        @click="selectOpen = !selectOpen"
+        :class="{ 'ring-2 ring-offset-2 ring-neutral-800': selectOpen }"
+        class="relative w-full min-h-[38px] py-2 pl-3 pr-10 text-left bg-secondary border border-neutral rounded-md shadow-sm cursor-pointer text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-neutral-800"
+        type="button"
     >
-        {{ $slot }}
-    </select>
-    
-    <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true" class="h-4 w-4 text-base">
-            <path d="M18.2072 9.0428 12.0001 2.83569 5.793 9.0428 7.20721 10.457 12.0001 5.66412 16.793 10.457 18.2072 9.0428ZM5.79285 14.9572 12 21.1643 18.2071 14.9572 16.7928 13.543 12 18.3359 7.20706 13.543 5.79285 14.9572Z"></path>
-        </svg>
-    </span>
+        <span x-text="selectableItems.find(item => item.value === selectedValue)?.label ?? '{{ $placeholder ?? 'Select option' }}'" class="block truncate"></span>
+        <span class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+            <x-ri-expand-up-down-line class="w-4 h-4" />
+        </span>
+    </button>
+
+    <ul
+        x-show="selectOpen"
+        x-ref="selectableItemsList"
+        @click.away="selectOpen = false"
+        x-transition:enter="transition ease-out duration-100"
+        x-transition:enter-start="opacity-0 scale-95"
+        x-transition:enter-end="opacity-100 scale-100"
+        :class="{ 'bottom-full mb-1': selectDropdownPosition === 'top', 'top-full mt-1': selectDropdownPosition === 'bottom' }"
+        class="absolute z-50 w-full py-1 overflow-auto bg-background-secondary border border-neutral rounded-lg shadow-lg max-h-60 focus:outline-none text-sm"
+        x-cloak
+    >
+        <template x-for="item in selectableItems" :key="item.value">
+            <li
+                :id="item.value + '-' + selectId"
+                @click="selectedValue = item.value; selectOpen = false"
+                @mousemove="selectableItemActive = item"
+                :class="{ 'bg-neutral-700': selectableItemIsActive(item) }"
+                class="relative py-2 pl-8 pr-4 cursor-pointer select-none hover:bg-neutral-700"
+            >
+                <span 
+                    class="block truncate"
+                    :class="{ 'font-semibold': item.value === selectedValue }"
+                    x-text="item.label"
+                ></span>
+                <span
+                    x-show="item.value === selectedValue"
+                    class="absolute inset-y-0 left-0 flex items-center pl-2 text-neutral-400"
+                >
+                    <x-ri-check-fill class="w-4 h-4" />
+                </span>
+            </li>
+        </template>
+    </ul>
 </div>
