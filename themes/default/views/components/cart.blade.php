ecc6603acd6112e7a2ace4569860c0fc4f3498fa
Darkle <tomasromero@outlook.com>
2024-12-11 03:10:01 +0100

    feat: cart button with icon

--- themes/default/views/components/cart.blade.php
+++ themes/default/views/components/cart.blade.php
@@ -1,7 +1,7 @@
-<div>
+<div class="w-10 h-10 flex items-center justify-center rounded-lg hover:bg-neutral transition">
     @if($cartCount > 0)
         <x-navigation.link :href="route('cart')">
-            Cart
+            <x-ri-shopping-bag-4-fill class="w-4 h-4" />
         </x-navigation.link>
     @endif
 </div>
