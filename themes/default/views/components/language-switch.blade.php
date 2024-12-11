27b4f1bb6b97453a121aef0193194c56df6e3822
Darkle <tomasromero@outlook.com>
2024-12-11 15:37:24 +0100

    feat: update language switch to the new select component

--- themes/default/views/components/language-switch.blade.php
+++ themes/default/views/components/language-switch.blade.php
@@ -1,12 +1,8 @@
 <x-select
-    id="language-switch"
-    name="language"
     wire:model.live="currentLocale"
-    class="text-sm text-base/50"
->
-    @foreach ($locales as $locale)
-        <option value="{{ $locale }}" @selected($locale === $currentLocale)>
-            {{ strtoupper($locale) }}
-        </option>
-    @endforeach
-</x-select>
+    :options="collect($locales)->map(fn($locale) => [
+        'value' => $locale,
+        'label' => strtoupper($locale)
+    ])->values()->toArray()"
+    placeholder="Select language"
+/>
