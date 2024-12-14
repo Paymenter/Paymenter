ce745d36b8b8f433ecab42184d1d9bcd117a55d5
CorwinDev <CorwinDev@users.noreply.github.com>
2024-12-14 11:51:44 +0000

    style: linted files with pint

--- tests/Feature/Auth/RegisterTest.php
+++ tests/Feature/Auth/RegisterTest.php
@@ -11,7 +11,7 @@ class RegisterTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
         config(['settings.optional_fields' => [
