638262bd55607512a43f4265c7a40e950f34ca1c
Darkle <tomasromero@outlook.com>
2024-12-14 02:03:29 +0100

    feat: add SetLocale middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\EnsureUserHasPermissions;
+use App\Http\Middleware\SetLocale;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -19,6 +20,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'has' => EnsureUserHasPermissions::class,
             'scope' => CheckForAnyScope::class,
         ]);
+        $middleware->web(SetLocale::class);
     })
     ->withEvents(discover: [
         __DIR__ . '/../app/Extensions',
