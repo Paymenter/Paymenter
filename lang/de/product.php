f73181495490f8fd95d97bbc02b6575f89e2c152
Corwin <info@corwindev.nl>
2024-12-19 17:56:42 +0100

    New translations product.php (German)

--- lang/de/product.php
+++ lang/de/product.php
@@ -1,24 +1,24 @@
 <?php
 
 return [
-    'add_to_cart' => 'Add to cart',
-    'view' => 'View',
+    'add_to_cart' => 'Zum Warenkorb hinzufügen',
+    'view' => 'Anzeigen',
     'checkout' => 'Checkout',
-    'remove' => 'Remove',
-    'empty_cart' => 'Your cart is empty',
-    'total_today' => 'Total today',
-    'subtotal' => 'Subtotal',
-    'total' => 'Total',
+    'remove' => 'Entfernen',
+    'empty_cart' => 'Dein Warenkorb ist leer',
+    'total_today' => 'Heute zu bezahlender Betrag',
+    'subtotal' => 'Zwischensumme',
+    'total' => 'Gesamtpreis',
 
-    'then_after_x' => 'Then after :time',
-    'order_summary' => 'Order Summary',
-    'edit' => 'Edit',
+    'then_after_x' => 'Anschließend nach :time',
+    'order_summary' => 'Bestellübersicht',
+    'edit' => 'Bearbeiten',
 
-    'apply' => 'Apply',
-    'price' => 'Price',
-    'setup_fee' => 'Setup fee',
+    'apply' => 'Anwenden',
+    'price' => 'Preis',
+    'setup_fee' => 'Einrichtungsgebühr',
 
-    'in_stock' => 'In stock',
-    'out_of_stock' => 'Product :product is out of stock',
-    'user_limit' => 'You have reached the limit of :product',
+    'in_stock' => 'Auf Lager',
+    'out_of_stock' => 'Produkt :product ist nicht auf Lager',
+    'user_limit' => 'Du hast das Limit von :product erreicht',
 ];
