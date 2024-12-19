3cb3d640b59f6e5ef75f5c5191842c055f0c7403
Corwin <info@corwindev.nl>
2024-12-19 17:56:41 +0100

    New translations passwords.php (German)

--- lang/de/passwords.php
+++ lang/de/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Dein Passwort wurde zurückgesetzt.',
+    'sent' => 'Wir haben Ihnen den Link zum Zurücksetzen Ihres Passworts per E-Mail zugesandt.',
+    'throttled' => 'Bitte warte, bevor du es erneut versuchst.',
+    'token' => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig oder abgelaufen.',
+    'user' => "Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.",
 
 ];
