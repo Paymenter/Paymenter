e06327ff35f966327d619a4d34a46283a93fdc97
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:48:04 -0300

    Create services.php

---
+++ lang/pt/services.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'services' => 'Serviços',
+    'product' => 'Produto',
+    'price' => 'Preço',
+    'status' => 'Status',
+    'name' => 'Nome',
+    'actions' => 'Ações',
+    'view' => 'Ver',
+
+    'product_details' => 'Detalhes do Produto',
+    'billing_cycle' => 'Ciclo de Cobrança',
+    'cancel' => 'Cancelar',
+    'cancellation' => 'Cancelamento de :service',
+    'cancel_are_you_sure' => 'Você tem certeza de que deseja cancelar este serviço?',
+    'cancel_reason' => 'Motivo do cancelamento',
+    'cancel_type' => 'Tipo de Cancelamento',
+    'cancel_immediate' => 'Cancelar imediatamente',
+    'cancel_end_of_period' => 'Cancelar no final do período de cobrança',
+    'cancel_immediate_warning' => 'Ao pressionar o botão abaixo, o serviço será cancelado imediatamente e você não poderá mais utilizá-lo.',
+    'cancellation_requested' => 'Cancelamento solicitado',
+
+    'current_plan' => 'Plano Atual',
+    'new_plan' => 'Novo Plano',
+    'change_plan' => 'Alterar Plano',
+    'current_price' => 'Preço Atual',
+    'new_price' => 'Novo Preço',
+    'upgrade' => 'Atualizar',
+    'upgrade_summary' => 'Resumo da Atualização',
+    'total_today' => 'Total Hoje',
+];
