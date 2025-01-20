d72143490f94d844ed6b0c49af6f5d83ea202483
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:47:24 -0300

    Create product.php

---
+++ lang/pt/product.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'add_to_cart' => 'Adicionar ao carrinho',
+    'view' => 'Ver',
+    'checkout' => 'Finalizar compra',
+    'remove' => 'Remover',
+    'empty_cart' => 'Seu carrinho está vazio',
+    'total_today' => 'Total hoje',
+    'subtotal' => 'Subtotal',
+    'total' => 'Total',
+
+    'then_after_x' => 'Então, após :time',
+    'order_summary' => 'Resumo do Pedido',
+    'edit' => 'Editar',
+
+    'apply' => 'Aplicar',
+    'price' => 'Preço',
+    'setup_fee' => 'Taxa de configuração',
+
+    'in_stock' => 'Em estoque',
+    'out_of_stock' => 'Produto :product está fora de estoque',
+    'user_limit' => 'Você atingiu o limite de :product',
+];
