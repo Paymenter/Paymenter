bf00ed75b71c306e31d95826a0f0b42c2906be15
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:46:00 -0300

    Create pagination.php

---
+++ lang/pt/pagination.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Paginação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são usadas pela biblioteca de paginação
+    | para construir os links de paginação simples. Você está livre para
+    | alterá-las para qualquer coisa que desejar, a fim de personalizar suas
+    | visualizações para melhor corresponder à sua aplicação.
+    |
+    */
+
+    'previous' => '&laquo; Anterior',
+    'next' => 'Próximo &raquo;',
+
+];
