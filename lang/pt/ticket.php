c886c1006f4d92ee895785da1337ecf966a9d9d2
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:48:31 -0300

    Create ticket.php

---
+++ lang/pt/ticket.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    'ticket_details' => 'Detalhes do Ticket',
+    'subject' => 'Assunto',
+    'status' => 'Status',
+    'priority' => 'Prioridade',
+    'department' => 'Departamento',
+    'created_at' => 'Criado Em',
+    'reply' => 'Responder',
+
+    'create_ticket' => 'Criar Ticket',
+    'select_department' => 'Selecionar Departamento',
+    'select_priority' => 'Selecionar Prioridade',
+    'low' => 'Baixa',
+    'medium' => 'Média',
+    'high' => 'Alta',
+
+    'last_activity' => 'Última atividade',
+
+    'service' => 'Serviço',
+    'select_service' => 'Selecionar Serviço',
+    'create' => 'Criar',
+];
