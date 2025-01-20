88a72ed049e1f9b5567a9703878ba4005c4e00d2
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:42:45 -0300

    Create auth.php

---
+++ lang/pt/auth.php
@@ -0,0 +1,55 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Linhas de Linguagem de Autenticação
+    |--------------------------------------------------------------------------
+    |
+    | As seguintes linhas de linguagem são usadas durante a autenticação para várias
+    | mensagens que precisamos exibir ao usuário. Você está livre para modificar
+    | essas linhas de linguagem de acordo com os requisitos da sua aplicação.
+    |
+    */
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
+    'sign_in' => 'Entrar',
+    'sign_in_title' => 'Faça login na sua conta',
+    'or_sign_in_with' => 'Ou entre com',
+    'forgot_password' => 'Esqueceu sua senha?',
+    'dont_have_account' => 'Não tem uma conta?',
+    'already_have_account' => 'Já tem uma conta?',
+
+    'sign_up' => 'Cadastrar',
+    'sign_up_title' => 'Crie sua conta',
+
+    'logout' => 'Sair',
+
+    'input' => [
+        'email' => 'Email',
+        'email_label' => 'Endereço de E-mail',
+        'password' => 'Senha',
+    ],
+
+    'oauth' => [
+        'unverified_discord_account' => 'Sua conta do Discord não está verificada.',
+        'account_not_registered' => 'Você não está registrado neste site.',
+    ],
+
+    'reset_password' => 'Redefinir senha',
+
+    'verify_2fa' => 'Verificar 2FA',
+    'verify' => 'Verificar',
+
+    'verification' => [
+        'notice' => 'Verifique seu endereço de e-mail',
+        'sent' => 'Um novo link de verificação foi enviado para o seu endereço de e-mail.',
+        'check_your_email' => 'Antes de prosseguir, verifique seu e-mail para um link de verificação.',
+        'not_received' => 'Se você não recebeu o e-mail, pode solicitar outro e-mail de verificação.',
+        'request_another' => 'Reenviar e-mail de verificação',
+    ],
+];
