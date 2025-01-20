3b3382e585bf4cc5aff06d6b5720c0ad42eec351
Nícolas Pastorello <nicolaspastorello1@gmail.com>
2025-01-20 14:45:26 -0300

    Create navigation.php

---
+++ lang/pt/navigation.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'home' => 'Início',
+    'shop' => 'Loja',
+    'cart' => 'Carrinho',
+    'login' => 'Login',
+    'register' => 'Registrar',
+    'account' => 'Conta',
+    'dashboard' => 'Painel',
+    'tickets' => 'Tickets',
+    'admin' => 'Admin',
+];
