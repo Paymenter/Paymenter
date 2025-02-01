ce0cc469e64b602f0294815163f809f08a60cd51
Corwin <info@corwindev.nl>
2025-02-01 14:52:06 +0100

    New translations dashboard.php (Ukrainian)

--- lang/uk/dashboard.php
+++ lang/uk/dashboard.php
@@ -1,7 +1,11 @@
 <?php
 
 return [
+    'dashboard_title' => 'Dashboard',
+    'welcome_back' => 'Welcome back, :name!',
+    'dashboard_description' => 'Manage your active services, invoices, tickets, and stay updated here.',
     'open_tickets' => 'Відкриті тікети',
     'unpaid_invoices' => 'Неоплачені рахунки',
     'active_services' => 'Активні послуги',
+    'view_all' => 'View All',
 ];
