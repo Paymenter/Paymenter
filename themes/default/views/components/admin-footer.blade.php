fd5d0896eab7fb61c5e3845931bdc733a92e36df
iamdarkle <tomasromero@outlook.com>
2025-02-17 23:29:44 +0100

    feat: improvements to admin footer

--- themes/default/views/components/admin-footer.blade.php
+++ themes/default/views/components/admin-footer.blade.php
@@ -1,50 +1,62 @@
-<div class="flex flex-col gap-2">
-    <p class="text-xs text-gray-400 mt-auto">
-        Powered by <a href="https://paymenter.org" target="_blank" class="text-gray-400">Paymenter &copy;
-            {{ date('Y') }}</a>
-    </p>
-    <a href="https://github.com/sponsors/Paymenter" target="_blank"
-        class="btn">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5 mr-2">
-            <path
-                d="M14.1213 10.4816C13.7308 10.091 13.0976 10.091 12.7071 10.4816L12 11.1887C11.2189 11.9697 9.95259 11.9697 9.17154 11.1887C8.39049 10.4076 8.39049 9.14132 9.17154 8.36027L14.8022 2.72812C16.9061 2.25217 19.2008 2.83319 20.8388 4.47119C23.2582 6.89055 23.3716 10.7426 21.1792 13.2963L19.071 15.4313L14.1213 10.4816ZM3.16113 4.47119C5.33452 2.2978 8.66411 1.98527 11.17 3.5336L7.75732 6.94606C6.19523 8.50816 6.19523 11.0408 7.75732 12.6029C9.27209 14.1176 11.6995 14.1635 13.2695 12.7406L13.4142 12.6029L17.6568 16.8455L13.4142 21.0882C12.6331 21.8692 11.3668 21.8692 10.5858 21.0882L3.16113 13.6635C0.622722 11.1251 0.622722 7.00959 3.16113 4.47119Z"
-                fill="#DB61A2" />
-        </svg>
-        Sponsor
-    </a>
-
-    <a href="https://github.com/Paymenter/Paymenter" target="_blank"
-        class="btn">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"
-            class="w-5 h-5 mr-2">
-            <g clip-path="url(#clip0_161_50)">
-                <path
-                    d="M12.0006 18.2624L4.94715 22.2106L6.52248 14.2823L0.587891 8.79424L8.61493 7.8425L12.0006 0.502441L15.3862 7.8425L23.4132 8.79424L17.4787 14.2823L19.054 22.2106L12.0006 18.2624Z"
-                    fill="#E3B341" />
-            </g>
-            <defs>
-                <clipPath id="clip0_161_50">
-                    <rect width="24" height="24" fill="white" transform="translate(0 0.00244141)" />
-                </clipPath>
-            </defs>
-        </svg>
-        Star us on GitHub
-    </a>
-
-    <style>
-        .btn {
-            display: inline-flex;
-            align-items: center;
-            padding: 10px 15px;
-            border-radius: 5px;
-            text-decoration: none;
-        }
-
-        .btn svg {
-            width: 20px;
-            height: 20px;
-            margin-right: 8px;
-        }
-    </style>
+<div class="ring-1 ring-gray-950/5 dark:ring-white/10">
 </div>
+
+<span class="text-xs text-gray-400 mt-auto">
+    Powered by <a href="https://paymenter.org" target="_blank" class="text-gray-400">Paymenter &copy;
+        {{ date('Y') }}</a>
+</span>
+
+<ul class="fi-sidebar-nav-groups -mx-2 flex flex-col gap-y-7">
+    <li class="fi-sidebar-group flex flex-col gap-y-1">
+        <ul class="fi-sidebar-group-items flex flex-col gap-y-1">
+            <li class="fi-sidebar-item">
+                <a href="https://github.com/sponsors/Paymenter" target="_blank" class="fi-sidebar-item-button relative flex items-center justify-center gap-x-3 rounded-lg px-2 py-2 outline-none transition duration-75 hover:bg-gray-100 focus-visible:bg-gray-100 dark:hover:bg-white/5 dark:focus-visible:bg-white/5">
+                    <x-ri-service-fill class="fi-sidebar-item-icon h-6 w-6 sponsor" />
+                    <span class="fi-sidebar-item-label flex-1 truncate text-sm font-medium text-gray-700 dark:text-gray-200">
+                        Sponsor
+                    </span>
+                </a>
+            </li>
+            <li class="fi-sidebar-item">
+                <a href="https://github.com/Paymenter/Paymenter" target="_blank" class="fi-sidebar-item-button relative flex items-center justify-center gap-x-3 rounded-lg px-2 py-2 outline-none transition duration-75 hover:bg-gray-100 focus-visible:bg-gray-100 dark:hover:bg-white/5 dark:focus-visible:bg-white/5">
+                    <x-ri-star-fill class="fi-sidebar-item-icon h-6 w-6 star-git" />
+                    <span class="fi-sidebar-item-label flex-1 truncate text-sm font-medium text-gray-700 dark:text-gray-200">
+                        Star us on GitHub
+                    </span>
+                </a>
+            </li>
+            <li class="fi-sidebar-item">
+                <a href="https://paymenter.org/docs/getting-started/introduction" target="_blank" class="fi-sidebar-item-button relative flex items-center justify-center gap-x-3 rounded-lg px-2 py-2 outline-none transition duration-75 hover:bg-gray-100 focus-visible:bg-gray-100 dark:hover:bg-white/5 dark:focus-visible:bg-white/5">
+                    <x-ri-book-2-line class="fi-sidebar-item-icon h-6 w-6 text-gray-400 dark:text-gray-500" />
+                    <span class="fi-sidebar-item-label flex-1 truncate text-sm font-medium text-gray-700 dark:text-gray-200">
+                        Documentation
+                    </span>
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
+
+<style>
+    .btn {
+        display: inline-flex;
+        align-items: center;
+        padding: 10px 15px;
+        border-radius: 5px;
+        text-decoration: none;
+    }
+
+    .btn svg {
+        width: 20px;
+        height: 20px;
+        margin-right: 8px;
+    }
+
+    .sponsor {
+        fill: #DB61A2;
+    }
+
+    .star-git {
+        fill: #E3B341;
+    }
+</style>
