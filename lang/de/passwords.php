24381c57c49d503303ee9c3094d621855b0384cf
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-02-19 14:32:52 +0000

    style: linted files with pint

--- lang/de/passwords.php
+++ lang/de/passwords.php
@@ -6,6 +6,6 @@ return [
     'sent' => 'Wir haben Ihnen den Link zum Zurücksetzen Ihres Passworts per E-Mail zugesandt.',
     'throttled' => 'Bitte warte, bevor du es erneut versuchst.',
     'token' => 'Der Passwort-Wiederherstellungs-Schlüssel ist ungültig oder abgelaufen.',
-    'user' => "Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.",
+    'user' => 'Es konnte leider kein Nutzer mit dieser E-Mail-Adresse gefunden werden.',
 
 ];
