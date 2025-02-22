b480dc81974f3daf9f0d845c983d35f32eacc05d
Corwin <info@corwindev.nl>
2025-02-22 21:41:20 +0100

    New translations passwords.php (Latvian)

--- lang/lv/passwords.php
+++ lang/lv/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Jūsu parole tika atjaunota.',
+    'sent' => 'E-pastā esam nosūtījuši jūsu paroles atjaunošanas saiti!',
+    'throttled' => 'Lūdzu uzgaidiet pirms mēģināt vēlreiz',
+    'token' => 'Paroles atjaunošanas žetons ir nederīga.',
+    'user' => "Mēs nevaram atrast lietotāju ar šādu e-pasta adresi.",
 
 ];
