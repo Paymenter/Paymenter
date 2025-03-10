a9bc566344c3286cc841ef483c85ba32a998168c
Corwin <info@corwindev.nl>
2025-03-10 15:26:24 +0100

    fix: paypal webhook not working

--- extensions/Gateways/PayPal/routes.php
+++ extensions/Gateways/PayPal/routes.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 use Paymenter\Extensions\Gateways\PayPal\PayPal;
 
-Route::get('/extensions/paypal/webhook', [PayPal::class, 'webhook'])->name('extensions.gateways.paypal.webhook');
-Route::get('/extensions/paypal/capture', [PayPal::class, 'capture'])->name('extensions.gateways.paypal.capture');
+Route::post('/extensions/paypal/webhook', [PayPal::class, 'webhook'])->name('extensions.gateways.paypal.webhook');
+Route::post('/extensions/paypal/capture', [PayPal::class, 'capture'])->name('extensions.gateways.paypal.capture');
