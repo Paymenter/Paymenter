67562305339263fa5dd9fbfdf695362072cecb9f
Corwin <info@corwindev.nl>
2025-03-19 14:17:11 +0100

    New translations services.php (Serbian (Latin))

---
+++ lang/sr/services.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'services' => 'Services',
+    'product' => 'Product',
+    'price' => 'Price',
+    'status' => 'Status',
+    'name' => 'Name',
+    'actions' => 'Actions',
+    'view' => 'View',
+
+    'product_details' => 'Product Details',
+    'billing_cycle' => 'Billing Cycle',
+    'cancel' => 'Cancel',
+    'cancellation' => 'Cancellation of :service',
+    'cancel_are_you_sure' => 'Are you sure you want to cancel this service?',
+    'cancel_reason' => 'Reason for cancellation',
+    'cancel_type' => 'Cancellation Type',
+    'cancel_immediate' => 'Cancel immediate',
+    'cancel_end_of_period' => 'Cancel at the end of the billing period',
+    'cancel_immediate_warning' => 'When you press the button below, the service will be cancelled immediately and you will not be able to use it anymore.',
+    'cancellation_requested' => 'Cancellation requested',
+
+    'current_plan' => 'Current Plan',
+    'new_plan' => 'New Plan',
+    'change_plan' => 'Change Plan',
+    'current_price' => 'Current Price',
+    'new_price' => 'New Price',
+    'upgrade' => 'Upgrade',
+    'upgrade_summary' => 'Upgrade Summary',
+    'total_today' => 'Total Today',
+];
