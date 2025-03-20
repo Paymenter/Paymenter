5e3507f0bfc3a3e1fc801f2880ff1277dcbd3f1a
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-03-20 17:51:21 +0000

    style: linted files with pint

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -2,10 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use App\Observers\TicketObserver;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
 #[ObservedBy([TicketObserver::class])]
 
