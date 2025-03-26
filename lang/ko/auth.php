99db233de7a433f474faefe9dc332bb9acaf8dee
Corwin <info@corwindev.nl>
2025-03-26 06:09:16 +0100

    New translations auth.php (Korean)

--- lang/ko/auth.php
+++ lang/ko/auth.php
@@ -13,43 +13,43 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '로그인 정보가 올바르지 않습니다.',
+    'password' => '비밀번호가 올바르지 않습니다.',
+    'throttle' => '로그인 시도가 너무 많습니다. :seconds 초 후에 다시 시도해주세요.',
 
-    'sign_in' => 'Sign in',
-    'sign_in_title' => 'Sign in to your account',
-    'or_sign_in_with' => 'Or sign in with',
-    'forgot_password' => 'Forgot your password?',
-    'dont_have_account' => 'Don\'t have an account?',
-    'already_have_account' => 'Already have an account?',
+    'sign_in' => '로그인',
+    'sign_in_title' => '계정에 로그인',
+    'or_sign_in_with' => '또는 다음을 사용하여 로그인하세요:',
+    'forgot_password' => '비밀번호를 잊으셨나요?',
+    'dont_have_account' => '계정이 없으신가요?',
+    'already_have_account' => '이미 계정이 있으신가요?',
 
-    'sign_up' => 'Sign up',
-    'sign_up_title' => 'Create your account',
+    'sign_up' => '회원가입',
+    'sign_up_title' => '계정 생성하기',
 
-    'logout' => 'Logout',
+    'logout' => '로그아웃',
 
     'input' => [
-        'email' => 'Email',
-        'email_label' => 'Email Address',
-        'password' => 'Password',
+        'email' => '이메일',
+        'email_label' => '이메일 주소',
+        'password' => '비밀번호',
     ],
 
     'oauth' => [
-        'unverified_discord_account' => 'Your Discord account is not verified.',
-        'account_not_registered' => 'You are not registered on this site.',
+        'unverified_discord_account' => 'Discord 계정이 인증되지 않았습니다.',
+        'account_not_registered' => '이 사이트에 가입되어 있지 않습니다.',
     ],
 
-    'reset_password' => 'Reset password',
+    'reset_password' => '비밀번호 재설정',
 
-    'verify_2fa' => 'Verify 2FA',
-    'verify' => 'Verify',
+    'verify_2fa' => '2단계 인증',
+    'verify' => '인증',
 
     'verification' => [
-        'notice' => 'Verify your email address',
-        'sent' => 'A new verification link has been sent to your email address.',
-        'check_your_email' => 'Before proceeding, please check your email for a verification link.',
-        'not_received' => 'If you did not receive the email you can request another verification email.',
-        'request_another' => 'Resend verification email',
+        'notice' => '이메일 주소 인증',
+        'sent' => '새 인증 링크가 이메일로 전송되었습니다.',
+        'check_your_email' => '진행하기 전에, 이메일로 인증 링크가 왔는지 확인해주세요.',
+        'not_received' => '이메일을 받지 못했다면 새 인증 이메일을 요청할 수 있습니다.',
+        'request_another' => '인증 이메일 재전송',
     ],
 ];
