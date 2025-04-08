bfbf58785cd4cf05d4b8b6f933e034b5d9ec6feb
Corwin <info@corwindev.nl>
2025-04-08 15:23:12 +0200

    fix: service cancellation not triggering events

--- app/Models/ServiceCancellation.php
+++ app/Models/ServiceCancellation.php
@@ -2,9 +2,13 @@
 
 namespace App\Models;
 
+use App\Observers\ServiceCancellationObserver;
+use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+#[ObservedBy([ServiceCancellationObserver::class])]
 class ServiceCancellation extends Model
 {
     use HasFactory;
