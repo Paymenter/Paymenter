bcbcfaf3be91b2ee2758d87d731e934151c300bf
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-04-09 13:27:32 +0000

    style: linted files with pint

--- app/Livewire/Cart.php
+++ app/Livewire/Cart.php
@@ -15,7 +15,6 @@ use App\Models\Invoice;
 use App\Models\Order;
 use App\Models\Service;
 use App\Models\User;
-use Exception;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
