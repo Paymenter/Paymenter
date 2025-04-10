303181691be3ecb84969e216a592f3e2e983ea9b
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-04-10 09:49:20 +0000

    style: linted files with pint

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -6,6 +6,6 @@ return [
     'sent' => 'Te hemos enviado el enlace para restablecer tu contraseña por correo electrónico.',
     'throttled' => 'Por favor, espera antes de reintentarlo.',
     'token' => 'La clave de enlace para cambiar la contraseña no es válida.',
-    'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
+    'user' => 'No podemos encontrar un usuario con esa dirección de correo electrónico.',
 
 ];
