8c0e346c5c108f8c5c8faf797490efae45b4ce00
Corwin <info@corwindev.nl>
2025-04-11 21:16:55 +0200

    New translations dashboard.php (German)

--- lang/de/dashboard.php
+++ lang/de/dashboard.php
@@ -3,7 +3,7 @@
 return [
     'dashboard_title' => 'Übersicht',
     'welcome_back' => 'Willkommen zurück :name!',
-    'dashboard_description' => 'Verwalte deine aktiven Dienste, Rechnungen und Tickets.',
+    'dashboard_description' => 'Verwalte deine aktiven Dienste, Rechnungen, Tickets und bleibe auf dem Laufendem.',
     'open_tickets' => 'Offene Tickets',
     'unpaid_invoices' => 'Offene Rechnungen',
     'active_services' => 'Aktive Dienste',
