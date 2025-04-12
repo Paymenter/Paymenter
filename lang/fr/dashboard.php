f4bdce833d9067da2c65fa6e057d66ad233b1944
Corwin <info@corwindev.nl>
2025-04-12 20:57:22 +0200

    New translations dashboard.php (French)

--- lang/fr/dashboard.php
+++ lang/fr/dashboard.php
@@ -2,7 +2,7 @@
 
 return [
     'dashboard_title' => 'Tableau de bord',
-    'welcome_back' => 'Heureux de vous revoir, :name!',
+    'welcome_back' => 'Heureux de vous revoir, :name !',
     'dashboard_description' => 'Gérez vos services actifs, vos factures, vos tickets et obtenez les dernières informations ici.',
     'open_tickets' => 'Tickets Ouverts',
     'unpaid_invoices' => 'Factures impayées',
