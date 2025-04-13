978c4dbcb6b36859470ca8b22d898e1272bf9f9d
Corwin <info@corwindev.nl>
2025-04-13 17:37:34 +0200

    New translations passwords.php (Dutch)

--- lang/nl/passwords.php
+++ lang/nl/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Uw wachtwoord is gereset.',
+    'sent' => 'We hebben je een e-mail gestuurd met een link om je wachtwoord te resetten.',
+    'throttled' => 'Wacht even voordat je het opnieuw probeert.',
+    'token' => 'Dit wachtwoord reset token is ongeldig.',
+    'user' => "We kunnen geen gebruiker met dat e-mailadres vinden.",
 
 ];
