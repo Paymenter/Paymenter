f1931908838d45b81763ea3b98f88e16516a3b97
Corwin <info@corwindev.nl>
2025-04-13 17:37:30 +0200

    New translations dashboard.php (Dutch)

--- lang/nl/dashboard.php
+++ lang/nl/dashboard.php
@@ -6,6 +6,6 @@ return [
     'dashboard_description' => 'Beheer uw actieve diensten, facturen en tickets.',
     'open_tickets' => 'Open tickets',
     'unpaid_invoices' => 'Onbetaalde facturen',
-    'active_services' => 'Active Services',
-    'view_all' => 'View All',
+    'active_services' => 'Actieve services',
+    'view_all' => 'Bekijk alle',
 ];
