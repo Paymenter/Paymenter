3d3622d2399f6f36a219d1787fa14c0742bfcc5d
Corwin <info@corwindev.nl>
2025-04-13 13:07:42 +0200

    New translations auth.php (Dutch)

--- lang/nl/auth.php
+++ lang/nl/auth.php
@@ -13,43 +13,43 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Deze combinatie van email en wachtwoord is niet geldig.',
+    'password' => 'Het opgegeven wachtwoord is incorrect.',
+    'throttle' => 'Te veel inlogpogingen. Probeer het over :seconds seconden opnieuw.',
 
-    'sign_in' => 'Sign in',
-    'sign_in_title' => 'Sign in to your account',
-    'or_sign_in_with' => 'Or sign in with',
-    'forgot_password' => 'Forgot your password?',
-    'dont_have_account' => 'Don\'t have an account?',
-    'already_have_account' => 'Already have an account?',
+    'sign_in' => 'Inloggen',
+    'sign_in_title' => 'Log in op uw account',
+    'or_sign_in_with' => 'Of log in met',
+    'forgot_password' => 'Wachtwoord vergeten?',
+    'dont_have_account' => 'Heeft u nog geen account?',
+    'already_have_account' => 'Heeft u al een account?',
 
-    'sign_up' => 'Sign up',
-    'sign_up_title' => 'Create your account',
+    'sign_up' => 'Registreren',
+    'sign_up_title' => 'Maak uw account aan',
 
-    'logout' => 'Logout',
+    'logout' => 'Uitloggen',
 
     'input' => [
-        'email' => 'Email',
-        'email_label' => 'Email Address',
-        'password' => 'Password',
+        'email' => 'E-mail',
+        'email_label' => 'E-mailadres',
+        'password' => 'Wachtwoord',
     ],
 
     'oauth' => [
-        'unverified_discord_account' => 'Your Discord account is not verified.',
-        'account_not_registered' => 'You are not registered on this site.',
+        'unverified_discord_account' => 'Je Discord-account is niet geverifieerd.',
+        'account_not_registered' => 'U bent niet geregistreerd op deze site.',
     ],
 
-    'reset_password' => 'Reset password',
+    'reset_password' => 'Wachtwoord resetten',
 
-    'verify_2fa' => 'Verify 2FA',
-    'verify' => 'Verify',
+    'verify_2fa' => '2FA verifiëren',
+    'verify' => 'Verifiëren',
 
     'verification' => [
-        'notice' => 'Verify your email address',
-        'sent' => 'A new verification link has been sent to your email address.',
-        'check_your_email' => 'Before proceeding, please check your email for a verification link.',
-        'not_received' => 'If you did not receive the email you can request another verification email.',
-        'request_another' => 'Resend verification email',
+        'notice' => 'Verifieer uw e-mailadres',
+        'sent' => 'Een nieuwe verificatie link naar je e-mailadres gestuurd.',
+        'check_your_email' => 'Controleer voordat u verder gaat uw e-mail voor een verificatielink.',
+        'not_received' => 'Als je de e-mail niet hebt ontvangen, kan je nog een verificatielink aanvragen.',
+        'request_another' => 'Verificatie e-mail opnieuw versturen',
     ],
 ];
