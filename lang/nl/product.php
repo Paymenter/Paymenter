3f4cc3f49e37c85c8c82d16a4308eb2d958df530
Corwin <info@corwindev.nl>
2025-04-13 17:37:34 +0200

    New translations product.php (Dutch)

--- lang/nl/product.php
+++ lang/nl/product.php
@@ -1,24 +1,24 @@
 <?php
 
 return [
-    'add_to_cart' => 'Add to cart',
-    'view' => 'View',
-    'checkout' => 'Checkout',
-    'remove' => 'Remove',
-    'empty_cart' => 'Your cart is empty',
-    'total_today' => 'Total today',
-    'subtotal' => 'Subtotal',
-    'total' => 'Total',
+    'add_to_cart' => 'Aan winkelwagen toevoegen',
+    'view' => 'Weergeven',
+    'checkout' => 'Afrekenen',
+    'remove' => 'Verwijder',
+    'empty_cart' => 'Uw winkelwagen is leeg',
+    'total_today' => 'Totale prijs vandaag',
+    'subtotal' => 'Subtotaal',
+    'total' => 'Totaal',
 
-    'then_after_x' => 'Then after :time',
-    'order_summary' => 'Order Summary',
-    'edit' => 'Edit',
+    'then_after_x' => 'Vervolgens na :time',
+    'order_summary' => 'Besteloverzicht',
+    'edit' => 'Bewerken',
 
-    'apply' => 'Apply',
-    'price' => 'Price',
-    'setup_fee' => 'Setup fee',
+    'apply' => 'Toepassen',
+    'price' => 'Prijs',
+    'setup_fee' => 'Eenmalige kosten',
 
-    'in_stock' => 'In stock',
-    'out_of_stock' => 'Product :product is out of stock',
-    'user_limit' => 'You have reached the limit of :product',
+    'in_stock' => 'Op voorraad',
+    'out_of_stock' => 'Product :product is niet op voorraad',
+    'user_limit' => 'U heeft het limiet van :product bereikt',
 ];
