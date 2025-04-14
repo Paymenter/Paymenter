19f20582f6b6ab7ff106e3759fa5b0a1d9cb8de9
Corwin <info@corwindev.nl>
2025-04-14 20:48:11 +0200

    New translations product.php (Dutch)

--- lang/nl/product.php
+++ lang/nl/product.php
@@ -21,4 +21,6 @@ return [
     'in_stock' => 'Op voorraad',
     'out_of_stock' => 'Product :product is niet op voorraad',
     'user_limit' => 'U heeft het limiet van :product bereikt',
+    'payment_method' => 'Payment method',
+    'use_credits' => 'Use credits',
 ];
