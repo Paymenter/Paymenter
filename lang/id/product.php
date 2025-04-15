e5f37d3b134efaeeab85575bd789b5bc8a4fc1ef
Corwin <info@corwindev.nl>
2025-04-15 18:19:17 +0200

    New translations product.php (Indonesian)

--- lang/id/product.php
+++ lang/id/product.php
@@ -10,7 +10,7 @@ return [
     'subtotal' => 'Subtotal',
     'total' => 'Total',
 
-    'then_after_x' => 'Setelah pukul :time',
+    'then_after_x' => 'Setelah pukul :waktu',
     'order_summary' => 'Ringkasan Pesanan',
     'edit' => 'Ubah',
 
@@ -19,8 +19,8 @@ return [
     'setup_fee' => 'Biaya Setup',
 
     'in_stock' => 'Tersedia',
-    'out_of_stock' => ':product tidak tersedia',
-    'user_limit' => 'Anda telah mencapai batas untuk :product',
-    'payment_method' => 'Payment method',
-    'use_credits' => 'Use credits',
+    'out_of_stock' => 'Produk : produk tidak tersedia',
+    'user_limit' => 'Anda telah mencapai batas untuk : produk',
+    'payment_method' => 'Metode pembayaran',
+    'use_credits' => 'Gunakan Kredit',
 ];
