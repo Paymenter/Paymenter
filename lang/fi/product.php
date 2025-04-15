561a732e44e700a96999ec853aa19b3908e3c6d8
Corwin <info@corwindev.nl>
2025-04-15 19:55:56 +0200

    New translations product.php (Finnish)

--- lang/fi/product.php
+++ lang/fi/product.php
@@ -23,4 +23,7 @@ return [
     'user_limit' => 'Olet saavuttanut :product tuotteen maksimirajan',
     'payment_method' => 'Payment method',
     'use_credits' => 'Use credits',
+    'tos' => 'I agree to the ',
+    'tos_link' => 'Terms of Service',
+    'tos_required' => 'You must agree to the Terms of Service',
 ];
