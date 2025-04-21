b352ea03425537d949f746fbcb76a31a70e4cef0
Corwin <info@corwindev.nl>
2025-04-21 23:28:30 +0200

    New translations validation.php (Swedish)

--- lang/sv/validation.php
+++ lang/sv/validation.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'accepted' => 'The :attribute field must be accepted.',
+    'accepted' => 'Fältet :attribute måste accepteras.',
     'accepted_if' => 'The :attribute field must be accepted when :other is :value.',
     'active_url' => 'The :attribute field must be a valid URL.',
     'after' => 'The :attribute field must be a date after :date.',
