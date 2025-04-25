60f0dbbf8a1a03dd7b708092655a8750ab617201
Ronald van Eede <ronald@vaneede.nu>
2025-04-25 15:29:48 +0200

    migration

---
+++ database/migrations/2025_04_25_064729_add_number_to_invoices.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->string('number')->nullable()->unique()->after('id');
+        });
+
+        DB::statement('UPDATE invoices SET number = id');
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('invoices', function (Blueprint $table) {
+            $table->dropColumn('number');
+        });
+    }
+};
