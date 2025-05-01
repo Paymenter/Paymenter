779bc1c0d19a5f87127f237a0e916a185387d592
Ronald van Eede <ronald@vaneede.nu>
2025-05-01 21:25:43 +0200

    wip

--- app/Observers/PropertyObserver.php
+++ app/Observers/PropertyObserver.php
@@ -7,16 +7,25 @@ use App\Models\Property;
 
 class PropertyObserver
 {
+    /**
+     * Handle the Property "creating" event.
+     */
     public function created(Property $property): void
     {
         event(new PropertyEvent\Created($property));
     }
 
+    /**
+     * Handle the Property "updating" event.
+     */
     public function updated(Property $property): void
     {
         event(new PropertyEvent\Updated($property));
     }
 
+    /**
+     * Handle the Property "deleted" event.
+     */
     public function deleted(Property $property): void
     {
         event(new PropertyEvent\Deleted($property));
