1c3e50c1736dd0a5de46d8a463762ddb4e114715
Corwin <info@corwindev.nl>
2025-05-02 21:23:28 +0200

    fix: checkout quantity text field not visible

--- themes/default/views/components/form/input.blade.php
+++ themes/default/views/components/form/input.blade.php
@@ -12,7 +12,7 @@
         </legend>
     @endif
     <input type="{{ $type ?? 'text' }}" id="{{ $id ?? $name }}" name="{{ $name }}"
-        class="block w-full text-sm text-base bg-background-secondary border border-neutral rounded-md shadow-sm focus:outline-none transition-all duration-300 ease-in-out disabled:bg-secondary disabled:cursor-not-allowed {{ $class ?? '' }} @if ($type !== 'color') px-2.5 py-2.5 @endif"
+        class="block w-full text-sm text-base bg-background-secondary border border-neutral rounded-md shadow-sm focus:outline-none transition-all duration-300 ease-in-out disabled:bg-background-secondary/50 disabled:cursor-not-allowed {{ $class ?? '' }} @if ($type !== 'color') px-2.5 py-2.5 @endif"
         placeholder="{{ $placeholder ?? ($label ?? '') }}"
         @if ($dirty && isset($attributes['wire:model'])) wire:dirty.class="!border-yellow-600" @endif
         {{ $attributes->except(['placeholder', 'label', 'id', 'name', 'type', 'class', 'divClass', 'required', 'hideRequiredIndicator', 'dirty']) }} @required($required) />
