f964171d39fdfa84f3264401bc629edb725b2e6d
Corwin <info@corwindev.nl>
2025-05-07 21:52:36 +0200

    New translations auth.php (Chinese Traditional)

--- lang/zh/auth.php
+++ lang/zh/auth.php
@@ -13,43 +13,43 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => '這些憑證與我們的記錄不符。',
+    'password' => '提供的密碼不正確。',
+    'throttle' => '登入嘗試次數過多。請在 :seconds 秒後再試。',
 
-    'sign_in' => 'Sign in',
-    'sign_in_title' => 'Sign in to your account',
-    'or_sign_in_with' => 'Or sign in with',
-    'forgot_password' => 'Forgot your password?',
-    'dont_have_account' => 'Don\'t have an account?',
-    'already_have_account' => 'Already have an account?',
+    'sign_in' => '登入',
+    'sign_in_title' => '登入您的帳戶',
+    'or_sign_in_with' => '或使用以下方式登入',
+    'forgot_password' => '忘記密碼？',
+    'dont_have_account' => '還沒有帳戶？',
+    'already_have_account' => '已經有帳戶了？',
 
-    'sign_up' => 'Sign up',
-    'sign_up_title' => 'Create your account',
+    'sign_up' => '註冊',
+    'sign_up_title' => '建立您的帳戶',
 
-    'logout' => 'Logout',
+    'logout' => '登出',
 
     'input' => [
-        'email' => 'Email',
-        'email_label' => 'Email Address',
-        'password' => 'Password',
+        'email' => '電子郵件',
+        'email_label' => '電子郵件地址',
+        'password' => '密碼',
     ],
 
     'oauth' => [
-        'unverified_discord_account' => 'Your Discord account is not verified.',
-        'account_not_registered' => 'You are not registered on this site.',
+        'unverified_discord_account' => '您的 Discord 帳戶尚未驗證。',
+        'account_not_registered' => '您尚未在本站註冊。',
     ],
 
-    'reset_password' => 'Reset password',
+    'reset_password' => '重設密碼',
 
-    'verify_2fa' => 'Verify 2FA',
-    'verify' => 'Verify',
+    'verify_2fa' => '驗證兩步驟驗證',
+    'verify' => '驗證',
 
     'verification' => [
-        'notice' => 'Verify your email address',
-        'sent' => 'A new verification link has been sent to your email address.',
-        'check_your_email' => 'Before proceeding, please check your email for a verification link.',
-        'not_received' => 'If you did not receive the email you can request another verification email.',
-        'request_another' => 'Resend verification email',
+        'notice' => '驗證您的電子郵件地址',
+        'sent' => '新的驗證連結已發送到您的電子郵件地址。',
+        'check_your_email' => '在繼續之前，請檢查您的電子郵件以獲取驗證連結。',
+        'not_received' => '如果您沒有收到電子郵件，您可以要求重新發送驗證郵件。',
+        'request_another' => '重新發送驗證郵件',
     ],
 ];
