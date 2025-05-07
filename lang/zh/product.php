d23cad418d852b8725f1415be3de1c10881107ca
Corwin <info@corwindev.nl>
2025-05-07 20:33:54 +0200

    New translations product.php (Chinese Traditional)

---
+++ lang/zh/product.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'add_to_cart' => 'Add to cart',
+    'view' => 'View',
+    'checkout' => 'Checkout',
+    'remove' => 'Remove',
+    'empty_cart' => 'Your cart is empty',
+    'total_today' => 'Total today',
+    'subtotal' => 'Subtotal',
+    'total' => 'Total',
+
+    'then_after_x' => 'Then after :time',
+    'order_summary' => 'Order Summary',
+    'edit' => 'Edit',
+
+    'apply' => 'Apply',
+    'price' => 'Price',
+    'setup_fee' => 'Setup fee',
+
+    'in_stock' => 'In stock',
+    'out_of_stock' => 'Product :product is out of stock',
+    'user_limit' => 'You have reached the limit of :product',
+    'payment_method' => 'Payment method',
+    'use_credits' => 'Use credits',
+    'tos' => 'I agree to the ',
+    'tos_link' => 'Terms of Service',
+    'tos_required' => 'You must agree to the Terms of Service',
+];
