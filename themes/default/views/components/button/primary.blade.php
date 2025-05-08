16d12be6f4d1e12dd3dba37e7bb328f9d9ce6730
iamdarkle <tomasromero@outlook.com>
2025-05-09 01:26:53 +0200

    feat: improved buttons

--- themes/default/views/components/button/primary.blade.php
+++ themes/default/views/components/button/primary.blade.php
@@ -1,6 +1,5 @@
 <button 
-    {{ $attributes->merge(['class' => 'bg-primary text-white font-semibold hover:bg-primary/80 py-2 px-4 rounded-md w-full duration-300 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50']) }}>
-
+    {{ $attributes->merge(['class' => 'flex items-center gap-2 justify-center bg-primary text-white text-sm font-semibold hover:bg-primary/80 py-2.5 lg:py-2 px-4.5 rounded-md w-full duration-300 cursor-pointer disabled:cursor-not-allowed disabled:opacity-50']) }}>
     @if (isset($type) && $type === 'submit')
         <div role="status" wire:loading>
             <x-ri-loader-5-fill aria-hidden="true" class="size-6 me-2 fill-background animate-spin" />
