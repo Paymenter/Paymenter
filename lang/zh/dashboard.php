cf25463627e9025cc34f26be40790788a931f66f
Corwin <info@corwindev.nl>
2025-05-08 14:40:53 +0200

    New translations dashboard.php (Chinese Traditional)

--- lang/zh/dashboard.php
+++ lang/zh/dashboard.php
@@ -2,7 +2,7 @@
 
 return [
     'dashboard_title' => '客戶中心',
-    'welcome_back' => '歡迎回來, :name!',
+    'welcome_back' => '歡迎回來，{name}！',
     'dashboard_description' => '在這裡管理您啟用中的服務、帳單、服務單，並保持最新狀態。',
     'open_tickets' => '處理中的服務單',
     'unpaid_invoices' => '未付款的帳單',
