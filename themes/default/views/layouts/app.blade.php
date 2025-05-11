ae4d338f84784b7875a5267c1a306ee217b44b28
VXsz <34054757+VXsz@users.noreply.github.com>
2025-05-11 07:45:05 +0300

    Update app.blade.php
    Check for RTL langs not just ar

--- themes/default/views/layouts/app.blade.php
+++ themes/default/views/layouts/app.blade.php
@@ -1,6 +1,10 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @if(app()->getLocale() === 'ar') dir="rtl" @endif>
+@php
+    $rtlLocales = ['ar', 'fa', 'ur', 'ps', 'sd', 'ug', 'dv', 'ku'];
+@endphp
 
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" @if(in_array(app()->getLocale(), $rtlLocales)) dir="rtl" @endif>
+    
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
