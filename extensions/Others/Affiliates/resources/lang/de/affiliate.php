fc7a6495e875b21e5e21af6cde427c38cc4cc44a
gmrrh <florian.gutz06@gmail.com>
2025-05-13 23:22:33 +0200

    Add German Language to Paymenter

---
+++ extensions/Others/Affiliates/resources/lang/de/affiliate.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'affiliate' => 'Affiliate',
+    'signup-for-affiliate' => 'Zum Partnerprogramm anmelden',
+    'code' => 'Code',
+    'visitors' => 'Besucher',
+    'total-visitors' => 'Besucher insgesamt',
+    'signups' => 'Registrierungen',
+    'total-signups' => 'Registrierungen insgesamt',
+    'earnings' => 'Einnahmen',
+    'total-earnings' => 'Einnahmen insgesamt',
+    'your-affiliate-link' => 'Dein Affiliate-Link',
+    'copy' => 'Kopieren',
+
+    'you-havent-signed-up-yet' => 'Du hast dich bis jetzt noch nicht für unser Partnerprogramm angemeldet.',
+    'you-are-already-affiliated' => 'Du hast dich bereits für das Partnerprogramm angemeldet.',
+    'signup-success' => 'Erfolgreich für das Partnerprogramm angemeldet!',
+];
