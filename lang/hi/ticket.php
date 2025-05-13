58b394b9d430b2ea84b6203b4378aa619a53ffb9
Corwin <info@corwindev.nl>
2025-05-13 17:46:54 +0200

    New translations ticket.php (Hindi)

---
+++ lang/hi/ticket.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+    'ticket_details' => 'टिकट विवरण',
+    'tickets' => 'टिकट्स',
+    'subject' => 'विषय',
+    'status' => 'स्थिति',
+    'priority' => 'प्राथमिकता',
+    'department' => 'विभाग',
+    'created_at' => 'निर्मित समय',
+    'reply' => 'उत्तर',
+
+    'create_ticket' => 'टिकट बनाएं',
+    'select_department' => 'विभाग चुनें',
+    'select_priority' => 'प्राथमिकता चुनें',
+    'low' => 'कम',
+    'medium' => 'मध्यम',
+    'high' => 'उच्च',
+
+    'last_activity' => 'अंतिम गतिविधि',
+
+    'service' => 'सेवा',
+    'select_service' => 'सेवा चुनें',
+    'create' => 'बनाएं',
+];
