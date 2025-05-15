57c409b1d003b4fd479735fc6bcb98ac9677c20e
Corwin <info@corwindev.nl>
2025-05-15 16:17:19 +0200

    New translations invoices.php (Norwegian)

--- lang/no/invoices.php
+++ lang/no/invoices.php
@@ -13,6 +13,7 @@ return [
     'invoice' => 'Faktura #:id',
     'unit_price' => 'Enhetspris',
     'bill_to' => 'Fakturer Til',
+    'issued_to' => 'Issued To',
     'paid' => 'Betalt',
     'payment_pending' => 'Venter på betaling',
     'checking_payment' => 'Kontrollerer betaling',
