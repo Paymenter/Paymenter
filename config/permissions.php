14dde23321e02e380f84d3684ccbfd256e8ffa53
Corwin <info@corwindev.nl>
2025-05-21 19:40:27 +0200

    feat: add impersonation support (closes #771)

--- config/permissions.php
+++ config/permissions.php
@@ -14,6 +14,7 @@ return [
             'viewAny' => 'View Users',
             'delete' => 'Delete Users',
             'deleteAny' => 'Bulk Delete Users',
+            'impersonate' => 'Impersonate Users',
         ],
         'invoices' => [
             'create' => 'Create Invoices',
