65b3febe9a32f9abd0335d2cd5b509e4cc8bb900
Corwin <info@corwindev.nl>
2025-07-02 15:37:10 +0200

    New translations passwords.php (Portuguese, Brazilian)

--- lang/pt/passwords.php
+++ lang/pt/passwords.php
@@ -6,6 +6,6 @@ return [
     'sent' => 'Enviamos o link de redefinição de senha para seu e-mail.',
     'throttled' => 'Por favor, aguarde antes de tentar novamente.',
     'token' => 'O token de redefinição de senha é inválido.',
-    'user' => 'Não foi encontrado nenhum usuário com este endereço de e-mail.',
+    'user' => "Não foi encontrado nenhum usuário com este endereço de e-mail.",
 
 ];
