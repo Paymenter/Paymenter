7111f2311e3e19f702f5bbd5c8a9e336d535fb11
cst1x <costytalent981@proton.me>
2025-07-05 00:42:20 +0300

    Prevent password reset emails for users with roles (admins) and keep user on page to show err message properly

--- app/Livewire/Auth/Password/Request.php
+++ app/Livewire/Auth/Password/Request.php
@@ -25,7 +25,7 @@ class Request extends Component
         // Find the user
         $user = User::where('email', $this->email)->first();
 
-        if ($user) {
+        if ($user && !$user?->role) {
             NotificationHelper::passwordResetNotification($user, ['url' => url(route('password.reset', [
                 'token' => Password::createToken($user),
                 'email' => $user->email,
@@ -33,8 +33,6 @@ class Request extends Component
         }
 
         $this->notify('If the email address is associated with an account, you will receive an email with instructions on how to reset your password.', 'success');
-
-        $this->redirect(route('login'));
     }
 
     public function render()
