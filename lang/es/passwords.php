c099c542158d8d11039dc309dc9c1ec089779583
Corwin <info@corwindev.nl>
2025-07-10 12:47:13 +0200

    New translations passwords.php (Spanish)

--- lang/es/passwords.php
+++ lang/es/passwords.php
@@ -4,7 +4,7 @@ return [
 
     'reset' => 'Tu contraseña ha sido restablecida.',
     'sent' => 'Te hemos enviado el enlace para restablecer tu contraseña por correo electrónico.',
-    'throttled' => 'Por favor, espera antes de reintentarlo.',
+    'throttled' => 'Por favor, espere antes de reintentarlo.',
     'token' => 'La clave de enlace para cambiar la contraseña no es válida.',
     'user' => "No podemos encontrar un usuario con esa dirección de correo electrónico.",
 
