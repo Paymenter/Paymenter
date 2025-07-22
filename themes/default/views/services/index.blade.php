e5de498b030a255786861bf23f4fb1f6eeda0c1a
Mohammad Raska <42745054+Adekabang@users.noreply.github.com>
2025-07-21 23:25:03 -0400

    feat: add expired at information on services list and details

--- themes/default/views/services/index.blade.php
+++ themes/default/views/services/index.blade.php
@@ -24,10 +24,13 @@
                 @endif
             </div>
         </div>
-        <p class="text-base text-sm">Product(s): {{ $service->product->category->name }} {{ in_array($service->plan->type, ['recurring']) ? ' - ' . __('services.every_period', [
-            'period' => $service->plan->billing_period > 1 ? $service->plan->billing_period : '',
-            'unit' => trans_choice(__('services.billing_cycles.' . $service->plan->billing_unit), $service->plan->billing_period)
-        ]) : '' }}</p>
+        <p class="text-base text-sm">Product(s): {{ $service->product->category->name }} {{
+                in_array($service->plan->type, ['recurring']) ? ' - ' . __('services.every_period', [
+                'period' => $service->plan->billing_period > 1 ? $service->plan->billing_period : '',
+                'unit' => trans_choice(__('services.billing_cycles.' . $service->plan->billing_unit),
+                $service->plan->billing_period)
+                ]) : '' }} - {{ $service->expires_at ? __('services.expires_at') . ': '. $service->expires_at->format('M d, Y') : '' }}
+        </p>
         </div>
     </a>
     @endforeach
