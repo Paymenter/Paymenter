3126bcac9684a46591f39ed9454a78600ed697f8
Corwin <info@corwindev.nl>
2025-07-29 21:40:15 +0200

    New translations services.php (Norwegian)

--- lang/no/services.php
+++ lang/no/services.php
@@ -53,5 +53,5 @@ return [
     ],
     'every_period' => 'Hver :period :unit',
     'price_every_period' => ':price per :period :unit',
-    'expires_at' => 'Expires at',
+    'expires_at' => 'Utløper den',
 ];
