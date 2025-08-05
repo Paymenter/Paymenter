546658e3863d1cc8fbf43f1f619cfd24d156d33a
Corwin <info@corwindev.nl>
2025-08-05 21:16:14 +0200

    feat: attachments client side upload

--- lang/en/ticket.php
+++ lang/en/ticket.php
@@ -12,7 +12,8 @@ return [
     'attachments' => 'Attachments',
     'upload_attachments' => 'Upload Attachments',
     'or_drag_and_drop' => 'or drag and drop',
-    'attachments_max' => 'Attachments up to 10MB',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Create Ticket',
     'select_department' => 'Select Department',
