23053621696d65ce2357af04c0e1dc6365b9233e
Corwin <info@corwindev.nl>
2025-08-20 20:35:50 +0200

    New translations ticket.php (Spanish)

--- lang/es/ticket.php
+++ lang/es/ticket.php
@@ -9,6 +9,11 @@ return [
     'department' => 'Departamento',
     'created_at' => 'Creado en',
     'reply' => 'Responder',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Crear un ticket',
     'select_department' => 'Seleccionar Departamento',
