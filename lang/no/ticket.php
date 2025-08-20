3dee320e976ca47a8912b5eacc8fdc986ea58230
Corwin <info@corwindev.nl>
2025-08-20 20:35:56 +0200

    New translations ticket.php (Norwegian)

--- lang/no/ticket.php
+++ lang/no/ticket.php
@@ -9,6 +9,11 @@ return [
     'department' => 'Avdeling',
     'created_at' => 'Opprettet',
     'reply' => 'Svar',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Opprett sak',
     'select_department' => 'Velg avdeling',
