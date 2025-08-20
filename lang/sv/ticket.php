70ab193c068ff196345591ed23677aa5ed35fb83
Corwin <info@corwindev.nl>
2025-08-20 20:35:58 +0200

    New translations ticket.php (Swedish)

--- lang/sv/ticket.php
+++ lang/sv/ticket.php
@@ -9,6 +9,11 @@ return [
     'department' => 'Avdelning',
     'created_at' => 'Skapad på',
     'reply' => 'Svara',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Skapa ärende',
     'select_department' => 'Välj avdelning',
