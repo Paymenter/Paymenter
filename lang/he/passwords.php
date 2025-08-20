2a199720c6cacca0432b7412472a22760e177b01
Corwin <info@corwindev.nl>
2025-08-21 00:25:03 +0200

    New translations passwords.php (Hebrew)

--- lang/he/passwords.php
+++ lang/he/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'הססמה שלך אופסה!',
+    'sent' => 'שלחנו בדוא"ל קישור לאיפוס הסיסמה שלך!',
+    'throttled' => 'אנא המתן לפני שתנסה שוב.',
+    'token' => 'הקישור לאיפוס הסיסמה אינו תקין.',
+    'user' => "לא נמצא משתמש עם כתובת דוא\"ל זו.",
 
 ];
