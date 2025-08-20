fc713c114baf76d01b8947ec2e129eec311b01a2
Corwin <info@corwindev.nl>
2025-08-20 20:35:51 +0200

    New translations ticket.php (German)

--- lang/de/ticket.php
+++ lang/de/ticket.php
@@ -9,6 +9,11 @@ return [
     'department' => 'Abteilung',
     'created_at' => 'Erstellt am',
     'reply' => 'Antworten',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Ticket erstellen',
     'select_department' => 'Kategorie wählen',
