c0bb2ee7147c2cb496dd02cacae1f3e7ecaa6ea5
Corwin <info@corwindev.nl>
2025-08-20 20:36:02 +0200

    New translations ticket.php (Indonesian)

--- lang/id/ticket.php
+++ lang/id/ticket.php
@@ -9,6 +9,11 @@ return [
     'department' => 'Departemen',
     'created_at' => 'Dibuat Pada',
     'reply' => 'Balas',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
 
     'create_ticket' => 'Buat Tiket',
     'select_department' => 'Pilih Departemen',
