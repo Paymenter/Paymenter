f7f7fc5f565578e5ed1d1fce9657cb47cf8f4710
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-08-21 09:23:28 +0000

    style: linted files with pint

--- app/Livewire/Auth/Register.php
+++ app/Livewire/Auth/Register.php
@@ -15,10 +15,15 @@ class Register extends ComponentWithProperties
     use Captchable;
 
     public string $first_name = '';
+
     public string $last_name = '';
+
     public string $email = '';
+
     public string $password = '';
+
     public string $password_confirmation = '';
+
     public bool $tos = false;
 
     public function mount()
@@ -30,9 +35,9 @@ class Register extends ComponentWithProperties
     {
         $rules = [
             'first_name' => 'required|string|max:255',
-            'last_name'  => 'required|string|max:255',
-            'email'      => 'required|email|max:255|unique:users',
-            'password'   => 'required|string|min:8|confirmed',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
         ];
 
         if (config('settings.tos')) {
@@ -48,9 +53,9 @@ class Register extends ComponentWithProperties
 
         $user = User::create([
             'first_name' => $this->first_name,
-            'last_name'  => $this->last_name,
-            'email'      => $this->email,
-            'password'   => Hash::make($this->password),
+            'last_name' => $this->last_name,
+            'email' => $this->email,
+            'password' => Hash::make($this->password),
         ]);
 
         if (array_key_exists('properties', $validatedData)) {
