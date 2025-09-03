35c5fb4e46354df7e7e44a204a81339b8587320c
Corwin <info@corwindev.nl>
2025-09-03 21:34:57 +0200

    New translations services.php (Norwegian)

--- lang/no/services.php
+++ lang/no/services.php
@@ -44,6 +44,7 @@ return [
         'active' => 'Aktiv',
         'cancelled' => 'Kansellert',
         'suspended' => 'Suspendert',
+        'cancellation_pending' => 'Cancellation Pending',
     ],
     'billing_cycles' => [
         'day' => 'dag|dager',
