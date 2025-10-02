f65e42f86565ef739b1136852f4707d75a6b18cc
Corwin <info@corwindev.nl>
2025-10-03 01:13:27 +0200

    New translations admin.php (Italian)

--- lang/it/admin.php
+++ lang/it/admin.php
@@ -3,12 +3,12 @@
 return [
 
     'cronjob' => [
-        'invoices_created' => 'Invoices created',
-        'orders_cancelled' => 'Orders cancelled',
-        'upgrade_invoices_updated' => 'Upgrade invoices updated',
-        'services_suspended' => 'Services suspended',
-        'services_terminated' => 'Services terminated',
-        'tickets_closed' => 'Tickets closed',
-        'email_logs_deleted' => 'Email logs deleted',
+        'invoices_created' => 'Fatture create',
+        'orders_cancelled' => 'Ordini annullati',
+        'upgrade_invoices_updated' => 'Aggiornamento fatture aggiornate',
+        'services_suspended' => 'Servizi sospesi',
+        'services_terminated' => 'Servizi terminati',
+        'tickets_closed' => 'Tickets Chiuso',
+        'email_logs_deleted' => 'Log email eliminati',
     ],
 ];
