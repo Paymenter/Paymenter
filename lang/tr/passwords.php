6f41a5005afb2814a4cc49b99fd39d0839b8dc70
Corwin <info@corwindev.nl>
2025-10-09 17:09:49 +0200

    New translations passwords.php (Turkish)

--- lang/tr/passwords.php
+++ lang/tr/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Şifreniz sıfırlandı.',
+    'sent' => 'Şifre sıfırlama bağlantısı e-posta adresinize gönderildi.',
+    'throttled' => 'Yeniden denemeden önce lütfen bir süre bekleyin.',
+    'token' => 'Şifre sıfırlama kodu geçersiz.',
+    'user' => "Bu e-posta adresine sahip bir kullanıcı bulunamadı.",
 
 ];
