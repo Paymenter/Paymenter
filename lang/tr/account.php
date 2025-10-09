c8110d459565480c9a147016dc239506f299cd74
Corwin <info@corwindev.nl>
2025-10-09 13:59:50 +0200

    New translations account.php (Turkish)

---
+++ lang/tr/account.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'account' => 'Account',
+    'personal_details' => 'Personal details',
+    'security' => 'Security',
+    'credits' => 'Credits',
+
+    'change_password' => 'Change password',
+
+    'two_factor_authentication' => 'Two-factor authentication',
+    'two_factor_authentication_description' => 'Add an extra layer of security to your account by enabling two-factor authentication.',
+    'two_factor_authentication_enabled' => 'Two-factor authentication is enabled for your account.',
+    'two_factor_authentication_enable' => 'Enable two-factor authentication',
+    'two_factor_authentication_disable' => 'Disable two-factor authentication',
+    'two_factor_authentication_enable_description' => 'To enable two factor authentication, you need to scan the QR code below with an authenticator app like Google Authenticator or Authy.',
+    'two_factor_authentication_qr_code' => 'Scan the QR code below with your authenticator app:',
+    'two_factor_authentication_secret' => 'Or enter the following code manually:',
+
+    'sessions' => 'Sessions',
+    'sessions_description' => 'Manage and log out your active sessions on other browsers and devices.',
+    'logout_sessions' => 'Log this session out',
+
+    'input' => [
+        'current_password' => 'Current password',
+        'current_password_placeholder' => 'Your current password',
+        'new_password' => 'New password',
+        'new_password_placeholder' => 'Your new password',
+        'confirm_password' => 'Confirm password',
+        'confirm_password_placeholder' => 'Confirm your new password',
+
+        'two_factor_code' => 'Enter the code from your authenticator app',
+        'two_factor_code_placeholder' => 'Your two-factor authentication code',
+
+        'currency' => 'Currency',
+        'amount' => 'Amount',
+    ],
+
+    'notifications' => [
+        'password_changed' => 'Password has been changed.',
+        'password_incorrect' => 'The current password is incorrect.',
+        'two_factor_enabled' => 'Two-factor authentication has been enabled.',
+        'two_factor_disabled' => 'Two-factor authentication has been disabled.',
+        'two_factor_code_incorrect' => 'The code is incorrect.',
+        'session_logged_out' => 'Session has been logged out.',
+    ],
+
+    'no_credit' => 'You have no credits.',
+    'add_credit' => 'Add credit',
+    'credit_deposit' => 'Credit deposit (:currency)',
+
+];
