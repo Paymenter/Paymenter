46c00eff98d86df4ba9609776dfea6473ef1e1aa
Corwin <info@corwindev.nl>
2025-10-09 14:00:01 +0200

    New translations ticket.php (Turkish)

---
+++ lang/tr/ticket.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'ticket_details' => 'Ticket Details',
+    'tickets' => 'Tickets',
+    'subject' => 'Subject',
+    'status' => 'Status',
+    'priority' => 'Priority',
+    'department' => 'Department',
+    'created_at' => 'Created At',
+    'reply' => 'Reply',
+    'attachments' => 'Attachments',
+    'upload_attachments' => 'Upload Attachments',
+    'or_drag_and_drop' => 'or drag and drop',
+    'files_max' => 'Files up to 10MB',
+    'selected_files' => 'Selected Files',
+
+    'create_ticket' => 'Create Ticket',
+    'select_department' => 'Select Department',
+    'select_priority' => 'Select Priority',
+    'low' => 'Low',
+    'medium' => 'Medium',
+    'high' => 'High',
+
+    'last_activity' => 'Last activity',
+
+    'service' => 'Service',
+    'select_service' => 'Select Service',
+    'create' => 'Create',
+];
