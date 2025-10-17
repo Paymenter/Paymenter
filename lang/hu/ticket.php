4dc265cb9113c80b98f32f02f20fafb03f4a45f7
Corwin <info@corwindev.nl>
2025-10-17 18:57:20 +0200

    New translations ticket.php (Hungarian)

--- lang/hu/ticket.php
+++ lang/hu/ticket.php
@@ -11,9 +11,9 @@ return [
     'reply' => 'Válasz',
     'attachments' => 'Mellékletek',
     'upload_attachments' => 'Csatolmány feltöltése',
-    'or_drag_and_drop' => 'or drag and drop',
-    'files_max' => 'Files up to 10MB',
-    'selected_files' => 'Selected Files',
+    'or_drag_and_drop' => 'vagy húzd ide',
+    'files_max' => 'Legfeljebb 10MB méretű fájlok',
+    'selected_files' => 'Fájlok kiválasztása',
 
     'create_ticket' => 'Hibajegy létrehozása',
     'select_department' => 'Részleg kiválasztása',
