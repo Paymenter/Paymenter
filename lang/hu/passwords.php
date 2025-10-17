6ebcd6fa1a7b13feb982b49184ebfbabc0d84a72
Corwin <info@corwindev.nl>
2025-10-17 18:00:39 +0200

    New translations passwords.php (Hungarian)

--- lang/hu/passwords.php
+++ lang/hu/passwords.php
@@ -2,10 +2,10 @@
 
 return [
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'A jelszavad visszaállítva.',
+    'sent' => 'E-mailben elküldtük a jelszó visszaállításához szükséges linket.',
+    'throttled' => 'Kérjük, várj, mielőtt újra megpróbálnád.',
+    'token' => 'Ez a jelszó visszaállító token érvénytelen.',
+    'user' => "Nem található felhasználó a megadott email címmel.",
 
 ];
