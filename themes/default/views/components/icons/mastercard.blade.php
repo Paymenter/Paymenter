9a9f1be266e362716cd1ec9bcecdbba249bd9708
iamdarkle <tomasromero@outlook.com>
2025-10-18 19:36:31 +0200

    feat: add new component icons

---
+++ themes/default/views/components/icons/mastercard.blade.php
@@ -0,0 +1,6 @@
+<svg {{ $attributes }} viewBox="0 0 24 16" fill="none" xmlns="http://www.w3.org/2000/svg" role="presentation" focusable="false" class="p-Logo p-Logo--md p-CardBrandIcon">
+    <rect fill="#252525" height="16" rx="2" width="24"></rect>
+    <circle cx="9" cy="8" fill="#eb001b" r="5"></circle>
+    <circle cx="15" cy="8" fill="#f79e1b" r="5"></circle>
+    <path d="M12 4c1.214.912 2 2.364 2 4s-.786 3.088-2 4c-1.214-.912-2-2.364-2-4s.786-3.088 2-4z" fill="#ff5f00"></path>
+</svg>
---
+++ themes/default/views/components/icons/stripe.blade.php
@@ -0,0 +1,4 @@
+<svg {{ $attributes }} viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="512" height="512" fill="#533AFD" />
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M128 384L384 329.71V128L128 182.924V384Z" fill="white" />
+</svg>
