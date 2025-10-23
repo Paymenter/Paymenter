8600ef99e70a548b6471a6a2ab38696d9b1d3a97
Corwin <info@corwindev.nl>
2025-10-23 04:13:25 +0200

    New translations ticket.php (Portuguese, Brazilian)

--- lang/pt/ticket.php
+++ lang/pt/ticket.php
@@ -9,11 +9,11 @@ return [
     'department' => 'Departamento',
     'created_at' => 'Criado em',
     'reply' => 'Responder',
-    'attachments' => 'Attachments',
-    'upload_attachments' => 'Upload Attachments',
-    'or_drag_and_drop' => 'or drag and drop',
-    'files_max' => 'Files up to 10MB',
-    'selected_files' => 'Selected Files',
+    'attachments' => 'Documentos',
+    'upload_attachments' => 'Carregar Anexos',
+    'or_drag_and_drop' => 'ou arraste e solte',
+    'files_max' => 'Arquivos com até 10MB',
+    'selected_files' => 'Arquivos Selecionados',
 
     'create_ticket' => 'Criar ticket',
     'select_department' => 'Selecionar departamento',
@@ -27,5 +27,5 @@ return [
     'service' => 'Serviço',
     'select_service' => 'Selecionar serviço',
     'create' => 'Criar',
-    'no_tickets' => 'No tickets found',
+    'no_tickets' => 'Nenhum ticket encontrado',
 ];
