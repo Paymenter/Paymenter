83aa3e323727894b6aa20c71107f322ce9582a08
Corwin <info@corwindev.nl>
2025-10-23 05:35:03 +0200

    New translations admin.php (Portuguese, Brazilian)

--- lang/pt/admin.php
+++ lang/pt/admin.php
@@ -5,7 +5,7 @@ return [
     'cronjob' => [
         'invoices_created' => 'Faturas criadas',
         'orders_cancelled' => 'Pedidos cancelados',
-        'upgrade_invoices_updated' => 'Upgrade invoices updated',
+        'upgrade_invoices_updated' => 'Atualização de faturas atualizadas',
         'services_suspended' => 'Serviços suspensos',
         'services_terminated' => 'Serviços encerrados',
         'tickets_closed' => 'Tickets fechados',
