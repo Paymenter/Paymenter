b3b66efd6ac97c6774275963cb7bb4c3df9005cd
26bz <26bz@proton.me>
2025-10-26 09:29:45 -0400

    fix(easymde): initialize editor after bundle load

--- themes/default/js/easymde-entry.js
+++ themes/default/js/easymde-entry.js
@@ -2,3 +2,5 @@ import EasyMDE from 'easymde';
 import 'easymde/dist/easymde.min.css';
 
 window.EasyMDE = EasyMDE;
+
+document.dispatchEvent(new CustomEvent('easymde:ready'));
--- themes/default/views/components/easymde-editor.blade.php
+++ themes/default/views/components/easymde-editor.blade.php
@@ -4,65 +4,73 @@
 
 @script
     <script>
-        const editor = new EasyMDE({
-            element: document.getElementById('editor'),
-            spellChecker: false,
-            previewImagesInEditor: true,
-            autoDownloadFontAwesome: false,
-            status: [{
-                className: 'upload-image',
-                defaultValue: '',
-            }],
-            toolbar: [{
-                    name: 'bold',
-                    action: EasyMDE.toggleBold,
-                }, {
-                    name: 'italic',
-                    action: EasyMDE.toggleItalic,
-                }, {
-                    name: 'strikethrough',
-                    action: EasyMDE.toggleStrikethrough,
-                }, {
-                    name: 'link',
-                    action: EasyMDE.drawLink,
-                }, '|',
-                {
-                    name: 'heading',
-                    action: EasyMDE.toggleHeadingSmaller,
-                }, '|',
-                {
-                    name: 'quote',
-                    action: EasyMDE.toggleBlockquote,
-                }, {
-                    name: 'code',
-                    action: EasyMDE.toggleCodeBlock,
+        const initializeEditor = () => {
+            const editor = new EasyMDE({
+                element: document.getElementById('editor'),
+                spellChecker: false,
+                previewImagesInEditor: true,
+                autoDownloadFontAwesome: false,
+                status: [{
+                    className: 'upload-image',
+                    defaultValue: '',
+                }],
+                toolbar: [{
+                        name: 'bold',
+                        action: EasyMDE.toggleBold,
+                    }, {
+                        name: 'italic',
+                        action: EasyMDE.toggleItalic,
+                    }, {
+                        name: 'strikethrough',
+                        action: EasyMDE.toggleStrikethrough,
+                    }, {
+                        name: 'link',
+                        action: EasyMDE.drawLink,
+                    }, '|',
+                    {
+                        name: 'heading',
+                        action: EasyMDE.toggleHeadingSmaller,
+                    }, '|',
+                    {
+                        name: 'quote',
+                        action: EasyMDE.toggleBlockquote,
+                    }, {
+                        name: 'code',
+                        action: EasyMDE.toggleCodeBlock,
 
-                }, {
-                    name: 'unordered-list',
-                    action: EasyMDE.toggleUnorderedList,
-                }, {
-                    name: 'ordered-list',
-                    action: EasyMDE.toggleOrderedList,
-                }, '|',
-                {
-                    name: 'undo',
-                    action: EasyMDE.undo,
-                }, {
-                    name: 'redo',
-                    action: EasyMDE.redo,
-                },
+                    }, {
+                        name: 'unordered-list',
+                        action: EasyMDE.toggleUnorderedList,
+                    }, {
+                        name: 'ordered-list',
+                        action: EasyMDE.toggleOrderedList,
+                    }, '|',
+                    {
+                        name: 'undo',
+                        action: EasyMDE.undo,
+                    }, {
+                        name: 'redo',
+                        action: EasyMDE.redo,
+                    },
 
-            ],
-        });
+                ],
+            });
 
-        editor.codemirror.on('change', function() {
-            @this.set('message', editor.value(), false);
-        });
+            editor.codemirror.on('change', function() {
+                @this.set('message', editor.value(), false);
+            });
 
-        // Listen for event called saved
-        $wire.on('saved', () => {
-            editor.clearAutosavedValue();
-            editor.value('');
-        });
+            // Listen for event called saved
+            $wire.on('saved', () => {
+                editor.clearAutosavedValue();
+                editor.value('');
+            });
+        };
+
+        if (window.EasyMDE) {
+            initializeEditor();
+        } else {
+            document.addEventListener('easymde:ready', initializeEditor, { once: true });
+        }
     </script>
 @endscript
