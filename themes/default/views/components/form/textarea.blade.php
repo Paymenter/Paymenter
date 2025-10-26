a691c8611732e0fb876b848dda6a97550f0d0b2b
26bz <26bz@proton.me>
2025-10-25 22:30:32 -0400

    feat: align form component labels above inputs

--- themes/default/views/components/form/textarea.blade.php
+++ themes/default/views/components/form/textarea.blade.php
@@ -10,17 +10,14 @@
 'hideRequiredIndicator' => false,
 'dirty' => false,
 ])
-<fieldset class="flex flex-col relative mt-3 w-full {{ $divClass ?? '' }}">
+<fieldset class="flex flex-col mt-3 w-full {{ $divClass ?? '' }}">
     @if ($label)
-    <legend>
-        <label for="{{ $name }}"
-            class="text-sm text-primary-100 absolute -translate-y-1/2 start-1 ml-1 bg-primary-800 px-2 rounded-md">
-            {{ $label }}
-            @if ($required && !$hideRequiredIndicator)
-            <span class="text-red-500">*</span>
-            @endif
-        </label>
-    </legend>
+    <label for="{{ $name }}" class="mb-1 text-sm text-primary-100">
+        {{ $label }}
+        @if ($required && !$hideRequiredIndicator)
+        <span class="text-red-500">*</span>
+        @endif
+    </label>
     @endif
     <textarea type="{{ $type ?? 'text' }}" id="{{ $id ?? $name }}" name="{{ $name }}"
         class="block w-full text-sm text-primary-100 bg-primary-800 border-2 border-neutral rounded-md outline-none focus:outline-none focus:border-secondary transition-all duration-300 ease-in-out disabled:bg-primary-700 disabled:cursor-not-allowed {{ $class ?? '' }} @if ($type !== 'color') px-2.5 py-2.5 @endif"
