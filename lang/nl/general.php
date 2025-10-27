042483ea9faf567b5baa2f447e407984ac3954de
Corwin <info@corwindev.nl>
2025-10-27 22:31:05 +0100

    New translations general.php (Dutch)

--- lang/nl/general.php
+++ lang/nl/general.php
@@ -28,6 +28,9 @@ return [
 
         'password' => 'Wachtwoord',
         'password_placeholder' => 'Uw wachtwoord',
+
+        'password_confirmation' => 'Confirm Password',
+        'password_confirmation_placeholder' => 'Confirm your password',
     ],
 
     'all_rights_reserved' => 'Alle rechten voorbehouden.',
