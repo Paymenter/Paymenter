105400c7fffdc97bbe692b38bc589bc9d753b08e
Corwin <info@corwindev.nl>
2025-10-27 22:31:08 +0100

    New translations general.php (Swedish)

--- lang/sv/general.php
+++ lang/sv/general.php
@@ -28,6 +28,9 @@ return [
 
         'password' => 'Lösenord',
         'password_placeholder' => 'Ditt lösenord',
+
+        'password_confirmation' => 'Confirm Password',
+        'password_confirmation_placeholder' => 'Confirm your password',
     ],
 
     'all_rights_reserved' => 'Alla rättigheter reserverade.',
