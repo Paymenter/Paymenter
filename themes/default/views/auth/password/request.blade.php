c3857acb385cebe5a8fd3fa63d09d2a82f712fc3
26bz <26bz@proton.me>
2025-10-28 12:17:55 -0400

    fix(theme): align reset password layout to match the other auth pages

--- themes/default/views/auth/password/request.blade.php
+++ themes/default/views/auth/password/request.blade.php
@@ -1,9 +1,8 @@
-<form
-    class="flex flex-col gap-2 max-w-md mx-auto px-6 sm:px-14 pb-10 bg-primary-800 rounded-md"
+<form class="mx-auto flex flex-col gap-2 mt-4 px-6 sm:px-14 pb-10 bg-primary-800 rounded-md xl:max-w-[40%] w-full"
     wire:submit="submit" id="reset">
-    <div class="flex flex-col items-center mt-4 mb-10">
+    <div class="flex flex-col items-center my-14">
         <x-logo class="h-10" />
-        <h1 class="text-2xl text-center mt-2">{{ __('auth.reset_password') }} </h1>
+        <h1 class="text-2xl text-center mt-6">{{ __('auth.reset_password') }} </h1>
     </div>
     <x-form.input name="email" type="text" :label="__('general.input.email')" :placeholder="__('general.input.email_placeholder')" wire:model="email" required />
 
--- themes/default/views/auth/password/reset.blade.php
+++ themes/default/views/auth/password/reset.blade.php
@@ -1,16 +1,14 @@
-<form class="flex flex-col gap-2 max-w-md mx-auto px-6 sm:px-14 pb-10 bg-primary-800 rounded-md" wire:submit="submit"
-    id="reset">
-    <div class="flex flex-col items-center mt-4 mb-10">
+<form class="mx-auto flex flex-col gap-2 mt-4 px-6 sm:px-14 pb-10 bg-primary-800 rounded-md xl:max-w-[40%] w-full"
+    wire:submit="submit" id="reset">
+    <div class="flex flex-col items-center my-14">
         <x-logo class="h-10" />
-        <h1 class="text-2xl text-center mt-2">{{ __('auth.reset_password') }} </h1>
+        <h1 class="text-2xl text-center mt-6">{{ __('auth.reset_password') }} </h1>
     </div>
-    <x-form.input name="email" type="text" :label="__('general.input.email')" :placeholder="__('general.input.email_placeholder')"
-        wire:model="email" required disabled />
+    <x-form.input name="email" type="text" :label="__('general.input.email')" :placeholder="__('general.input.email_placeholder')" wire:model="email" required disabled />
 
-    <x-form.input name="password" type="password" :label="__('general.input.password')" :placeholder="__('general.input.password_placeholder')"
-        wire:model="password" required />
-    <x-form.input name="password_confirm" type="password" :label="__('general.input.password_confirmation')"
-        :placeholder="__('general.input.password_confirmation_placeholder')" wire:model="password_confirmation" required />
+    <x-form.input name="password" type="password" :label="__('general.input.password')" :placeholder="__('general.input.password_placeholder')" wire:model="password" required />
+    <x-form.input name="password_confirm" type="password" :label="__('general.input.password_confirmation')" :placeholder="__('general.input.password_confirmation_placeholder')"
+        wire:model="password_confirmation" required />
 
     <x-captcha :form="'reset'" />
 
