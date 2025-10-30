832a4c13dce7a08ef5e32ac37abd7d6cfd5dcf57
Corwin <info@corwindev.nl>
2025-10-30 11:06:07 +0100

    New translations ticket.php (Hungarian)

--- lang/hu/ticket.php
+++ lang/hu/ticket.php
@@ -14,6 +14,10 @@ return [
     'or_drag_and_drop' => 'vagy húzd ide',
     'files_max' => 'Legfeljebb 10MB méretű fájlok',
     'selected_files' => 'Fájlok kiválasztása',
+    'close_ticket' => 'Close Ticket',
+    'close_ticket_confirmation' => 'Are you sure you want to close this ticket? This action cannot be undone.',
+    'close_ticket_success' => 'Ticket closed successfully.',
+    'close_ticket_already_closed' => 'This ticket is already closed.',
 
     'create_ticket' => 'Hibajegy létrehozása',
     'select_department' => 'Részleg kiválasztása',
