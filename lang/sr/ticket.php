c8efff944a6cbeb360d5352354597525e66486c9
Corwin <info@corwindev.nl>
2025-10-30 11:06:26 +0100

    New translations ticket.php (Serbian (Latin))

--- lang/sr/ticket.php
+++ lang/sr/ticket.php
@@ -14,6 +14,10 @@ return [
     'or_drag_and_drop' => 'ili prevuci i otpusti',
     'files_max' => 'Fajlovi do 10MB',
     'selected_files' => 'Izabrani fajlovi',
+    'close_ticket' => 'Close Ticket',
+    'close_ticket_confirmation' => 'Are you sure you want to close this ticket? This action cannot be undone.',
+    'close_ticket_success' => 'Ticket closed successfully.',
+    'close_ticket_already_closed' => 'This ticket is already closed.',
 
     'create_ticket' => 'Kreiraj tiket',
     'select_department' => 'Izaberi odeljenje',
