45ac326d21fcadd7f929f90b55230a2c48a4c9b0
Corwin <info@corwindev.nl>
2025-10-30 11:06:09 +0100

    New translations ticket.php (Spanish)

--- lang/es/ticket.php
+++ lang/es/ticket.php
@@ -14,6 +14,10 @@ return [
     'or_drag_and_drop' => 'o arrastra y suelta',
     'files_max' => 'Archivos de hasta 10 Mb',
     'selected_files' => 'Archivos seleccionados',
+    'close_ticket' => 'Close Ticket',
+    'close_ticket_confirmation' => 'Are you sure you want to close this ticket? This action cannot be undone.',
+    'close_ticket_success' => 'Ticket closed successfully.',
+    'close_ticket_already_closed' => 'This ticket is already closed.',
 
     'create_ticket' => 'Crear un ticket',
     'select_department' => 'Seleccionar Departamento',
