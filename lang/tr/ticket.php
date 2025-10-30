2f00a387998573dca1a20d9afb7a89a191c5380d
Corwin <info@corwindev.nl>
2025-10-30 11:06:06 +0100

    New translations ticket.php (Turkish)

--- lang/tr/ticket.php
+++ lang/tr/ticket.php
@@ -14,6 +14,10 @@ return [
     'or_drag_and_drop' => 'ya da sürükleyip bırakın',
     'files_max' => 'Dosyalar en fazla 10 MB olabilir.',
     'selected_files' => 'Seçili Dosyalar',
+    'close_ticket' => 'Close Ticket',
+    'close_ticket_confirmation' => 'Are you sure you want to close this ticket? This action cannot be undone.',
+    'close_ticket_success' => 'Ticket closed successfully.',
+    'close_ticket_already_closed' => 'This ticket is already closed.',
 
     'create_ticket' => 'Destek Talebi Oluştur',
     'select_department' => 'Birim Seç',
