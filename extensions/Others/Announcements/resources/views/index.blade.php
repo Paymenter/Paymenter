8e31aa9cce1d25b9a9d5b4f079f3e5a23ecb1cb2
Chenyang Gao <gps949@outlook.com>
2025-11-01 10:10:30 -0700

    fix(ui): Align buttons on homepage cards
    Signed-off-by: Chenyang Gao <gps949@outlook.com>

--- extensions/Others/Announcements/resources/views/index.blade.php
+++ extensions/Others/Announcements/resources/views/index.blade.php
@@ -6,12 +6,12 @@
 
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             @foreach($announcements as $announcement)
-            <div class="bg-background-secondary hover:bg-background-secondary/80 border border-neutral p-4 rounded-lg">
+            <div class="flex flex-col bg-background-secondary hover:bg-background-secondary/80 border border-neutral p-4 rounded-lg">
                 <h2 class="text-xl font-bold">{{ $announcement->title }}</h2>
                 <div class="my-2 text-sm text-base/70">
                     {{ $announcement->description }}
                 </div>
-                <a href="{{ route('announcements.show', $announcement) }}" wire:navigate>
+                <a href="{{ route('announcements.show', $announcement) }}" wire:navigate class="mt-auto pt-2">
                     <x-button.primary>
                         {{ __('common.button.view') }}
                     </x-button.primary>
