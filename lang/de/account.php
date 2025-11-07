7d8fcffae31fb7b4c31e711b35fbdb037550b088
Corwin <info@corwindev.nl>
2025-11-07 22:35:25 +0100

    New translations account.php (German)

--- lang/de/account.php
+++ lang/de/account.php
@@ -13,7 +13,7 @@ return [
     'two_factor_authentication_enabled' => 'Zwei-Faktor-Authentifizierung wurde deinem Account hinzugefügt.',
     'two_factor_authentication_enable' => 'Zwei-Faktor-Authentifizierung aktivieren',
     'two_factor_authentication_disable' => 'Zwei-Faktor-Authentifizierung deaktivieren',
-    'two_factor_authentication_disable_description' => 'Are you sure you want to disable two-factor authentication? This will remove the extra layer of security from your account.',
+    'two_factor_authentication_disable_description' => 'Sind Sie sicher, dass Sie die Zwei-Faktor-Authentifizierung deaktivieren möchten? Dadurch wird die zusätzliche Sicherheitsebene Ihres Kontos entfernt.',
     'two_factor_authentication_enable_description' => 'Zur Aktivierung der Zwei-Faktor-Authentifizierung, müssen Sie den folgenden QR-Code mit einer Authenticator App, wie Google Auth oder Authy, scannen.',
     'two_factor_authentication_qr_code' => 'Scanne den folgenden QR-Code mit deiner Authenticator App:',
     'two_factor_authentication_secret' => 'Oder gib den folgenden Code manuell ein:',
@@ -35,7 +35,7 @@ return [
 
         'currency' => 'Währung',
         'amount' => 'Anzahl',
-        'payment_gateway' => 'Payment gateway',
+        'payment_gateway' => 'Zahlungs-Gateway',
     ],
 
     'notifications' => [
@@ -49,11 +49,11 @@ return [
 
     'no_credit' => 'Du hast kein Guthaben.',
     'add_credit' => 'Guthaben hinzufügen',
-    'credit_deposit' => 'Guthabeneinzahlung (:currency)',
+    'credit_deposit' => 'Guthaben (:currency)',
 
     'payment_methods' => 'Payment Methods',
     'recent_transactions' => 'Recent Transactions',
-    'saved_payment_methods' => 'Saved Payment Methods',
+    'saved_payment_methods' => 'Gespeicherte Zahlungsarten',
     'setup_payment_method' => 'Set up a new payment method',
     'no_saved_payment_methods' => 'You have no saved payment methods.',
     'saved_payment_methods_description' => 'Manage your saved payment methods for faster checkout and automatic payments.',
@@ -82,8 +82,8 @@ return [
     'push_notifications_description' => 'Enable push notifications to receive real-time updates directly in your browser, even when you are not on the site.',
     'enable_push_notifications' => 'Enable Push Notifications',
     'push_status' => [
-        'not_supported' => 'Push notifications are not supported by your browser.',
-        'denied' => 'Push notifications are blocked. Please enable them in your browser settings.',
+        'not_supported' => 'Push-Mitteilungen werden von Ihrem Browser nicht unterstützt.',
+        'denied' => 'Push-Mitteilungen sind gesperrt. Bitte aktivieren Sie diese in Ihren Browser-Einstellungen.',
         'subscribed' => 'Push notifications are enabled.',
     ],
 ];
