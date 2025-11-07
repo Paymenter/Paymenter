e4713151a9386f37dc74aa0ff753b125ddc1e284
Corwin <info@corwindev.nl>
2025-11-08 00:07:46 +0100

    New translations admin.php (German)

--- lang/de/admin.php
+++ lang/de/admin.php
@@ -6,7 +6,7 @@ return [
         'invoices_created' => 'Erstellte Rechnungen',
         'orders_cancelled' => 'Stornierte Bestellungen',
         'upgrade_invoices_updated' => 'Aktualisierte Upgrade-Rechnungen',
-        'services_suspended' => 'Services suspended',
+        'services_suspended' => 'Dienste gesperrt',
         'services_terminated' => 'Dienste beendet',
         'tickets_closed' => 'Tickets geschlossen',
         'email_logs_deleted' => 'E-Mail-Protokolle gelöscht',
