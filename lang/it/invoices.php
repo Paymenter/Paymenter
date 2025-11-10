6c575eb347232ae07b98160a974530ef9ab14465
Corwin <info@corwindev.nl>
2025-11-10 13:22:10 +0100

    New translations invoices.php (Italian)

--- lang/it/invoices.php
+++ lang/it/invoices.php
@@ -30,6 +30,7 @@ return [
     'gateway' => 'Gateway',
     'payment_date' => 'Data pagamento',
     'payment_method' => 'Metodo di Pagamento',
+    'download_pdf' => 'Download PDF',
     'amount' => 'Quantità',
     'date' => 'Data',
     'due_date' => 'Data di scadenza',
@@ -47,10 +48,13 @@ return [
     'apply_credit' => 'Apply Credit ( :amount )',
     'no_invoices' => 'No invoices found.',
     'paid_with_credits' => 'Paid with Credits',
+    'available_credits' => 'Available Credits: :amount',
+    'account_credits' => 'Account Credits',
     'one_time_payment' => 'One-Time Payment',
     'use_for_recurring' => 'Use for Recurring Payments',
     'pay_now' => 'Pay Now',
     'apply_credits_and_continue' => 'Apply Credits and Continue',
     'apply_credits_and_pay' => 'Apply Credits and Pay',
+    'amount_due' => 'Amount Due: :amount',
 
 ];
