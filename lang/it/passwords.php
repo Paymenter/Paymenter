f3dda3af2fc8a615c34e36692bcabf5ca0cf2ef8
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2025-11-12 10:32:58 +0000

    style: linted files with pint

--- lang/it/passwords.php
+++ lang/it/passwords.php
@@ -6,6 +6,6 @@ return [
     'sent' => 'Ti abbiamo inviato una email con il link per il reset della password!',
     'throttled' => 'Per favore, attendi prima di riprovare.',
     'token' => 'Questo token per reimpostare la password non è valido.',
-    'user' => "Non riusciamo a trovare un utente con questo indirizzo email.",
+    'user' => 'Non riusciamo a trovare un utente con questo indirizzo email.',
 
 ];
