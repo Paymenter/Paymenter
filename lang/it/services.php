8ecc2f00bcf3fa15dc93e367e70f471db23f2c2f
Corwin <info@corwindev.nl>
2025-12-31 17:00:09 +0100

    New translations services.php (Italian)

--- lang/it/services.php
+++ lang/it/services.php
@@ -55,8 +55,8 @@ return [
     'every_period' => 'Ogni :period :unit',
     'price_every_period' => ':price al :period :unit',
     'price_one_time' => ':price una volta',
-    'renews_in' => 'Renews in',
-    'renews_on' => 'Renews on',
+    'renews_in' => 'Si rinnova in',
+    'renews_on' => 'Si rinnova il',
     'auto_pay' => 'Pagamento automatico tramite',
     'auto_pay_not_configured' => 'Non configurato',
 
@@ -68,9 +68,9 @@ return [
     'remove_payment_method' => 'Rimuovi il metodo di pagamento',
     'remove_payment_method_confirm' => 'Sei sicuro di voler rimuovere il metodo di pagamento ":name" da questo servizio? Il tuo servizio non sarà più in grado di pagare le fatture in automatico.',
 
-    'label' => 'Label',
-    'label_placeholder' => 'Enter a custom label for this service',
-    'label_modal_title' => 'Edit Service Label',
-    'update_label' => 'Update Label',
+    'label' => 'Etichetta',
+    'label_placeholder' => 'Inserire un\'etichetta personalizzata per questo servizio',
+    'label_modal_title' => 'Modifica Etichetta Servizio',
+    'update_label' => 'Aggiorna etichetta',
 
 ];
