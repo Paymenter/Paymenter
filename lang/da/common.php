992ce490f33cf1a0acb5c5e6cde50c64d26b20f5
Corwin <info@corwindev.nl>
2026-01-12 14:35:35 +0100

    New translations common.php (Danish)

---
+++ lang/da/common.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    '' => '',
+
+    'button' => [
+        'view' => 'View',
+        'view_all' => 'View All',
+    ],
+    'confirm' => 'Confirm',
+    'cancel' => 'Cancel',
+    'select' => [
+        'placeholder' => 'Please select :name',
+    ],
+];
