9a692cb8643e372b24dd8e006b427b0ad3ee7f4b
Corwin <info@corwindev.nl>
2026-01-12 14:35:23 +0100

    New translations general.php (Danish)

---
+++ lang/da/general.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    'input' => [
+        'first_name' => 'First name',
+        'first_name_placeholder' => 'Your first name',
+        'last_name' => 'Last name',
+        'last_name_placeholder' => 'Your last name',
+        'email' => 'Email',
+        'email_placeholder' => 'Your email address',
+        'phone' => 'Phone',
+        'phone_placeholder' => 'Your phone number',
+        'company_name' => 'Company name',
+        'company_name_placeholder' => 'Your company name',
+        'country' => 'Country',
+        'country_placeholder' => 'Your country',
+        'city' => 'City',
+        'city_placeholder' => 'Your city',
+        'address' => 'Address',
+        'address_placeholder' => 'Your address',
+        'address2' => 'Address 2',
+        'address2_placeholder' => 'Your address 2',
+        'zip' => 'ZIP',
+        'zip_placeholder' => 'Your ZIP code',
+        'state' => 'State',
+        'state_placeholder' => 'Your state',
+
+        'password' => 'Password',
+        'password_placeholder' => 'Your password',
+
+        'password_confirmation' => 'Confirm Password',
+        'password_confirmation_placeholder' => 'Confirm your password',
+    ],
+
+    'all_rights_reserved' => 'All rights reserved.',
+    'update' => 'Update',
+    'save' => 'Save',
+];
