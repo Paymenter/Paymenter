2c87c1044b29c4198b57096653d9cd3ee9029f40
Corwin <info@corwindev.nl>
2026-01-12 14:35:33 +0100

    New translations affiliate.php (Danish)

---
+++ extensions/Others/Affiliates/resources/lang/da/affiliate.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'affiliate' => 'Affiliate',
+    'signup-for-affiliate' => 'Signup for Affiliate',
+    'code' => 'Code',
+    'visitors' => 'Visitors',
+    'total-visitors' => 'Total Visitors',
+    'signups' => 'Signups',
+    'total-signups' => 'Total Signups',
+    'earnings' => 'Earnings',
+    'total-earnings' => 'Total Earnings',
+    'your-affiliate-link' => 'Your affiliate link',
+    'copy' => 'Copy',
+
+    'you-havent-signed-up-yet' => 'You have not signed up for affiliates program yet!',
+    'you-are-already-affiliated' => 'You have already signed up for affiliate program.',
+    'signup-success' => 'Successfully signed up for affiliate program!',
+];
