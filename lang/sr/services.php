1d4d0e908539d65f9370d5b9a3ed6947ae2aa6eb
Corwin <info@corwindev.nl>
2026-01-15 14:44:15 +0100

    New translations services.php (Serbian (Latin))

--- lang/sr/services.php
+++ lang/sr/services.php
@@ -55,8 +55,8 @@ return [
     'every_period' => 'Svakih :period :unit',
     'price_every_period' => ':price po :period :unit',
     'price_one_time' => ':price jednokratno',
-    'renews_in' => 'Renews in',
-    'renews_on' => 'Renews on',
+    'renews_in' => 'Obnavlja se za',
+    'renews_on' => 'Obnavlja se dana',
     'auto_pay' => 'Automatsko plaćanje putem',
     'auto_pay_not_configured' => 'Nije konfigurisano',
 
@@ -68,9 +68,9 @@ return [
     'remove_payment_method' => 'Ukloni način plaćanja',
     'remove_payment_method_confirm' => 'Da li ste sigurni da želite da uklonite način plaćanja ":name" sa ove usluge? Vaša usluga više neće moći da automatski plaća svoje račune.',
 
-    'label' => 'Label',
-    'label_placeholder' => 'Enter a custom label for this service',
-    'label_modal_title' => 'Edit Service Label',
-    'update_label' => 'Update Label',
+    'label' => 'Oznaka',
+    'label_placeholder' => 'Unesite prilagođenu oznaku za ovu uslugu',
+    'label_modal_title' => 'Izmeni oznaku usluge',
+    'update_label' => 'Ažuriraj oznaku',
 
 ];
