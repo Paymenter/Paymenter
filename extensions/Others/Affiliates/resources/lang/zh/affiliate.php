635aeb2cf537626795f28449697c4f4434f266a0
Corwin <info@corwindev.nl>
2026-01-22 15:29:02 +0100

    New translations affiliate.php (Chinese Traditional)

--- extensions/Others/Affiliates/resources/lang/zh/affiliate.php
+++ extensions/Others/Affiliates/resources/lang/zh/affiliate.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'affiliate' => 'Affiliate',
-    'signup-for-affiliate' => 'Signup for Affiliate',
-    'code' => 'Code',
-    'visitors' => 'Visitors',
-    'total-visitors' => 'Total Visitors',
-    'signups' => 'Signups',
-    'total-signups' => 'Total Signups',
-    'earnings' => 'Earnings',
-    'total-earnings' => 'Total Earnings',
-    'your-affiliate-link' => 'Your affiliate link',
-    'copy' => 'Copy',
+    'affiliate' => '附屬機構',
+    'signup-for-affiliate' => '註冊附屬機構',
+    'code' => '代碼',
+    'visitors' => '訪客數',
+    'total-visitors' => '總訪客數',
+    'signups' => '註冊數',
+    'total-signups' => '總註冊數',
+    'earnings' => '收入',
+    'total-earnings' => '總收入',
+    'your-affiliate-link' => '您的附屬機構連結',
+    'copy' => '複製',
 
-    'you-havent-signed-up-yet' => 'You have not signed up for affiliates program yet!',
-    'you-are-already-affiliated' => 'You have already signed up for affiliate program.',
-    'signup-success' => 'Successfully signed up for affiliate program!',
+    'you-havent-signed-up-yet' => '您尚未註冊附屬機構計畫！',
+    'you-are-already-affiliated' => '您已經註冊附屬機構計畫。',
+    'signup-success' => '成功註冊附屬機構計畫！',
 ];
