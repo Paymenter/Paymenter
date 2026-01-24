b92a3e9fb2164c07dcbf3449e4a94883def7b432
Corwin <info@corwindev.nl>
2026-01-24 17:34:23 +0100

    New translations admin.php (Chinese Traditional)

--- lang/zh/admin.php
+++ lang/zh/admin.php
@@ -5,10 +5,10 @@ return [
     'cronjob' => [
         'invoices_created' => '發票已創建',
         'orders_cancelled' => '已取消訂單',
-        'upgrade_invoices_updated' => 'Upgrade invoices updated',
-        'services_suspended' => 'Services suspended',
-        'services_terminated' => 'Services terminated',
+        'upgrade_invoices_updated' => '升級發票已更新',
+        'services_suspended' => '服務已停權',
+        'services_terminated' => '服務已終止',
         'tickets_closed' => '關閉的票據',
-        'email_logs_deleted' => 'Email logs deleted',
+        'email_logs_deleted' => '電子郵件紀錄已刪除',
     ],
 ];
