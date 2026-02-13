4f7e652479cd0de3722be9b87253433bc6304e69
Corwin <info@corwindev.nl>
2026-02-13 11:51:53 +0100

    New translations passwords.php (Indonesian)

--- lang/id/passwords.php
+++ lang/id/passwords.php
@@ -2,8 +2,8 @@
 
 return [
 
-    'reset' => 'Kata sandi Anda telah disetel ulang.',
-    'sent' => 'Kami telah mengirimkan email tautan pengaturan ulang kata sandi kamu!',
+    'reset' => 'Kata sandi Anda telah di reset.',
+    'sent' => 'Kami telah mengirimkan email tautan pengaturan ulang kata sandi kamu.',
     'throttled' => 'Harap tunggu sebelum mencoba kembali.',
     'token' => 'Token reset kata sandi ini tidak valid.',
     'user' => "Kami tidak dapat menemukan pengguna dengan alamat email tersebut.",
