9d42fee8e4adcae9785f3e60bc519d719ec79256
Corwin <info@corwindev.nl>
2026-05-14 14:15:45 +0200

    New translations passwords.php (Polish)
    [ci skip]

--- lang/pl/passwords.php
+++ lang/pl/passwords.php
@@ -6,6 +6,6 @@ return [
     'sent' => 'Właśnie wysłaliśmy wiadomość na Twój adres e-mail z linkiem do zmiany hasła.',
     'throttled' => 'Proszę poczekać przed ponownym próbą.',
     'token' => 'Ten token resetowania hasła jest nieprawidłowy.',
-    'user' => 'Nie możemy znaleźć użytkownika z tym adresem e-mail.',
+    'user' => "Nie możemy znaleźć użytkownika z tym adresem e-mail.",
 
 ];
