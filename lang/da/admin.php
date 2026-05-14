d07b1dc10d8ee2b14b7562ba104ff55123ea4a89
Corwin <info@corwindev.nl>
2026-05-15 01:51:13 +0200

    New translations admin.php (Danish)
    [ci skip]

--- lang/da/admin.php
+++ lang/da/admin.php
@@ -9,6 +9,6 @@ return [
         'services_suspended' => 'Services suspended',
         'services_terminated' => 'Services terminated',
         'tickets_closed' => 'Tickets closed',
-        'email_logs_deleted' => 'Email logs deleted',
+        'email_logs_deleted' => 'Email logs slettet',
     ],
 ];
