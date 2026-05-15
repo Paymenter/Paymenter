cf16f010fc5beef7520bb61eca1e1de63796e118
Corwin <info@corwindev.nl>
2026-05-15 02:52:55 +0200

    New translations affiliate.php (Danish)
    [ci skip]

--- extensions/Others/Affiliates/resources/lang/da/affiliate.php
+++ extensions/Others/Affiliates/resources/lang/da/affiliate.php
@@ -1,19 +1,19 @@
 <?php
 
 return [
-    'affiliate' => 'Affiliate',
-    'signup-for-affiliate' => 'Signup for Affiliate',
-    'code' => 'Code',
-    'visitors' => 'Visitors',
-    'total-visitors' => 'Total Visitors',
-    'signups' => 'Signups',
-    'total-signups' => 'Total Signups',
-    'earnings' => 'Earnings',
-    'total-earnings' => 'Total Earnings',
-    'your-affiliate-link' => 'Your affiliate link',
-    'copy' => 'Copy',
+    'affiliate' => 'Partner',
+    'signup-for-affiliate' => 'Bliv partner',
+    'code' => 'Kode',
+    'visitors' => 'Besøgende',
+    'total-visitors' => 'Totalt antal besøgende',
+    'signups' => 'Tilmeldinger',
+    'total-signups' => 'Totalt antal tilmeldinger',
+    'earnings' => 'Indtægter',
+    'total-earnings' => 'Total indtægt',
+    'your-affiliate-link' => 'Dit partner link',
+    'copy' => 'Kopir',
 
-    'you-havent-signed-up-yet' => 'You have not signed up for affiliates program yet!',
-    'you-are-already-affiliated' => 'You have already signed up for affiliate program.',
+    'you-havent-signed-up-yet' => 'Du er ikke en del af vores partner program!',
+    'you-are-already-affiliated' => 'Du er allerede en del af vores partner program.',
     'signup-success' => 'Du er nu en del af vores partner program!',
 ];
