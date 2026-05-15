17db3c59353576f8b47b4e83a178094f732a5ff9
Andrei Bucur <andreibucur@Andreis-MacBook-Air.local>
2026-05-15 15:40:02 +0300

    chore: code cleanup

--- app/Observers/InvoiceObserver.php
+++ app/Observers/InvoiceObserver.php
@@ -15,7 +15,6 @@ class InvoiceObserver
     public function creating(Invoice $invoice): void
     {
         if ($invoice->status === Invoice::STATUS_DRAFT) {
-            Log::info('status draft creating');
             return;
         }
 
@@ -28,7 +27,6 @@ class InvoiceObserver
     public function created(Invoice $invoice): void
     {
         if ($invoice->status === Invoice::STATUS_DRAFT) {
-            Log::info('status draft created');
             return;
         }
 
