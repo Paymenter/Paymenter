e60567981acfb12e3c8d13e513e2959edd8b9d1c
Corwin <info@corwindev.nl>
2026-05-19 19:33:04 +0200

    fix: reCAPTCHA v3 not working after Livewire navigation

--- themes/default/views/components/captcha/recaptcha-v3.blade.php
+++ themes/default/views/components/captcha/recaptcha-v3.blade.php
@@ -2,20 +2,20 @@
 </script>
 <div id="g-recaptcha" data-sitekey="{{ config('settings.captcha_site_key') }}"></div>
 
+@script
 <script>
-    document.addEventListener('livewire:initialized', () => {
-        const submitBtn = document.querySelector('#{{ $form }} button[type="submit"]')
-
-        submitBtn.addEventListener("click", function(event) {
-            event.preventDefault();
-            grecaptcha.ready(function() {
-                grecaptcha.execute('{{ config('settings.captcha_site_key') }}', {
-                    action: 'submit'
-                }).then(function(token) {
-                    @this.set('captcha', token, false)
-                    @this.submit();
-                });
+    const submitBtn = document.querySelector('#{{ $form }} button[type="submit"]')
+    
+    submitBtn.addEventListener("click", function(event) {
+        event.preventDefault();
+        grecaptcha.ready(function() {
+            grecaptcha.execute('{{ config('settings.captcha_site_key') }}', {
+                action: 'submit'
+            }).then(function(token) {
+                @this.set('captcha', token, false)
+                @this.submit();
             });
         });
     });
 </script>
+@endscript
