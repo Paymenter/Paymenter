08e3c14b9b3a8950771ecd508f034cfeebf23678
unknown <alex.vlad@updivision.com>
2026-07-14 15:38:52 +0300

    fix: redirect edit page to view page

--- app/Admin/Resources/InvoiceResource/Pages/EditInvoice.php
+++ app/Admin/Resources/InvoiceResource/Pages/EditInvoice.php
@@ -14,15 +14,24 @@ class EditInvoice extends EditRecord
 {
     protected static string $resource = InvoiceResource::class;
 
+    public function mount(int | string $record): void
+    {
+        parent::mount($record);
+
+        if (config('settings.immutable_invoices_enabled', false)) {
+            redirect(InvoiceResource::getUrl('view', ['record' => $record]));
+        }
+    }
+
     protected function getHeaderActions(): array
     {
         return [
             Action::make('publish')
                 ->label('Publish')
                 ->action(function (Invoice $record) {
-                   $this->changeInvoiceStatusToPending($record);
+                    $this->changeInvoiceStatusToPending($record);
                 })
-                ->visible(fn (Invoice $record): bool => $record->status === Invoice::STATUS_DRAFT)
+                ->visible(fn(Invoice $record): bool => $record->status === Invoice::STATUS_DRAFT)
                 ->requiresConfirmation()
                 ->color('success')
                 ->icon('heroicon-o-check-circle')
@@ -43,7 +52,8 @@ class EditInvoice extends EditRecord
         ];
     }
 
-    protected function changeInvoiceStatusToPending(Invoice $invoice): void{
+    protected function changeInvoiceStatusToPending(Invoice $invoice): void
+    {
         $invoice->status = Invoice::STATUS_PENDING;
         $invoice->save();
     }
