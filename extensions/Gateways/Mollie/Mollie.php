79cce17f037eda3e9997b881a7d8a387ce6226bc
CorwinDev <88144943+CorwinDev@users.noreply.github.com>
2026-08-07 11:13:40 +0000

    style: linted files with pint

--- extensions/Gateways/Mollie/Mollie.php
+++ extensions/Gateways/Mollie/Mollie.php
@@ -44,7 +44,7 @@ class Mollie extends Gateway
                 'name' => 'api_key',
                 'label' => 'API Key',
                 'type' => 'text',
-		'encrypted' => true,
+                'encrypted' => true,
                 'required' => true,
             ],
         ];
