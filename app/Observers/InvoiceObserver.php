74b6f93f24c08f7c4be2c726e16e83791ac73b5a
FastCat Servers <admin@fastcat.co>
2026-08-17 13:13:13 -0400

    add import to Illuminate\Support\Facades\DB in InvoiceObserver

--- app/Observers/InvoiceObserver.php
+++ app/Observers/InvoiceObserver.php
@@ -5,6 +5,7 @@ namespace App\Observers;
 use App\Events\Invoice as InvoiceEvent;
 use App\Models\Invoice;
 use App\Services\Invoice\ProcessPaidInvoiceService;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceObserver
 {
