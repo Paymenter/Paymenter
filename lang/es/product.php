5de507f04c5fed56e33cb2aa1b3442258f9e1010
Corwin <info@corwindev.nl>
2026-08-18 22:53:01 +0200

    New translations product.php (Spanish)
    [ci skip]

--- lang/es/product.php
+++ lang/es/product.php
@@ -20,6 +20,7 @@ return [
 
     'in_stock' => 'En stock',
     'out_of_stock' => 'El producto :product está fuera de stock',
+    'not_available' => ':product is not available in your currency',
     'user_limit' => 'Has alcanzado el límite de :product',
     'payment_method' => 'Método de pago',
     'use_credits' => 'Usar créditos',
